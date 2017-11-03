15894baf67b8513f9041a1139e32d0866bc02fc9
ivanmora <iam.ivan.mora@gmail.com>
2017-11-03 11:24:47 -0500

    Challenge Ready

--- village/src/reducers/smurfsReducer.js
+++ village/src/reducers/smurfsReducer.js
@@ -5,14 +5,5 @@ import {
 
 
 export default (smurfs = [], action) => {
-  switch (action.type) {
-    case GET_SMURFS:
-      console.log("payload", action.payload.data);
-      return action.payload.data;
-    case ADD_SMURF:
-      smurfs.push(action.payload);
-      return smurfs;
-    default:
-      return smurfs;
-  }
+
 }
