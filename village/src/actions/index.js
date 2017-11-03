337af182edec320c1ab01da8ff7b50dd7a467da0
Sam Kim <sams404link@gmail.com>
2017-11-03 10:32:47 -0700

    Sprint challenge updated

--- village/src/actions/index.js
+++ village/src/actions/index.js
@@ -4,9 +4,19 @@ export const GET_SMURFS = "GET_SMURFS";
 export const ADD_SMURF = "ADD_SMURF";
 
 export const getSmurfs = () => {
-
+    const smurfsEndPoint = `http://localhost:3333/smurfs`;
+    const smurfs = axios.get(smurfsEndPoint);
+    return{
+        type: GET_SMURFS,
+        payload: smurfs
+    }
 }
 
 export const addSmurf = (smurf) => {
-
+    const smurfsEndPoint = `http://localhost:3333/smurfs`;
+    const postSmurfs = axios.post(smurfsEndPoint, )
+    return{
+        type: ADD_SMURF,
+        postSmurfs
+    }
 }
