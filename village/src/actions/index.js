5a472efc457012305751b37f850e5fcccf4bfb01
Jesse Reichel <Jesse.Reichel@live.com>
2017-11-03 11:00:47 -0700

    action/reducer

--- village/src/actions/index.js
+++ village/src/actions/index.js
@@ -4,9 +4,19 @@ export const GET_SMURFS = "GET_SMURFS";
 export const ADD_SMURF = "ADD_SMURF";
 
 export const getSmurfs = () => {
-
-}
+    const smurfHouse = '/smurfs';
+    const smurfGrab = axios.get(smurfHouse)
+    return {
+      type: GET_SMURFS,
+      payload: smurfGrab
+    };
+};
 
 export const addSmurf = (smurf) => {
-
-}
+    const smurfHouse = 'http://localhost:3333/smurfs';
+    const smurfMake =  axios.post(smurfHouse, smurf)
+    return {
+      type: ADD_SMURF,
+      payload: smurfMake
+    };
+};
