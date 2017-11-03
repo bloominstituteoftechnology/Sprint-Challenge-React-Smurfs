3e8391ef6baac881e7b451484430270ba63c0734
David Long <davdaarn@live.com>
2017-11-03 13:52:38 -0500

    no extra credit

---
+++ Answers.md
@@ -0,0 +1,12 @@
+#1 To the best of my understanding in Redux all state is stored in a single object tree
+as apposed to React where state is stored within each component.
+
+#2 I don't understand action yet :'(
+#3 A reducer takes in state and an action and returns a new state
+
+#4 Hyper Text Transfer Protocol.
+Create Read Update Delete.
+Create - Post
+Read - Get
+Update - Put
+Delete - Delete
--- village/src/reducers/smurfsReducer.js
+++ village/src/reducers/smurfsReducer.js
@@ -1,9 +1,17 @@
-import {
-  GET_SMURFS,
-  ADD_SMURF
-} from '../actions';
-
+import { GET_SMURFS, ADD_SMURF, UPDATE_SMURF, DELETE_SMURF } from '../actions';
 
 export default (smurfs = [], action) => {
-
-}
+  switch (action.type) {
+    case ADD_SMURF:
+      // const newSmurf = smurfs.push(action.payload.data);
+      return action.payload.data;
+    case GET_SMURFS:
+      return action.payload.data;
+    case UPDATE_SMURF:
+      return action.payload.data;
+    case DELETE_SMURF:
+      return action.payload.data;
+    default:
+      return smurfs;
+  }
+};
