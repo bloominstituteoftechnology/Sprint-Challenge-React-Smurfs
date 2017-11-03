dfc3ccd475542a207a26e5a71436751d39cedf81
sulemc <32285312+sulemc@users.noreply.github.com>
2017-11-03 13:18:17 -0500

    sprint complete

--- village/src/actions/index.js
+++ village/src/actions/index.js
@@ -4,9 +4,21 @@ export const GET_SMURFS = "GET_SMURFS";
 export const ADD_SMURF = "ADD_SMURF";
 
 export const getSmurfs = () => {
+    const smurfsEndpoint = 'http://localhost:3333/smurfs';
+    const smurfs = axios.get(smurfsEndpoint);
 
+    return { 
+        type: GET_SMURFS,
+        payload: smurfs, };
 }
 
 export const addSmurf = (smurf) => {
+    const smurfsEndpoint = 'http://localhost:3333/smurfs';
+    const smurfs = axios.post(smurfsEndpoint, smurf);
+
+
+    return {
+        type: ADD_SMURF,
+        payload: smurfs, };
 
 }
--- village/src/reducers/smurfsReducer.js
+++ village/src/reducers/smurfsReducer.js
@@ -5,5 +5,15 @@ import {
 
 
 export default (smurfs = [], action) => {
+  switch (action.type){
+    case GET_SMURFS:
+        return action.payload.data;
+    case ADD_SMURF:
+        let newSmurfs = smurfs.slice(0);
+        newSmurfs = action.payload.data;
+        return newSmurfs;
+    default:
+        return smurfs;
+}
 
 }
