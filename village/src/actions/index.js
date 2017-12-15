c7e76d247073e3750edb18559ac9bb329e677027
Ting Wang <felix.wang.1026@gmail.com>
2017-12-15 11:49:23 -0500

    Added actions and reducers

--- village/src/actions/index.js
+++ village/src/actions/index.js
@@ -3,10 +3,19 @@ import axios from 'axios';
 export const GET_SMURFS = "GET_SMURFS";
 export const ADD_SMURF = "ADD_SMURF";
 
+const URL = 'http://localhost:3333/smurfs';
 export const getSmurfs = () => {
-
+  const promise = axios.get(URL);
+  return {
+    type: GET_SMURFS,
+    payload: promise
+  };
 }
 
 export const addSmurf = (smurf) => {
-
+  const promise = axios.post(URL, smurf);
+  return {
+    type: ADD_SMURF,
+    payload: promise
+  };
 }
--- village/src/reducers/smurfsReducer.js
+++ village/src/reducers/smurfsReducer.js
@@ -3,7 +3,13 @@ import {
   ADD_SMURF
 } from '../actions';
 
-
 export default (smurfs = [], action) => {
-
+  switch(action.type) {
+    case GET_SMURFS:
+      return action.payload.data;
+    case ADD_SMURF:
+      return action.payload.data;
+    default:
+      return smurfs;
+  }
 }
