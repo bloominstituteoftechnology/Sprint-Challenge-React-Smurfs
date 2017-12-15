3ca8f0dc16b02eb81c682b7eee7a0160805afacb
Patrick Sandoval <patsand24csu@yahoo.com>
2017-12-15 10:30:18 -0800

    add working, need to work on get

--- village/src/actions/index.js
+++ village/src/actions/index.js
@@ -4,9 +4,19 @@ export const GET_SMURFS = "GET_SMURFS";
 export const ADD_SMURF = "ADD_SMURF";
 
 export const getSmurfs = () => {
-
+    // GET since getting/reading smurf data
+    const promise = axios.get('http://localhost:3333/smurfs');
+    return {
+        type: GET_SMURFS,
+        payload: promise
+    }
 }
 
 export const addSmurf = (smurf) => {
-
+    // Should be POST since we are adding/creating new smurf
+    axios.post('http://localhost:3333/smurfs');
+    return {
+        type: ADD_SMURF,
+        payload: smurf
+    }
 }
--- village/src/reducers/smurfsReducer.js
+++ village/src/reducers/smurfsReducer.js
@@ -5,5 +5,13 @@ import {
 
 
 export default (smurfs = [], action) => {
-
+  switch(action.type) {
+    case GET_SMURFS:
+      return action.payload.data;
+    case ADD_SMURF:
+      const smurfsData = smurfs.concat(action.payload);
+      return smurfsData;
+    default:
+      return smurfs;
+  }
 }
