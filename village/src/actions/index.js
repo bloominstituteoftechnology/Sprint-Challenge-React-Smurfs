cfae328f3d354cd749428d3317d8bce86f588b65
Roarka <designerexpert@gmail.com>
2017-12-15 13:11:43 -0500

    Finished the Basic, added a few Buttons, added ID field, finished the actions for Delete and Put, working out the flow for functionality of HTTP request to finish it off

--- village/src/actions/index.js
+++ village/src/actions/index.js
@@ -2,11 +2,41 @@ import axios from 'axios';
 
 export const GET_SMURFS = "GET_SMURFS";
 export const ADD_SMURF = "ADD_SMURF";
+export const PUT_SMURF = "PUT_SMURF";
+export const DEL_SMURF = "DEL_SMURF";
+
+const serverUrl = 'http://localhost:3333/smurfs';
 
 export const getSmurfs = () => {
-
-}
+    const smurfs = axios.get(serverUrl);
+    return {
+        type: GET_SMURFS,
+        payload: smurfs
+    };
+};
 
 export const addSmurf = (smurf) => {
+    const newSmurfs = axios.post(serverUrl, smurf);
+    return {
+        type: ADD_SMURF,
+        payload: newSmurfs
+    };
+};
 
-}
+export const putSmurf = (smurf) => {
+    const smurfs = axios.get(serverUrl);
+    const smurfPut = axios.put(serverUrl, smurf);
+    return {
+        type: PUT_SMURF,
+        payload: smurfs
+    };
+};
+
+export const delSmurf = (smurf) => {
+    const smurfs = axios.get(serverUrl);
+    const smurfDel = axios.delete(serverUrl, smurf);
+    return {
+        type: DEL_SMURF,
+        payload: smurfs
+    };
+};
--- village/src/reducers/smurfsReducer.js
+++ village/src/reducers/smurfsReducer.js
@@ -1,9 +1,22 @@
 import {
   GET_SMURFS,
-  ADD_SMURF
+  ADD_SMURF,
+  PUT_SMURF,
+  DEL_SMURF
 } from '../actions';
 
 
 export default (smurfs = [], action) => {
-
+  switch (action.type) {
+    case GET_SMURFS:
+      return action.payload.data;
+    case ADD_SMURF:
+      return action.payload.data;
+    case PUT_SMURF:
+      return action.payload.data;
+    case DEL_SMURF:
+      return action.payload.data;
+    default:
+      return smurfs;
+  }
 }
