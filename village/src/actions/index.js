f94b05dbb6a48955722c5e14e7af60efd03b0225
Capril1042 <Caprilewis1042@gmail.com>
2017-12-15 14:09:45 -0500

    actions complete

--- village/src/actions/index.js
+++ village/src/actions/index.js
@@ -2,11 +2,42 @@ import axios from 'axios';
 
 export const GET_SMURFS = "GET_SMURFS";
 export const ADD_SMURF = "ADD_SMURF";
+export const UPDATE_SMURF= "UPDATE_SMURF";
+export const DELETE_SMURF= "DELETE_SMURF";
 
 export const getSmurfs = () => {
-
-}
+    const promise = axios.get('http://localhost:3333/smurfs');
+    return {
+        type: GET_SMURFS,
+        payload: promise
+    };
+};
 
 export const addSmurf = (smurf) => {
+    const promise =axios.post('http://localhost:3333/new-smurf', smurf);
+    return {
+        type: ADD_SMURF,
+        payload: promise
+    };
+};
 
-}
+export const updateSmurf = (smurf) => {
+    const promise =axios.put('http://localhost:3333/update-smurf', smurf);
+    return {
+        type: UPDATE_SMURF,
+        payload: promise
+    };
+};
+ 
+export const deleteSmurf =( index) => {
+    const promise =axios.delete('http://localhost:3333/delete-smurf', {
+        data: {
+            index
+        }
+    });
+    return {
+        type: DELETE_SMURF,
+        payload: promise
+    };
+};
+
