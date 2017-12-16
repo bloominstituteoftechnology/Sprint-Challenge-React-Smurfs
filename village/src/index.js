821ac14d45a17046bf2390f5262b4db80ca0b068
Yasin Shuman <email@yasin.io>
2017-12-15 16:04:21 -0800

    added action creators and smurfReducers for get an array of smurfs and adding smurfs to the array

--- village/src/index.js
+++ village/src/index.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import ReduxPromise from 'redux-promise';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import ReduxPromise from "redux-promise";
 
-import reducers from './reducers';
+import reducers from "./reducers";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider
+    store={createStoreWithMiddleware(
+      reducers,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
     <App />
-  </Provider>, document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
--- village/src/reducers/smurfsReducer.js
+++ village/src/reducers/smurfsReducer.js
@@ -1,9 +1,12 @@
-import {
-  GET_SMURFS,
-  ADD_SMURF
-} from '../actions';
-
+import { GET_SMURFS, ADD_SMURF } from "../actions";
 
 export default (smurfs = [], action) => {
-
-}
+  switch (action.type) {
+    case GET_SMURFS:
+      return action.payload.data;
+    case ADD_SMURF:
+      return action.payload.data;
+    default:
+      return smurfs;
+  }
+};
