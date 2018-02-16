94302ca071257bdea601a4d866dcb70e46e1f568
rcs784 <rcstinson@hotmail.com>
2018-02-16 15:27:15 -0500

    commit so TAs can diagnose bug

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import Smurf from './Smurf';
 
 class Smurfs extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
+  constructor() {
+    super();
+    this.state = {
+      smurfs: [],
+    };
+  }
+
+  componentDidMount() {
+    this.doGet();
+  }
+
+  doGet() {
+    let smurfs = [];
+    axios.get('http://localhost:3333/smurfs')
+      .then((response) => {
+        this.setState({
+          smurfs: response.data,
+        });
+      })
+      .catch((error) => {
+        throw new Error(error);
+      });
+  }
 
   render() {
     return (
       <div className="Smurfs">
         <h1>Smurf Village</h1>
         <ul>
-          { this.state.smurfs.map((smurf) => {
+          {this.state.smurfs.map((smurf) => {
             return <Smurf name={smurf.name} age={smurf.age} height={smurf.height} key={smurf.id} />;
           })}
         </ul>
