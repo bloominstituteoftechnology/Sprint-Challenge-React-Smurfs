2f448d3919e6292a1a4d57f318536145b4b18b07
Tommy Coleman <tommycoleman87@gmail.com>
2018-02-16 15:59:02 -0500

    got the add and delete function to rerender the village

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,79 +1,30 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-class SmurfForm extends Component {
+import React  from 'react';
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      age: '',
-      height: ''
-    };
-    this.addSmurf = this.addSmurf.bind(this);
-    this.updateName = this.updateName.bind(this);
-    this.updateAge = this.updateAge.bind(this);
-    this.updateHeight = this.updateHeight.bind(this);
-  }
- 
-  addSmurf(event) {
-    event.preventDefault();
-    // add code to create the smurf using the api
-    axios
-    .post('http://localhost:3333/smurfs', this.state)
-    .then(response => {
-    console.log(response)})
-    .catch(error => {
-      console.error('Server Error', error)
-    });
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
-  }
+const SmurfForm = (props) => {
 
-  updateName(event) {
-    this.setState({
-      name: event.target.value
-    });
-  }
-
-  updateAge(event) {
-    this.setState({
-      age: event.target.value
-    });
-  }
-
-  updateHeight(event) {
-    this.setState({
-      height: event.target.value
-    });
-  }
-
-  render() {
     return (
       <div className="SmurfForm">
-        <form onSubmit={this.addSmurf}>
+        <form onSubmit={props.addSmurf}>
           <input
-            onChange={this.updateName}
+            onChange={props.updateName}
             placeholder="name"
-            value={this.state.name}
+            value={props.name}
           />
           <input
-            onChange={this.updateAge}
+            onChange={props.updateAge}
             placeholder="age"
-            value={this.state.age}
+            value={props.age}
           />
           <input
-            onChange={this.updateHeight}
+            onChange={props.updateHeight}
             placeholder="height"
-            value={this.state.height}
+            value={props.height}
           />
-          <button type="submit">Add to the village</button>
+          <button type="submit" onSubmit={props.addSmurf} >Add to the village</button>
         </form>
       </div>
     );
   }
-}
+
 
 export default SmurfForm;
--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,49 +1,21 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React  from 'react';
+
 import Smurf from './Smurf';
 
-class Smurfs extends Component {
-  state = {
-   smurfs: [],
-   
-  }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  componentDidMount() {
-    this.loadFriends();
-  }
+const Smurfs = (props) => {
 
-  loadFriends() {
-    axios
-      .get('http://localhost:3333/smurfs')
-      .then(response => {
-        this.setState(() => ({ smurfs: response.data }));
-      })
-  }
-
-   deleteSmurf = (id) => {
-    const endpoint = `http://localhost:3333/smurfs/${id}`;
-    axios
-    .delete(endpoint)
-    .then(response => {
-      console.log(response);
-      this.loadFriends();
-    })
-    .catch(error => {
-      console.log(error)
-    })
-  }
-  render() {
+ 
     return (
       <div className="Smurfs">
         <h1>Smurf Village</h1>
         <ul>
-          { this.state.smurfs.map((smurf) => {
-          return <Smurf name={smurf.name} age={smurf.age} height={smurf.height} key={smurf.id} id={smurf.id} onDelete={this.deleteSmurf} />;
+          { props.smurfs.map((smurf) => {
+          return <Smurf name={smurf.name} age={smurf.age} height={smurf.height} key={smurf.id} id={smurf.id} onDelete={props.deleteSmurf} />;
           })}
         </ul>
       </div>
     );
-  }
+
 }
 
 export default Smurfs;
