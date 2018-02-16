03c16c41e754605d2c797fc2bcc6396052036408
Lo Saephan <teamsky916@gmail.com>
2018-02-16 09:54:34 -0800

    re-arranged data flow to have app.js/parent component handle all data. added post ajax call to server.

--- village/src/App.js
+++ village/src/App.js
@@ -3,15 +3,56 @@ import React, { Component } from 'react';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import axios from 'axios';
 
 class App extends Component {
-  render() {
-    return (
+	state = {smurfs: [], name: '', age: '', height: ''};
+
+  componentDidMount(){
+  	axios.get('http://localhost:3333/smurfs')
+    .then((res)=>{
+      this.setState({smurfs: res.data});
+    })
+    .catch((err)=>{
+      console.log(err);
+    });
+  }
+
+	render() {
+   	return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs/>
+        <SmurfForm name={this.state.name} age={this.state.age} height={this.state.height}handleInput={this.handleInput} addSmurf={this.addSmurf}/>
+        <Smurfs smurfs={this.state.smurfs}/>
       </div>
     );
+  }
+
+  addSmurf = (event)=>{
+    event.preventDefault();
+    let {name, age, height} = this.state;
+    let smurf = {name, age, height}
+
+    axios.post('http://localhost:3333/smurfs', smurf)
+    .then((res)=>{
+      this.setState({smurfs: res.data});
+    })
+    .catch((err)=>{
+      console.log(err);
+    });
+      
+    this.setState({
+      name: '',
+      age: '',
+      height: ''
+    });
+  }
+
+  handleInput = (event)=>{
+  	let name = event.target.name;
+  	let value = event.target.value;
+    this.setState({
+      [name]: value,
+    });
   }
 }
 
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,73 +1,33 @@
 import React, { Component } from 'react';
 
-class SmurfForm extends Component {
+function SmurfForm (props) {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      age: '',
-      height: ''
-    };
-    this.addSmurf = this.addSmurf.bind(this);
-    this.updateName = this.updateName.bind(this);
-    this.updateAge = this.updateAge.bind(this);
-    this.updateHeight = this.updateHeight.bind(this);
-  }
-
-  addSmurf(event) {
-    event.preventDefault();
-    // add code to create the smurf using the api
-    
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
-  }
-
-  updateName(event) {
-    this.setState({
-      name: event.target.value
-    });
-  }
-
-  updateAge(event) {
-    this.setState({
-      age: event.target.value
-    });
-  }
-
-  updateHeight(event) {
-    this.setState({
-      height: event.target.value
-    });
-  }
-
-  render() {
     return (
       <div className="SmurfForm">
-        <form onSubmit={this.addSmurf}>
+        <form onSubmit={props.addSmurf}>
           <input
-            onChange={this.updateName}
+            onChange={props.handleInput}
+            name='name'
             placeholder="name"
-            value={this.state.name}
+            value={props.name}
           />
           <input
-            onChange={this.updateAge}
+            onChange={props.handleInput}
+            name='age'
             placeholder="age"
-            value={this.state.age}
+            value={props.age}
           />
           <input
-            onChange={this.updateHeight}
+            onChange={props.handleInput}
+            name='height'
             placeholder="height"
-            value={this.state.height}
+            value={props.height}
           />
           <button type="submit">Add to the village</button>
         </form>
       </div>
     );
-  }
+
 }
 
 export default SmurfForm;
