912292c72412b54eb4d00631b1b2da5b68acadfa
Smithface <kylewibblesmith@gmail.com>
2018-02-16 12:59:46 -0500

    changed Smurf to class instead of function

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,14 +1,23 @@
 import React from 'react';
 
-const Smurf = (props) => {
-  return (
-    <div className="Smurf">
-      <h3>{props.name}</h3>
-      <strong>{props.height} tall</strong>
-      <p>{props.age} old</p>
-      <button onClick={() => props.onDelete(props.id)}>Delete {props.name}</button>
-    </div>
-  );
+class Smurf extends React.Component {
+  state = {
+    editing: false,
+    smurfName: '',
+    smurfAge: '',
+    smurfHeight: '',
+  }
+
+  render() {
+    return (
+      <div className="Smurf">
+        <h3>{this.props.name}</h3>
+        <strong>{this.props.height} tall</strong>
+        <p>{this.props.age} old</p>
+        <button onClick={() => this.props.onDelete(this.props.id)}>Delete {this.props.name}</button>
+      </div>
+    );
+  }
 }
 
 export default Smurf;
