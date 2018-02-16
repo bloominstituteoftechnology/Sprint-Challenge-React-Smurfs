2e8489768720ebeeabba1104afc606b5b945c84b
Mephestys <mephestys@gmail.com>
2018-02-16 15:35:53 -0700

    Added axios "get" function to Smurfs.js

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import Smurf from './Smurf';
 
 class Smurfs extends Component {
+  state = {
+    smurfs: [],
+  };
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
+
+  componentDidMount() {
+    this.loadSmurfs();
+  }
+
+  loadSmurfs = () => {
+    axios
+    .get('http://localhost:3333/smurfs')
+    .then(response => {
+      this.setState({
+        smurfs: response.data,
+      });
+    })
+    .catch(() => {
+      console.error('Error nabbing up smurfs');
+    });
+  };
 
   render() {
     return (
