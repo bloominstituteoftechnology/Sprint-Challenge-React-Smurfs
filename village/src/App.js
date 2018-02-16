2b53a52fe6d86111cf7915227971980bd68a1495
Amanda Phillips <amanda@phillipsdev.org>
2018-02-16 11:26:35 -0600

    errors fixed, get and post added, still not fully funtional

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -8,11 +9,29 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs/>
+        <SmurfForm onCreate={this.loadSmurfs}/>
+        <Smurfs />
       </div>
     );
   }
+
+  componentDidMount() {
+    this.loadSmurfs();
+  }
+
+  loadSmurfs = () => {
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(response => {
+        this.setState({
+          smurfs: response.data,
+        });
+      })
+      .catch(() => {
+        console.error('error getting data');
+      });
+  };
+
 }
 
 export default App;
