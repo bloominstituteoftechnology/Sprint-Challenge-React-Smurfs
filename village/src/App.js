624ca127144640c72934c0dc6e171b85cbd89785
Cody Windeknecht <lhg614@mocs.utc.edu>
2018-02-16 11:38:30 -0500

    Removed All Errors
    Don't believe I modified the json files, unless that is the result of importing axios...
    
    App.js --- Added axios.get to smurfs list.
    
    Smurfs.js --- gave the state a smurfs property, passed it the props from App.js.

--- village/src/App.js
+++ village/src/App.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 
 class App extends Component {
+  state = {
+    smurfs: []
+  }
   render() {
+    console.log("On app: ", this.state.smurfs)
     return (
       <div className="App">
         <SmurfForm />
-        <Smurfs/>
-      </div>
+        <Smurfs smurfs={this.state.smurfs}/>
+      </div> 
     );
+  }
+  componentDidMount() {
+    this.loadSmurfs();
+  }
+
+  loadSmurfs = () => {
+    axios.get('http://localhost:3333/smurfs')
+    .then(response => {
+      this.setState({ smurgs: response.data});
+    })
+    .catch(error => {
+      console.log('there was error', error);
+    });
   }
 }
 
