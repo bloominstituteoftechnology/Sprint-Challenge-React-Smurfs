217763b385d09753087dbe00f42605f7c9912590
Glenn-David Daniel <d2rd@outlook.com>
2018-02-16 12:55:33 -0800

    Safety before adding prop-types library

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -2,40 +2,45 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Smurf from './Smurf';
 
-  function Smurfs(props) {
-    return (
-      <ul>
-        {props.smurfs.map(smurf => {
-          return (
-            <Smurf
-              key={smurf.id}
-              name={smurf.name}
-              age={smurf.age}
-              height={smurf.height}
-            />
-          );
-        })}
-      </ul>
-    );
-  }
+function SmurfList(props) {
+  return (
+    <ul>
+      {props.smurfList.map(smurf => {
+        return (
+          <Smurf
+            key={smurf.id}
+            name={smurf.name}
+            age={smurf.age}
+            height={smurf.height}
+          />
+        );
+      })}
+    </ul>
+  );
+}
 
 class Smurfs extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-
-
+  state = {
+    // editing: false,
+    name: '',
+    age: '',
+    height: ''
+  };
+  
   render() {
     return (
       <div className="Smurfs">
         <h1>Smurf Village</h1>
         <ul>
-          { this.state.smurfs.map((smurf) => {
+          { this.smurfList.map((smurf) => {
             return 
-            <Smurf 
-            key={smurf.id}
-            name={smurf.name} 
-            age={smurf.age} 
-            height={smurf.height} 
-             />;
+              <Smurf 
+              key={smurf.id}
+              name={smurf.name} 
+              age={smurf.age} 
+              height={smurf.height} 
+              />;
           })}
         </ul>
       </div>
@@ -43,9 +48,10 @@ class Smurfs extends Component {
   }
 }
 
-Smurfs.propTypes = {
-  smurfs: propTypes.array.isRequired,
-}
+
+// Smurfs.propTypes = {
+//   smurfs: propTypes.array.isRequired,
+// }
 
 Smurfs.defaultProps = {
   smurfs: [],
