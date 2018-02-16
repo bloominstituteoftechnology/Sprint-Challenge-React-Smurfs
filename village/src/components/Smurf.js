2747cae89e3912be2a6a259869c22c8f5d955c71
Smithface <kylewibblesmith@gmail.com>
2018-02-16 13:27:39 -0500

    added modify button
    the button toggles between 3 inputs and the original HTML elements. next up is to add a save button that saves the changes

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -9,15 +9,41 @@ class Smurf extends React.Component {
   }
 
   render() {
+    const editing = this.state.editing;
+    
     return (
       <div className="Smurf">
-        <h3>{this.props.name}</h3>
-        <strong>{this.props.height} tall</strong>
-        <p>{this.props.age} old</p>
+        {editing === true ? (
+          <div>
+            <input type="text" name="smurfName" value={this.state.smurfName} onChange={this.handleChange} />
+            <input type="text" name="smurfAge" value={this.state.smurfAge} onChange={this.handleChange} />
+            <input type="text" name="smurfHeight" value={this.state.smurfHeight} onChange={this.handleChange} />
+          </div>
+        ) : (
+          <div>
+            <h3>{this.props.name}</h3>
+            <strong>{this.props.height} tall</strong>
+            <p>{this.props.age} old</p>
+          </div>
+        )}
+        <button onClick={this.toggleEditing}>{ editing ? 'Cancel' : `Modify ${this.props.name}`}</button>
         <button onClick={() => this.props.onDelete(this.props.id)}>Delete {this.props.name}</button>
       </div>
     );
   }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  toggleEditing = () => {
+    this.setState(prevState => {
+      return {
+        editing: !prevState.editing,
+      };
+    });
+  };
+
 }
 
 export default Smurf;
