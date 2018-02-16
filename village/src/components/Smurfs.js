d082b184319878c81326afddb0a71f67a55bd162
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-02-16 14:00:55 -0500

    Simplified Smurfs.js
    Now functional component as opposed to class component since state
    is now on App.js.
    
    Moved GET method to App.js
    
    Modified map function to use state passed in as prop

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,32 +1,19 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
 import Smurf from './Smurf';
 
-class Smurfs extends Component {
-  state = {
-    smurfs: [],
-  }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  componentDidMount() {
-    axios.get('http://localhost:3333/smurfs')
-    .then(response => 
-      this.setState({smurfs: response.data }))
-    .catch(error => {
-      console.error('Error', error);
-    })
-  }
-  render() {
-    return (
-      <div className="Smurfs">
-        <h1>Smurf Village</h1>
-        <ul>
-          { this.state.smurfs.map((smurf) => {
-            return <Smurf name={smurf.name} age={smurf.age} height={smurf.height} key={smurf.id} />;
-          })}
-        </ul>
-      </div>
-    );
-  }
+function Smurfs(props) {
+  return (
+    <div className="Smurfs">
+      <h1>Smurf Village</h1>
+      <ul>
+        {props.smurfs.map(smurf => {
+          return (
+            <Smurf name={smurf.name} age={smurf.age} height={smurf.height} key={smurf.id}/>
+          )
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default Smurfs;
