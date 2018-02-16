d84d8b7851e461df2460366c2ce37843a9610b8c
Tommy Coleman <tommycoleman87@gmail.com>
2018-02-16 12:00:07 -0500

    working api, first commit

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import Smurf from './Smurf';
 
 class Smurfs extends Component {
+  state = {
+   smurfs : [],
+  }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-
+  componentDidMount() {
+    axios
+      .get('http://localhost:3333/api/smurfs')
+      .then(response => {
+        this.setState(() => {smurfs: response.date });
+      })
+  }
   render() {
     return (
       <div className="Smurfs">
