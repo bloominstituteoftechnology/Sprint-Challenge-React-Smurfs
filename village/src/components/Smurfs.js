a8822b69b84039372ee04a9833d6c245d448941e
Christopher Beards <beardscm@gmail.com>
2018-02-16 18:30:16 -0500

    Primary Tasks Completed

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -2,12 +2,11 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Smurf from './Smurf';
 
-
 class Smurfs extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   state = {
     smurfs: [],
-  }
+  };
 
   componentDidMount() {
     const endpoint = 'http://localhost:3333/smurfs';
@@ -19,16 +18,23 @@ class Smurfs extends Component {
       })
       .catch(error => {
         console.log('error', error);
-      })
-    }
+      });
+  }
 
   render() {
     return (
       <div className="Smurfs">
         <h1>Smurf Village</h1>
         <ul>
-          { this.state.smurfs.map((smurf) => {
-            return <Smurf name={smurf.name} age={smurf.age} height={smurf.height} key={smurf.id} />;
+          {this.state.smurfs.map(smurf => {
+            return (
+              <Smurf
+                name={smurf.name}
+                age={smurf.age}
+                height={smurf.height}
+                key={smurf.id}
+              />
+            );
           })}
         </ul>
       </div>
