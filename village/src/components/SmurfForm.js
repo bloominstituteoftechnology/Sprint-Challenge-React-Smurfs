28bcc5562a5f63da6169a6a9de177ef9fdc0f036
Nate152 <35231160+Nate152@users.noreply.github.com>
2018-02-16 12:28:59 -0500

    Partial Post code, non-functional

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
 
@@ -18,11 +19,15 @@ class SmurfForm extends Component {
   addSmurf(event) {
     event.preventDefault();
     // add code to create the smurf using the api
-    
+    let id = 0;
+    const newSmurf = { name: this.name, age: this.age, height: this.height, id: this.id };
+    this.setState(() => (newSmurf));
+    axios.post('http://localhost:3333/smurfs', newSmurf).then((result => {}));
     this.setState({
       name: '',
       age: '',
-      height: ''
+      height: '',
+      id: (this.id) ++,
     });
   }
 
