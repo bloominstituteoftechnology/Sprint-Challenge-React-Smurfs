851dd9cc008c6216f10151f63af22520d683a9bc
Christopher Beards <beardscm@gmail.com>
2018-02-16 10:36:35 -0500

    Smurf Form Displays

--- village/src/App.js
+++ village/src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import Smurf from './components/Smurf';
 
 class App extends Component {
+  state = {
+    smurfs: [],
+  }
+
+  catchSmurfs = () => {
+    const endpoint = 'http://localhost:3333/smurfs';
+
+    axios
+      .get(endpoint)
+      .then(response => {
+        this.setState({ smurfs: response.data });
+      })
+      .catch(error => {
+        console.log('error', error);
+      })
+  }
+
   render() {
     return (
       <div className="App">
         <SmurfForm />
-        <Smurfs/>
+        <Smurf />
       </div>
     );
   }
