67150a4034143d97cb3cc474e26a804ecc7086f6
shobanavv <shobanavv@gmail.com>
2018-02-16 10:15:28 -0800

    added state to class component and GET method using Axios.

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import Smurf from './Smurf';
 
 class Smurfs extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-
+  state = {
+    smurfs:[{x:1}],
+  }
+  componentDidMount() {
+    axios 
+    .get('http://localhost:3333')
+    .then(res => {
+      this.setState(() => {smurfs: res});
+    })
+    .catch(error => {
+        console.error(error);
+    });
+    console.log();
+  }
   render() {
     return (
       <div className="Smurfs">
