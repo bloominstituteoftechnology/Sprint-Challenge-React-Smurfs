f5448eff72cf7a36ea478efe28196e9223c3c078
Randy Wilson <phoenixrr2113@gmail.com>
2018-03-16 11:06:52 -0700

    not finished

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,73 +1,81 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
 class SmurfForm extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: "",
+			age: "",
+			height: ""
+		};
+		this.addSmurf = this.addSmurf.bind(this);
+		this.updateName = this.updateName.bind(this);
+		this.updateAge = this.updateAge.bind(this);
+		this.updateHeight = this.updateHeight.bind(this);
+	}
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      age: '',
-      height: ''
-    };
-    this.addSmurf = this.addSmurf.bind(this);
-    this.updateName = this.updateName.bind(this);
-    this.updateAge = this.updateAge.bind(this);
-    this.updateHeight = this.updateHeight.bind(this);
-  }
-
-  addSmurf(event) {
-    event.preventDefault();
+	addSmurf(event) {
+		event.preventDefault();
     // add code to create the smurf using the api
-    
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
+    const newSmurf = {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height
+    }
+    axios
+    .post(`http://localhost:3333/smurfs`, newSmurf)
+    .then(response => {
+      this.setState({
+        name: "",
+        age: "",
+        height: ""
+      });
     });
-  }
+	}
 
-  updateName(event) {
-    this.setState({
-      name: event.target.value
-    });
-  }
+	updateName(event) {
+		this.setState({
+			name: event.target.value
+		});
+	}
 
-  updateAge(event) {
-    this.setState({
-      age: event.target.value
-    });
-  }
+	updateAge(event) {
+		this.setState({
+			age: event.target.value
+		});
+	}
 
-  updateHeight(event) {
-    this.setState({
-      height: event.target.value
-    });
-  }
+	updateHeight(event) {
+		this.setState({
+			height: event.target.value
+		});
+	}
 
-  render() {
-    return (
-      <div className="SmurfForm">
-        <form onSubmit={this.addSmurf}>
-          <input
-            onChange={this.updateName}
-            placeholder="name"
-            value={this.state.name}
-          />
-          <input
-            onChange={this.updateAge}
-            placeholder="age"
-            value={this.state.age}
-          />
-          <input
-            onChange={this.updateHeight}
-            placeholder="height"
-            value={this.state.height}
-          />
-          <button type="submit">Add to the village</button>
-        </form>
+	render() {
+		return (
+			<div className="SmurfForm">
+				<form onSubmit={this.addSmurf}>
+					<input
+						onChange={this.updateName}
+						placeholder="name"
+						value={this.state.name}
+					/>
+					<input
+						onChange={this.updateAge}
+						placeholder="age"
+						value={this.state.age}
+					/>
+					<input
+						onChange={this.updateHeight}
+						placeholder="height"
+						value={this.state.height}
+					/>
+					<button type="submit">Add to the village</button>
+				</form>
       </div>
-    );
-  }
+		);
+	}
 }
 
 export default SmurfForm;
