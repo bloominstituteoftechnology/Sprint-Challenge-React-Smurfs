a1bf07f0d9e3b842bcf5718220ed972339277b94
Kenny <khpham24@gmail.com>
2018-03-16 12:09:25 -0600

    Kenny Completed

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
 
@@ -16,9 +17,14 @@ class SmurfForm extends Component {
   }
 
   addSmurf(event) {
-    event.preventDefault();
     // add code to create the smurf using the api
-    
+    axios.post('http://localhost:3333/smurfs', {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height
+    }).then(console.log('Smurf added!'))
+      .catch(console.log('Smurf add failed!'));
+
     this.setState({
       name: '',
       age: '',
