fb214e024b21f3e7a36d891258208d4e0be4e02c
Carlos Andrade <carlos.andrade@outlook.com>
2018-03-16 13:56:05 -0400

    Completed-no stretch goals

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 const Smurf = (props) => {
   return (
-    <div className="Smurf">
+    <div className="Smurf" >
       <h3>{props.name}</h3>
       <strong>{props.height} tall</strong>
       <p>{props.age} old</p>
+      {console.log(props)}
     </div>
   );
 }
