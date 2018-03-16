e3073265ae1083f2cf6857d7acfdc98c4fb0fa47
DragonRydrz <DragonRydrz@me.com>
2018-03-16 11:14:25 -0400

    added state to Smurfs.js

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import Smurf from './Smurf';
 
 class Smurfs extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
+  constructor() {
+    super()
+    this.state = {
+      smurfs: [],
+    }
+  }
+
+  componentDidMount() {
+    axios.get()
+  }
 
   render() {
     return (
