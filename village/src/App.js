70daf0bb5c551c7c273141e49d8cd91c2fdc41b2
liz <elizabethbraggisgreat@gmail.com>
2018-03-16 16:22:09 -0400

    added smurfs to app.js

--- village/src/App.js
+++ village/src/App.js
@@ -3,13 +3,15 @@ import React, { Component } from 'react';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import smurfs from './components/Smurfs';
+import Smurfs from './components/Smurfs';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <SmurfForm />
-        <smurfs/>
+        <smurfs />
+        <Smurfs /> 
       </div>
     );
   }
