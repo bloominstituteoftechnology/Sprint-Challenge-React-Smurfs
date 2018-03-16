3261853ea0295000fd85069e247253c14c3abda4
RiaClay <sherria.rydyn@gmail.com>
2018-03-16 16:15:22 -0500

    .

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -14,7 +14,9 @@ class Smurfs extends Component {
   componentDidMount() {
     axios
       .get('http://localhost:3333/smurfs')
-        .then(response => {this.setState({smurfs: response.data})})
+        .then(response => {
+          this.setState({smurfs: response.data})
+        })
         .catch(error => {
           console.error('No smurfs found', error)
         })
