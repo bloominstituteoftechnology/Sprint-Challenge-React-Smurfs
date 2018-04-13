ac0570cfb2a260a8468e250c96d4dc898659c1ea
jeffrey <jeffreyneilflynn@gmail.com>
2018-04-13 12:58:29 -0400

    update button functionality

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import UpdateSmurf from './UpdateSmurf';
 
 class Smurf extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      updateSmurf: false
+      updateSmurf: false,
+      name: '',
+      height: '',
+      age: ''
     };
   }
 
@@ -20,11 +24,23 @@ class Smurf extends Component {
     this.setState({ updateSmurf: !this.state.updateSmurf });
   }
 
+  handleInputChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
   updateSmurf = smurfID => {
+    const newSmurfInfo = {};
+    if (this.state.name !== '') newSmurfInfo['name'] = this.state.name;
+    if (this.state.age !== '') newSmurfInfo['age'] = this.state.age;
+    if (this.state.height !== '') newSmurfInfo['height'] = this.state.height;
+
     axios
-      .put('http://localhost:3333/smurfs/${smurfID}')
-      .then(response => this.getSmurfs())
-      .catch(error => console.log(error))
+      .put(`http://localhost:3333/smurfs/${smurfID}`, newSmurfInfo)
+      .then(response => {
+        this.setState({ name: '', height: '', age: '' });
+        this.props.getSmurfs();
+        this.toggleState();
+      }).catch(error => console.log(error))
   }
 
   render() {
@@ -36,7 +52,27 @@ class Smurf extends Component {
         <button onClick={() => this.deleteSmurf(this.props.smurf.id)}>Delete</button>
         <button onClick={() => this.toggleState()}>Update</button>
         {this.state.updateSmurf ? (
-          <div>hey</div>
+          <div>
+            <input
+              type="text"
+              onChange={this.handleInputChange}
+              name="name"
+              placeholder="Name"
+            />
+            <input
+              type="text"
+              onChange={this.handleInputChange}
+              name="height"
+              placeholder="Height"
+            />
+            <input
+              type="text"
+              onChange={this.handleInputChange}
+              name="age"
+              placeholder="Age"
+            />
+            <button onClick={() => this.updateSmurf(this.props.smurf.id)}>Submit</button>
+          </div>
         ) : null}
       </div>
     );
