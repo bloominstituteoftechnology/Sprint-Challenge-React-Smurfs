ae142e0885255916119d1dc19a3eaaeffbb3de87
Devon Bacon <devon.bacon@gmail.com>
2018-04-13 10:56:09 -0700

    Set up Smurf.js for update functionality

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Smurf = props => {
-  return (
-    <div className="Smurf">
-      <h3>{props.name}</h3>
-      <strong>{props.height} tall</strong>
-      <p>{props.age} smurf years old</p>
-    </div>
-  );
-};
+class Smurf extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="Smurf">
+        <h3>{this.props.name}</h3>
+        <strong>{this.props.height} tall</strong>
+        <p>{this.props.age} smurf years old</p>
+        <button onClick={() => this.props.updateSmurf(this.props.id)}>Update Smurf</button>
+      </div>
+    );
+  };
+}
 
 export default Smurf;
