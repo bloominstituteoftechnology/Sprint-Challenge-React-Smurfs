41538c1f68c7320c408aea5d3f4dca678dadd2e0
jeffrey <jeffreyneilflynn@gmail.com>
2018-04-13 12:24:09 -0400

    update button toggles state when clicked

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -4,13 +4,26 @@ import axios from 'axios';
 class Smurf extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      updateSmurf: false
+    };
   }
 
   deleteSmurf = smurfID => {
     axios
       .delete(`http://localhost:3333/smurfs/${smurfID}`)
       .then(response => this.props.getSmurfs())
+      .catch(error => console.log(error))
+  }
+
+  toggleState = () => {
+    this.setState({ updateSmurf: !this.state.updateSmurf });
+  }
+
+  updateSmurf = smurfID => {
+    axios
+      .put('http://localhost:3333/smurfs/${smurfID}')
+      .then(response => this.getSmurfs())
       .catch(error => console.log(error))
   }
 
@@ -21,6 +34,10 @@ class Smurf extends Component {
         <strong>{this.props.smurf.height} tall</strong>
         <p>{this.props.smurf.age} smurf years old</p>
         <button onClick={() => this.deleteSmurf(this.props.smurf.id)}>Delete</button>
+        <button onClick={() => this.toggleState()}>Update</button>
+        {this.state.updateSmurf ? (
+          <div>hey</div>
+        ) : null}
       </div>
     );
   }
---
+++ village/src/components/UpdateSmurf.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class UpdateSmurf extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>{console.log('hey')}</div>
+    )
+  }
+}
+
+export default UpdateSmurf;
