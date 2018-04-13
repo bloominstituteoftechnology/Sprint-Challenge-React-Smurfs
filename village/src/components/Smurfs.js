bce4ba1d030df72776ec861c4323ef6292f6c341
faulkenfett11 <36997363+faulkenfett11@users.noreply.github.com>
2018-04-13 10:02:00 -0700

    added get and post functionality

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import Smurf from './Smurf';
 
 class Smurfs extends Component {
+  constructor() {
+    super();
+    this.state = {
+      smurfs: [],
+    }
+  }
+
+  componentDidMount(){
+    axios.get('http://localhost:3333/smurfs')
+      .then(response => {
+        this.setState({smurfs: response.data})
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(`Error fetching Smurfs: ${error}`);
+       console.log(error);
+    });
+  }
+
+
+
   render() {
     return (
       <div className="Smurfs">
         <h1>Smurf Village</h1>
         <ul>
-          {this.props.smurfs.map(smurf => {
+          {this.state.smurfs.map(smurf => {
             return (
               <Smurf
                 name={smurf.name}
