efd081ee5e8ba90790a8ad7cfbd7311c55455731
Justin M <jekm321@gmail.com>
2018-04-13 12:22:51 -0400

    fixed errors, smurf village and smurfs showing

--- village/src/App.js
+++ village/src/App.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      smurfs: []
+    }
+  }
+
+  componentDidMount = () => {
+    this.getSmurfs();
+  }
+
+  getSmurfs = () => {
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(response => {
+        console.log('cdm', response.data)
+        this.setState(() => ({ smurfs: response.data }));
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
@@ -12,7 +36,7 @@ class App extends Component {
     return (
       <div className="App">
         <SmurfForm />
-        <Smurfs />
+        <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
   }
