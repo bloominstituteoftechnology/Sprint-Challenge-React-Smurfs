b66bc16a0b30d7b5d1a076f42557eaa5304657c8
markstez05 <36493764+markstez05@users.noreply.github.com>
2018-04-13 13:21:00 -0500

    finished 75% cant get delete button to function

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import axios from 'axios';
+import Smurfs from './Smurfs';
+
+
+const deleteSmurf = smurfId => {
+  axios
+    .delete(`http://localhost:3333/smurfs/${smurfId}`)
+    .then(response => {
+      this.props.getSmurfs();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
 const Smurf = props => {
+
   return (
-    <div className="Smurf">
+    <div key={Smurf._id} className="Smurf">
       <h3>{props.name}</h3>
       <strong>{props.height} tall</strong>
       <p>{props.age} smurf years old</p>
+      <button onClick={() => this.deleteSmurf(Smurf._id)}> remove smurf </button>
     </div>
   );
 };
