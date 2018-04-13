88b68d820246c78c7e09dc071fbcd709947dfc45
David Alexander Morales <damorales89@gmail.com>
2018-04-13 15:21:02 -0400

    still updating. started from friends template...

--- village/src/App.js
+++ village/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-
+import axios from 'axios';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
@@ -9,13 +9,36 @@ class App extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+  constructor() {
+    super();
+    this.state = {
+      smurfs: [],
+      name: "",
+      age: "",
+      height: ""
+    };
+  }
+  componentDidMount() {
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(res => {
+        this.setState({ smurfs: res.data });
+      })
+      .catch(error => {
+        console.log(`There was an error getting smurfs: ${error}`);
+      });
+  }
+  
   render() {
+    const { smurfs } = this.state;
     return (
       <div className="App">
-        <Switch>
-          <Route exact path='/' components={SmurfForm} />
-          <Route path='/village/' components={Smurfs} />
-        </Switch>
+        {/* <Switch> */}
+          <SmurfForm />
+          <Smurfs smurfs={smurfs} />
+          {/* <Route exact path='/' components={SmurfForm} />
+          <Route path='/village/' components={Smurfs} /> */}
+        {/* </Switch> */}
       </div>
     );
   }
