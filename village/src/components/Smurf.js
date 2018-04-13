92166f43e093e1eb105eb4a5944fbd3d3ec9696c
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-04-13 14:12:54 -0400

    Robert Hogan. Additional progress on project, Not 100% complete.

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './smurf.css';
 
 const Smurf = props => {
   return (
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import '../SmurfForm.css';
 
 class SmurfForm extends Component {
   constructor(props) {
