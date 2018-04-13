0bb508df46c49e7a7ecbff605585d803368e3801
cgoez <goez2001@gmail.com>
2018-04-13 12:39:15 -0700

    Install reactstrap, add card on Smurf.js

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { Card, Button, CardTitle, CardText, Col } from "reactstrap";
 
 const Smurf = props => {
-  return (
-    <div className="Smurf">
-      <h3>{props.name}</h3>
-      <strong>{props.height} tall</strong>
-      <p>{props.age} smurf years old</p>
-    </div>
+  return (    
+      <Col sm={{ size: 6, offset: 3 }}>
+        <Card inverse color="primary">
+          <CardTitle>
+            <h3>{props.name}</h3>
+          </CardTitle>
+          <CardText>
+            <strong>{props.height} tall</strong>
+            <p>{props.age} smurf years old</p>
+          </CardText>
+          <Button color="secondary">Delete Smurf</Button>
+        </Card>
+      </Col>    
   );
 };
 
