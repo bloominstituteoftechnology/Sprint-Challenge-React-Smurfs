7b167f6f28ae937954ea8197bc93694a7b153a49
Yasin Shuman <email@yasin.io>
2018-04-13 10:13:58 -0700

    added axios, react-router-dom dependencies. added constructor and axios get function and passedsmurflist to smurfs component inside app.js

--- village/src/App.js
+++ village/src/App.js
@@ -1,18 +1,43 @@
-import React, { Component } from 'react';
-
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
 
 class App extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+  constructor(props) {
+    super();
+    this.state = {
+      smurfs: []
+    };
+  }
+
+  updateSmurfList() {
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(res => {
+        console.log(res.data);
+        this.setState(() => ({
+          smurfs: res.data
+        }));
+      })
+      .catch(err => {
+        console.error("Error updating smurf list from server", err);
+      });
+  }
+
+  componentDidMount() {
+    this.updateSmurfList();
+  }
+
   render() {
     return (
       <div className="App">
         <SmurfForm />
-        <Smurfs />
+        <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
   }
