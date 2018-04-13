44a45daca8daba79c3a98ee507e4439ff4e4ca2c
Ryan Hamblin <ryanhca@users.noreply.github.com>
2018-04-13 08:46:38 -0600

    updates needed id

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -12,6 +12,7 @@ class Smurfs extends Component {
             return (
               <Smurf
                 name={smurf.name}
+                id={smurf.id}
                 age={smurf.age}
                 height={smurf.height}
                 key={smurf.id}
