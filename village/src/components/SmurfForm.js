4ab6353d93a1ab8cfac5a16cd1ce9e895131b3f0
Devon Bacon <devon.bacon@gmail.com>
2018-04-13 09:16:49 -0700

    Used axios to add form functionality that adds smurf data to local server

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -13,6 +14,20 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
+    const smurf = {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height
+    };
+
+    axios
+      .post(`http://localhost:3333/smurfs`, smurf)
+      .then(smurf => {
+        this.props.updateSmurfs();
+      })
+      .catch(err => {
+        console.log(err);
+      });
 
     this.setState({
       name: '',
