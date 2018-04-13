df1c5f3fc00189db23faeda7813181dc23828ff2
Justin Hong <jhong2009@gmail.com>
2018-04-13 09:35:46 -0700

    in App.js: set up componentDidMount() and Get request in fetchSmurfs()

--- village/src/App.js
+++ village/src/App.js
@@ -1,20 +1,31 @@
-import React, { Component } from 'react';
-
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
 
 // pass down smurfs collection to Smurfs
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      smurfs: [];
-    }
+      smurfs: []
+    };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+  componentDidMount() {
+    this.fetchSmurfs();
+  }
+
+  // Get Request
+  fetchSmurfs() {
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(response => console.log(response))
+      .catch(error => console.error(error));
+  }
 
   render() {
     return (
