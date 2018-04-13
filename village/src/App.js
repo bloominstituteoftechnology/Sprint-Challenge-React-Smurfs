d8941f0f077b540208a005ae9877f93339764341
totsuya <supertatortots@gmail.com>
2018-04-13 13:44:46 -0400

    added constructor to class App in App.js

--- village/src/App.js
+++ village/src/App.js
@@ -8,7 +8,15 @@ class App extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
-  
+  constructor() {
+    super();
+    this.state = {
+      smurfs: [],
+      name: '',
+      age: '',
+      height: '',
+    }
+  }
   render() {
     return (
       <div className="App">
