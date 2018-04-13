2786d28146b16bf7cb300259b7d9642946695a95
Nedu Robert <nedumrobert@gmail.com>
2018-04-13 17:10:44 +0100

    added code to get the smurfs data and passed needed props to each child

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -8,11 +9,34 @@ class App extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+  constructor() {
+    super();
+    this.state = {
+      smurfs: []
+    }
+  }
+
+  componentDidMount() {
+    this.getSmurfs();
+  }
+
+  getSmurfs = () => {
+    axios
+      .get(`http://localhost:3333/smurfs`)
+      .then(res => {
+        console.log(res.data);
+        this.setState({smurfs: res.data})
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs />
+        <SmurfForm getSmurfs={this.getSmurfs}/>
+        <Smurfs smurfs={this.state.smurfs}/>
       </div>
     );
   }
