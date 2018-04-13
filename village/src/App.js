528580b8b6b71e87429654457f56d1b60f7f0b49
Mark Wong <fmarkwong@gmail.com>
2018-04-13 09:45:16 -0700

    delete

--- village/src/App.js
+++ village/src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
     return (
       <div className="App">
         <SmurfForm updateSmurfList={this.updateSmurfList} />
-        <Smurfs {...this.state}/>
+        <Smurfs updateSmurfList={this.updateSmurfList} {...this.state}/>
       </div>
     );
   }
--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Smurf = props => {
-  return (
-    <div className="Smurf">
-      <h3>{props.name}</h3>
-      <strong>{props.height} tall</strong>
-      <p>{props.age} smurf years old</p>
-    </div>
-  );
-};
+class Smurf extends Component {
+  deleteSmurf = e => {
+    axios.delete(`http://localhost:3333/smurfs/${this.props.id}`)
+      .then(response => {
+        this.props.updateSmurfList(response.data);
+      })
+      .catch(error => console.log(error));
+  }
+
+  render() {
+    return (
+      <div className="Smurf">
+        <h3>{this.props.name}</h3>
+        <strong>{this.props.height} tall</strong>
+        <p>{this.props.age} smurf years old</p>
+        <button onClick={this.deleteSmurf}>Delete</button>
+      </div>
+    );
+  }
+}
 
 export default Smurf;
