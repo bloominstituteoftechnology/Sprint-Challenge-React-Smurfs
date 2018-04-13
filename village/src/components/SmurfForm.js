801f4fc15778c7f73969f30b9be9659d06aeef95
imsdhk <jappleleaf@gmail.com>
2018-04-13 14:16:32 -0400

    finished requirements able to get and post will work on put and delete this weekend

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from "axios";
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -10,20 +11,26 @@ class SmurfForm extends Component {
     };
   }
 
-  addSmurf = event => {
-    event.preventDefault();
-    // add code to create the smurf using the api
-
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
-  }
-
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
+
+
+  addSmurf = event => {
+    event.preventDefault();
+    // add code to create the smurf using the api
+    axios.post('http://localhost:3333/smurfs', this.state)
+      .then(response => {
+        this.setState({
+          name: '',
+          age: '',
+          height: ''
+        })
+        {/*this.props.update()*/}
+      })
+      .catch(error => console.log(error))
+  }
+
 
   render() {
     return (
--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -3,11 +3,13 @@ import React, { Component } from 'react';
 import Smurf from './Smurf';
 
 class Smurfs extends Component {
+
   render() {
     return (
       <div className="Smurfs">
         <h1>Smurf Village</h1>
         <ul>
+          {console.log('inside smurfs' ,this.props.smurfs)}
           {this.props.smurfs.map(smurf => {
             return (
               <Smurf
