8a790d83ca18ee10b0015439b6b40e2a550bb127
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-04-13 10:48:12 -0700

    figuring out where to put component did mount

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -4,6 +4,26 @@ import Smurf from './Smurf';
 import axios from 'axios';
 
 class Smurfs extends Component {
+  constructor() {
+    super();
+    this.state = {
+      smurfs: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(response => {
+        this.setState({ smurfs: response.data });
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(`Error fetching Smurfs: ${error}`);
+        console.log(error);
+      });
+  }
+  
   render() {
     return (
       <div className="Smurfs">
