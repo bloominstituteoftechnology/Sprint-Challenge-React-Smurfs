1d5fa9829f20814ad0a4b67fe846aabd5f5c86c3
sadrak105 <sadrak10@gmail.com>
2018-04-13 10:58:46 -0500

    SmurfsComponent

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import Smurf from './Smurf';
 
 class Smurfs extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
+  constructor(props) {
+    super(props);
+    this.state= {
+      smurfs: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:3333/smurfs')
+  }
 
   render() {
     return (
