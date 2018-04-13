54757463b373e6fccac8cbbaaa6f0dcd4fda0322
Nedu Robert <nedumrobert@gmail.com>
2018-04-13 18:27:20 +0100

    passed getSmurfs function as a prop down to Smurf component

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,27 +1,20 @@
 import React, { Component } from 'react';
+import { Container, Row } from 'reactstrap';
 
 import Smurf from './Smurf';
 
 class Smurfs extends Component {
   render() {
-    return (
-      <div className="Smurfs">
+    return <div className="Smurfs">
         <h1>Smurf Village</h1>
-        <ul>
-          {this.props.smurfs.map(smurf => {
-            return (
-              <Smurf
-                name={smurf.name}
-                id={smurf.id}
-                age={smurf.age}
-                height={smurf.height}
-                key={smurf.id}
-              />
-            );
-          })}
-        </ul>
-      </div>
-    );
+        <Container>
+          <Row>
+            {this.props.smurfs.map(smurf => {
+              return <Smurf name={smurf.name} id={smurf.id} age={smurf.age} height={smurf.height} key={smurf.id} getSmurfs={this.props.getSmurfs} sm="12" />;
+            })}
+          </Row>
+        </Container>
+      </div>;
   }
 }
 
