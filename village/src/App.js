7b0063d5c948f1fb95cc8ba25879fb0a4452bdad
totsuya <supertatortots@gmail.com>
2018-04-13 12:58:54 -0400

    added axios for app.js

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
