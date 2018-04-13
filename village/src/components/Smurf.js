2d4631a8d83d5ff422bdf917d4716de40fa3d2ff
cgoez <goez2001@gmail.com>
2018-04-13 13:03:29 -0700

    Add delete smurf functionality

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,21 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import { Card, Button, CardTitle, CardText, Col } from "reactstrap";
+import axios from "axios";
 
-const Smurf = props => {
-  return (    
+class Smurf extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      age: "",
+      height: ""
+    };
+  }
+
+  deleteSmurf = id => {
+    axios
+      .delete(`http://localhost:3333/smurfs/${id}`)
+      .then(response => {
+        // console.log(response);
+        this.props.getSmurfList();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  render() {
+    return (
       <Col sm={{ size: 6, offset: 3 }}>
         <Card inverse color="primary">
           <CardTitle>
-            <h3>{props.name}</h3>
+            <h3>{this.props.name}</h3>
           </CardTitle>
           <CardText>
-            <strong>{props.height} tall</strong>
-            <p>{props.age} smurf years old</p>
+            <strong>{this.props.height} tall</strong>
+            <p>{this.props.age} smurf years old</p>
           </CardText>
-          <Button color="secondary">Delete Smurf</Button>
+          <Button
+            color="secondary"
+            onClick={() => this.deleteSmurf(this.props.id)}
+          >
+            Delete Smurf
+          </Button>
         </Card>
-      </Col>    
-  );
-};
+      </Col>
+    );
+  }
+}
 
 export default Smurf;
--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -27,6 +27,7 @@ class Smurfs extends Component {
                 age={smurf.age}
                 height={smurf.height}
                 key={smurf.id}
+                getSmurfList={this.props.getSmurfList}
               />
             );
           })}
