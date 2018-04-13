3053d54d042210149ef8d2f05953344da1ca074b
faulkenfett11 <36997363+faulkenfett11@users.noreply.github.com>
2018-04-13 12:21:43 -0700

    delete funtional

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Smurf from './Smurf';
 
+
 class Smurfs extends Component {
   constructor() {
     super();
@@ -10,17 +11,32 @@ class Smurfs extends Component {
     }
   }
 
-  componentDidMount(){
-    axios.get('http://localhost:3333/smurfs')
+  componentDidMount() {
+    this.deletingSmurf();
+  }
+
+  deletingSmurf = () => {
+    axios
+      .get("http://localhost:3333/smurfs")
       .then(response => {
         this.setState({smurfs: response.data})
-        console.log(response);
+      console.log(response);
+    })
+    .catch(error => {
+      console.log(`Error fetching Smurfs: ${error}`);
+  });
+};
+
+  deleteSmurf = smurfId => {
+    axios
+      .delete(`http://localhost:3333/smurfs/${smurfId}`)
+      .then(response => {
+        this.deletingSmurf();
       })
-      .catch(error => {
-        console.log(`Error fetching Smurfs: ${error}`);
-       console.log(error);
-    });
-  }
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
 
 
@@ -31,6 +47,7 @@ class Smurfs extends Component {
         <ul>
           {this.state.smurfs.map(smurf => {
             return (
+              <div>
               <Smurf
                 name={smurf.name}
                 id={smurf.id}
@@ -38,6 +55,8 @@ class Smurfs extends Component {
                 height={smurf.height}
                 key={smurf.id}
               />
+              <button onClick={() => this.deleteSmurf(smurf.id)}>Delete</button>
+              </div>
             );
           })}
         </ul>
