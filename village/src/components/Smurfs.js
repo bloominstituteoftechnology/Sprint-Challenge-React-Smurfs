ed3993fde5d7fe8870fb5a92d6e5b57acde76015
Ryan Hamblin <ryanhca@gmail.com>
2018-04-13 08:08:23 -0600

    updates format for where data lives

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -3,15 +3,12 @@ import React, { Component } from 'react';
 import Smurf from './Smurf';
 
 class Smurfs extends Component {
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning.
-  // You'll need to make sure you have the right properties on state for this to work
   render() {
     return (
       <div className="Smurfs">
         <h1>Smurf Village</h1>
         <ul>
-          {this.state.smurfs.map(smurf => {
+          {this.props.smurfs.map(smurf => {
             return (
               <Smurf
                 name={smurf.name}
