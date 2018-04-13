ae34a7290b2cbde11b626c9141252238112e44d7
Harrison Brock <harrison@harrisonbrock.com>
2018-04-13 11:26:26 -0400

    Added Axios
    Smurfs.js:
    Pulled data from the API server and got the smurf to display

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,28 +1,51 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import axios from 'axios'
 
 import Smurf from './Smurf';
 
 class Smurfs extends Component {
-  render() {
-    return (
-      <div className="Smurfs">
-        <h1>Smurf Village</h1>
-        <ul>
-          {this.props.smurfs.map(smurf => {
-            return (
-              <Smurf
-                name={smurf.name}
-                id={smurf.id}
-                age={smurf.age}
-                height={smurf.height}
-                key={smurf.id}
-              />
-            );
-          })}
-        </ul>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            smurfs: []
+        }
+    }
+
+    componentDidMount() {
+        this.getSmurfs();
+    }
+
+    getSmurfs() {
+
+        axios.get('http://localhost:3333/smurfs')
+            .then(response => {
+                console.log(response.data);
+                this.setState({smurfs: response.data});
+            })
+            .catch(error =>{
+                console.log(`Error get Smurfs: ${error}`);
+            })
+    }
+    render() {
+        return (
+            <div className="Smurfs">
+                <h1>Smurf Village</h1>
+                <ul>
+                    {this.state.smurfs.map(smurf => {
+                        return (
+                            <Smurf
+                                name={smurf.name}
+                                id={smurf.id}
+                                age={smurf.age}
+                                height={smurf.height}
+                                key={smurf.id}
+                            />
+                        );
+                    })}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default Smurfs;
