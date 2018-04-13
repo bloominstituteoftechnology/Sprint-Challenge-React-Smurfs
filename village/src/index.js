a40f7c0fc25ec3cf5f53e16e0d460c9448b40faa
Devon Bacon <devon.bacon@gmail.com>
2018-04-13 08:33:22 -0700

    Initial install and answered question in Answers.md

--- village/src/index.js
+++ village/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-rputer-dom';
 
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Rputer>
+    <App />
+  </Rputer>,
+  document.getElementById('root'));
