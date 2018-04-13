f540477f80e7e45b244737a2425e343764c37605
Justin Hong <jhong2009@gmail.com>
2018-04-13 13:29:02 -0700

    in Smurf.js: set up an update form

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -11,8 +11,16 @@ class Smurf extends React.Component {
 			age: ""
 		};
 	}
+	// Event Handlers
+	handleNewInput = event => {
+		this.setState({ [event.target.name]: event.target.value });
+	};
+
 	// console.log("props", props);
 	render() {
+		// console.log("name", this.state.name);
+		// console.log("height", this.state.height);
+		// console.log("age", this.state.age);
 		return (
 			<div className="Smurf">
 				<Link to={`/smurfs/${this.props.id}`}>
@@ -20,6 +28,26 @@ class Smurf extends React.Component {
 				</Link>
 				<strong>{this.props.height} tall</strong>
 				<p>{this.props.age} smurf years old</p>
+				<form>
+					<input
+						type="text"
+						name="name"
+						value={this.state.name}
+						onChange={this.handleNewInput}
+					/>
+					<input
+						type="text"
+						name="height"
+						value={this.state.height}
+						onChange={this.handleNewInput}
+					/>
+					<input
+						type="text"
+						name="age"
+						value={this.state.age}
+						onChange={this.handleNewInput}
+					/>
+				</form>
 			</div>
 		);
 	}
