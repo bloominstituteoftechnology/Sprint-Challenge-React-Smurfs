b9db40d856c12e72c1e3a82a3a0021a50733ca25
Velsu <riftmage50@gmail.com>
2018-04-13 18:08:50 +0200

    Implemented GET and POST, added Routing, fixed errors

--- village/src/App.js
+++ village/src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Smurf from "./components/Smurf";
+import Smurfs from "./components/Smurfs";
 
 class App extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
@@ -10,10 +10,10 @@ class App extends Component {
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
-      <div className="App">
-        <SmurfForm />
-        <Smurfs />
-      </div>
+      <Switch>
+        <Route exact path="/" component={Smurfs} />
+        <Route path="/smurf/:id" component={Smurf} />
+      </Switch>
     );
   }
 }
--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,14 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
+import SmurfForm from "./SmurfForm";
 
-import Smurf from './Smurf';
+import Smurf from "./Smurf";
 
 class Smurfs extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      smurfs: [],
+      name: "",
+      age: "",
+      height: "",
+      toggleForm: false
+    };
+  }
+
+  componentDidMount() {
+    this.getSmurfs();
+  }
+
+  getSmurfs = () => {
+    axios.get("http://localhost:3333/smurfs").then(response => {
+      this.setState({ smurfs: response.data }).catch(err => {
+        console.log(err);
+      });
+    });
+  };
+
   render() {
     return (
       <div className="Smurfs">
         <h1>Smurf Village</h1>
+        <SmurfForm getSmurfs={this.getSmurfs} />
         <ul>
-          {this.props.smurfs.map(smurf => {
+          {this.state.smurfs.map(smurf => {
             return (
               <Smurf
                 name={smurf.name}
