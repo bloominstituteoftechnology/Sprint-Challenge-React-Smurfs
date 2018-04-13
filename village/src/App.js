c96ae33380e91c87b5701478ac2930b5fbd90816
jeffrey <jeffreyneilflynn@gmail.com>
2018-04-13 11:17:10 -0400

    api data is rendering on screen

--- village/src/App.js
+++ village/src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 
 class App extends Component {
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
+  constructor(props) {
+    super(props);
+    this.state = {
+      smurfs: []
+    }
+  }
+
+  componentDidMount() {
+    this.getSmurfs();
+  }
+
+  getSmurfs = () => {
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(response => {this.setState({ smurfs: response.data })})
+      .catch(err => {console.log(err)});
+  }
+
   render() {
     return (
       <div className="App">
         <SmurfForm />
-        <Smurfs />
+        <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
   }
