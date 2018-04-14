96b7356bf8d8c5d6761f711560700d66d84f5532
JohnCoronel <john.m.coronel@gmail.com>
2018-04-14 12:00:46 -0700

    John Coronel Axios Sprint

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -12,7 +13,14 @@ class SmurfForm extends Component {
 
   addSmurf = event => {
     event.preventDefault();
-    // add code to create the smurf using the api
+    axios.post('http://localhost:3333/smurfs',{
+      name:this.state.name,
+      age:this.state.age,
+      height:this.state.height
+    })
+    .then( (response)=>{
+
+    })
 
     this.setState({
       name: '',
