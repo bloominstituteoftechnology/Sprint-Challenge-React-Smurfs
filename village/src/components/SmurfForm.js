bb25711e6bda499f4fe61f5dbfaf3ae7297a8982
unknown <alexisreyes90@gmail.com>
2018-04-14 14:01:36 -0400

    Alexis: SC-Routing-Axios get/ post complete

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -11,14 +12,11 @@ class SmurfForm extends Component {
   }
 
   addSmurf = event => {
+    console.log(this.state)
     event.preventDefault();
-    // add code to create the smurf using the api
-
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
+    axios
+    .post('http://localhost:3333/smurfs' , this.state)
+    .then( this.setState({name: '',age: '',height: ''}))
   }
 
   handleInputChange = e => {
