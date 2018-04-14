9771e598769e86399b90f5da0f904009b0acb4fb
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-04-13 17:21:05 -0700

    Final Save

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -13,7 +13,7 @@ class SmurfForm extends Component {
   }
 
   addSmurf = event => {
-    event.preventDefault();
+    // event.preventDefault();
     // add code to create the smurf using the api
     const newSmurf = {
       name: this.state.name,
