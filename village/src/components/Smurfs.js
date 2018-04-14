1c169eca09f687b960f2f126fc612d592b7460fc
Stephanie <sbowl001@fiu.edu>
2018-04-15 01:08:37 +0800

    get request

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import Smurf from './Smurf';
 
 class Smurfs extends Component {
+  constructor() {
+    super();
+    this.state = {
+      smurfs: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(response => {
+        this.setState({ smurfs: response.data });
+      })
+      .catch(error => {
+        console.log("error", error);
+      });
+  }
   render() {
-    return (
-      <div className="Smurfs">
+    return <div className="Smurfs">
         <h1>Smurf Village</h1>
         <ul>
-          {this.props.smurfs.map(smurf => {
+          {/* {this.state.smurfs.map(smurf => {
+            return <li key={smurf.id} className="smurf">
+                <p> Name= {smurf.name}</p>
+                <p> Age= {smurf.age}</p>
+                <p> Id= {smurf.id}</p>
+                <p> height= {smurf.height}</p>
+              </li>;
+          })} */}
+          {this.state.smurfs.map(smurf => { {/*changed props to state*/}
             return (
               <Smurf
                 name={smurf.name}
@@ -20,8 +44,7 @@ class Smurfs extends Component {
             );
           })}
         </ul>
-      </div>
-    );
+      </div>;
   }
 }
 
