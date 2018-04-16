135796a14a603cdfae4563ab9378762d275cc1ef
odax <nr10@albion.edu>
2018-04-15 17:03:18 -0700

    completed fixing the errors

---
+++ Answers.md
@@ -0,0 +1,15 @@
+client vs server side routing?
+
+Client side routing utilizes local data to create single page elements in order for navigation to be efficient. Server side will load new html elements and styles within new route changes.
+
+What does Crud stand for? What http methods can be mapped to CRUD?
+
+CRUD -
+create - .post
+read - .get
+update - .put
+destroy -  .delete
+
+Three tools we can use to make AJAX?
+AXIOS, Postman, Client/Web-browser
+
--- village/src/App.js
+++ village/src/App.js
@@ -3,19 +3,30 @@ import React, { Component } from 'react';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
-
+import axios from 'axios';
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      smurfs: []
+    };
+  }
+  componentDidMount() {
+    axios.get('http://localhost:3333/smurfs');
+  }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
+    const { smurfs } = this.state;
     return (
       <div className="App">
         <SmurfForm />
-        <Smurfs />
+        <Smurfs smurfs={smurfs}/>
       </div>
-    );
+    ); 
   }
+
 }
 
 export default App;
