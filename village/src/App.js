111fd5ce760f944478ace042cda12351400a7bd0
Lisa Cee <three_nineteen@yahoo.com>
2018-04-21 10:51:08 -0700

    updated code after solution lecture

--- village/src/App.js
+++ village/src/App.js
@@ -8,8 +8,8 @@ class App extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       smurfs: []
     }
@@ -18,17 +18,42 @@ class App extends Component {
     axios 
       .get('http://localhost:3333/smurfs')
       .then((response) => {
-        this.setState(() => ({smurfs: response.data}));
+        this.setState({smurfs: response.data});
       })
       .catch((error) => {
         console.log('Server Error', error);
       });
   }
+  addSmurfToServer = (smurf) => {
+    axios
+      .post('htttp://localhost:3333/smurfs', smurf)
+      .then(response => {
+        this.setState({smurfs: response.data});
+      })
+      .catch(err => console.log(err));
+  }
+  deleteSmurfFromServer = (id) => {
+    axios 
+      .delete(`http://localhost:3333/smurfs/${id}`)
+      .then(response => {
+        // response.data = {SmurfRemoved: {smurfRemoved data}}
+        const removedSmurf = response.data.SmurfRemoved;
+        const smurfsArray = this.state.smurfs.filter((item) => {
+          return item.id !== removedSmurf.id;
+        })
+        this.setState({smurfs: smurfsArray});
+        // get our entire smurfs array from state
+        // iterate over the array finding the deleted smurf
+        //remove smurf from array
+        //set state with new array of smurfs
+      })
+      .catch(err => console.log(err))
+  }
   render() {
     return (
       <div className="App">
-        <Smurfs {...this.state}/>
-        <SmurfForm />
+        <Smurfs smurfs={this.state.smurfs} deleteSmurfFromServer={this.deleteSmurfFromServer}/>
+        <SmurfForm addSmurfToServer={this.addSmurfToServer}/>
       </div>
     );
   }
--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -17,6 +17,7 @@ class Smurfs extends Component {
                 age={smurf.age}
                 height={smurf.height}
                 key={smurf.id}
+                deleteSmurfFromServer={this.props.deleteSmurfFromServer}
               />
             );
           })}
