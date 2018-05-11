1f1552040b9d6cd1792c4203ccb4236c3152f408
maxwashington <mnwashington18@gmail.com>
2018-05-11 12:31:37 -0500

    Minor Formatting

--- village/src/components/Header.js
+++ village/src/components/Header.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
       <h2>Smurfcome to Smurf Village! </h2>
-      <Link to='/smurfs'>Come On In! </Link>
+      <Link to="/smurfs">Come On In! </Link>
     </div>
-  )
-}
- 
+  );
+};
+
 export default Header;
