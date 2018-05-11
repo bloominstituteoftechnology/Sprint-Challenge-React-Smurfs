524f662fc0fa99504e4653529b6f13ae738cebac
JSNavey <jsnavey@gmail.com>
2018-05-11 12:58:02 -0400

    add GET and POST.  Able to add a new smurf

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import { Route } from 'react-router-dom';
+import axios from 'axios';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
@@ -8,16 +9,22 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: [], 
     };
   }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:3333/smurfs`)
+      .then( res => { this.setState({ smurfs: res.data })
+    })
+      .catch( err => console.log(err));
+  }
+
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm smurfs={this.state.smurfs} />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
