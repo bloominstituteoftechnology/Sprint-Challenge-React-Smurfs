c49ee06fc513895a58cb710840016d56bc6e01a8
mister-corn <code@ronaldlibago.com>
2018-05-11 07:32:58 -0700

    T+23min: GET & POST working. Smurf List being rerendered as expected.

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
@@ -12,12 +12,25 @@ class App extends Component {
     };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
+  componentDidMount() {
+    axios.get('http://localhost:3333/smurfs/')
+      .then(response=> {
+        console.log(response.data);
+        return response;
+      })
+      .then(response => this.setState({ smurfs : response.data }))
+      .catch(error => console.log(error));
+  }
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+  updateState = (data) => {
+    this.setState({ smurfs: data });
+  }
+
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm update={this.updateState} />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
