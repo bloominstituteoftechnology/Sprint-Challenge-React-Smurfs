d64a97963cf438572c10b6bfb029e78836619868
Brandon Hopper <BrandonHopper0@gmail.com>
2018-05-11 13:01:14 -0400

    -added to state keypair values -composed CDM method to Axios API to render state

--- village/src/App.js
+++ village/src/App.js
@@ -9,11 +9,27 @@ class App extends Component {
     super(props);
     this.state = {
       smurfs: [],
+      name: '',
+      age: '',
+      height: ''
     };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+
+componentDidMount() {
+  axios
+    .get('http://localhost:3333/smurfs')
+    .then(response => {
+      this.setState(() => ({ smurfs: [...response.data] }));
+    })
+    .catch(err => {
+      console.error("error:", err);
+    });
+}
+
+
   render() {
     return (
       <div className="App">
