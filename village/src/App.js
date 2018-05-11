385317c006cd348b841b78e53393c54676a8e9ac
masontmorrow <masontmorrow@gmail.com>
2018-05-11 18:28:25 +0200

    App.js & new file index.js
    1. App.js - added axios http methods
    2. Added index.js file to /components for easy import into App.js

--- village/src/App.js
+++ village/src/App.js
@@ -1,15 +1,48 @@
 import React, { Component } from 'react';
-
 import './App.css';
-import SmurfForm from './components/SmurfForm';
-import SmurfList from './components/SmurfList';
+import { SmurfList, SmurfForm } from "./components";
+import axios from "axios";
+import { Route, Link } from 'react-router-dom';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
+  }
+
+  componentDidMount = () => {
+    axios.get("http://localhost:3333/smurfs")
+      .then(res => {
+        const smurfs = res.data;
+        this.setState({ smurfs });
+      })
+      .catch(err => console.log(err))
+  }
+  handleAddSmurf = (newSmurf) => {
+    axios.post("http://localhost:3333/smurfs", newSmurf)
+    .then(res => {
+      const smurfs = res.data;
+      this.setState({ smurfs });
+    })
+    .catch(err => console.log(err))
+  }
+  handleUpdateSmurf = (id, updatedSmurf) => {
+    axios.put(`http://localhost:3333/smurfs/${id}`, updatedSmurf)
+    .then(res => {
+      const smurfs = res.data;
+      this.setState({ smurfs });
+    })
+    .catch(err => console.log(err))
+  }
+  handleDeleteSmurf = (id) => {
+    axios.delete(`http://localhost:3333/smurfs/${id}`)
+      .then(res => {
+        const smurfs = res.data;
+        this.setState({ smurfs });
+      })
+      .catch(err => console.log(err))
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
---
+++ village/src/components/index.js
@@ -0,0 +1,2 @@
+export {default as SmurfForm} from "../components/SmurfForm.js"; 
+export {default as SmurfList} from "../components/SmurfList.js";
