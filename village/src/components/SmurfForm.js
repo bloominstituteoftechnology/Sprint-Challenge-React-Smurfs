30206255f515058718ae85cd63b6bb00820e32ee
jesuarva <jean.ariza@jesuarva.com>
2018-05-11 19:08:46 +0200

    REFACTOR SmurfForm.handleImputChange Event listener, now there is only one event handler.

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -32,22 +32,21 @@ class SmurfForm extends Component {
 
   render() {
     return (
-      <div className="SmurfForm">
+      <div
+        onChange={this.handleInputChange}
+        className="SmurfForm">
         <form onSubmit={this.addSmurf}>
           <input
-            onChange={this.handleInputChange}
             placeholder="name"
             value={this.state.name}
             name="name"
           />
           <input
-            onChange={this.handleInputChange}
             placeholder="age"
             value={this.state.age}
             name="age"
           />
           <input
-            onChange={this.handleInputChange}
             placeholder="height"
             value={this.state.height}
             name="height"
