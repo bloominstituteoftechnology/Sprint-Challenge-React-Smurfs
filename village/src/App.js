0bcd214c8ff08b2da527d1856cb836acd021b901
jesuarva <jean.ariza@jesuarva.com>
2018-05-11 18:17:18 +0200

    ADD http GET succesfully.

--- village/src/App.js
+++ village/src/App.js
@@ -1,8 +1,16 @@
-import React, { Component } from 'react';
+/**
+ * STEPS:
+ * Add: GET, POST and PUT request.
+ * Lookup: for an API that delivers a JSON list with snurfs.
+ * SETUP: Router, Routes and Links.
+ */
 
+
+import React, { Component } from 'react';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
@@ -14,6 +22,17 @@ class App extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  componentDidMount() {
+    axios.get('http://localhost:3333/smurfs')
+    .then( res => {console.log(res.data); return res.data} )
+    .then( data => {
+      this.setState({ smurfs: data });
+    } )
+    .catch( e => console.log(e) );
+  }
+  
+
   render() {
     return (
       <div className="App">
@@ -25,4 +44,3 @@ class App extends Component {
 }
 
 export default App;
- 
