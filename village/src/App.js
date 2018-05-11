9bc9bfe794b48eb09bd7f4f43b7a48eb03e2ee50
Thuy Pham <tpham045@hotmail.com>
2018-05-11 09:20:00 -0700

    feat: add smurf

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -11,13 +12,28 @@ class App extends Component {
       smurfs: [],
     };
   }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
+  componentDidMount = () => {
+    axios.get('http://localhost:3333/smurfs')
+    .then(res=>
+      this.setState({ smurfs : res.data })
+    )
+    .catch(err=>
+      console.log(err) //to-do: display to user message
+    )
+  }
+  refreshSmurf = () => {
+    axios.get('http://localhost:3333/smurfs')
+    .then(res=>
+      this.setState({ smurfs : res.data })
+    )
+    .catch(err=>
+      console.log(err) //to-do: display to user message
+    )
+  }
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm refreshSmurf={this.refreshSmurf}/>
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
