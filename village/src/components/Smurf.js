f976a575a466a0e193ae5ff98084498484bf29d7
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-05-11 13:24:54 -0700

    implement put functionality

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -21,23 +21,39 @@ class Smurf extends Component {
     this.fetchSmurf(id);
   }
 
+  handleInputChange = (e) => {
+    this.setState({[e.target.name]:e.target.value})
+  }
+
   fetchSmurf = (id) => {
     console.log(`fetching smurf with id=${id}`)
     axios
       .get(`http://localhost:3333/smurfs/${id}`)
       .then((res) => {
-        this.setState(() => ({smurf: res.data}))
+        this.setState(() => ({smurf: res.data, name:res.data.name, age:res.data.age, height:res.data.height}))
       })
       .catch(err => console.log(err.name))
     console.log(this.state)
   }
 
+  updateSmurf = () => {
+    const id = this.state.smurf.id;
+    const updatedSmurf = {
+      name: this.state.name, 
+      age: this.state.age, 
+      height: this.state.height}
+    axios
+      .put(`http://localhost:3333/smurfs/${id}`, updatedSmurf)
+      .then(() => {this.setState({name:'',age:null,height:null})})
+      .catch(err => console.log(err))
+  }
+
   deleteSmurf = () => {
     const id = this.state.smurf.id;
     axios
-        .delete(`http://localhost:3333/smurfs/${id}`)
-        .then(resp => this.setState({deleted: true}))
-        .catch(err => console.log(err))
+      .delete(`http://localhost:3333/smurfs/${id}`)
+      .then(resp => this.setState({deleted: true}))
+      .catch(err => console.log(err))
   }
 
   render() {
@@ -53,6 +69,27 @@ class Smurf extends Component {
           />
           <div className="update-delete">
             <h2>Update Smurf information</h2>
+            <form className="smurf-form">
+              <input
+                onChange={this.handleInputChange}
+                placeholder="name"
+                value={this.state.name}
+                name="name"
+              />
+              <input
+                onChange={this.handleInputChange}
+                placeholder="age"
+                value={this.state.age}
+                name="age"
+              />
+              <input
+                onChange={this.handleInputChange}
+                placeholder="height"
+                value={this.state.height}
+                name="height"
+              />
+              <button onClick={this.updateSmurf} className="update btn">Update</button>
+            </form>
             <button onClick={this.deleteSmurf} className="delete btn">Delete</button>
           </div>
         </div>
