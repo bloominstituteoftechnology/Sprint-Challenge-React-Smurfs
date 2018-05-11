1a22a72d97167f9af50c80fb8b32b9695b2fa68f
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-05-12 04:25:40 +0800

    Updated files

--- village/src/components/Home.js
+++ village/src/components/Home.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Home = () => {
-    return <h1>Welcome to the Village!</h1>
+    return (
+    <h1>Welcome, Dear Guests, to the Smurf's Village!</h1>
+    
+) 
+
 }
  
 export default Home;
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,43 +1,60 @@
 import React, { Component } from 'react';
+// import SmurfForm from './components/SmurfForm';
+import Smurfs from './components/Smurfs';
+import Header from './components/Header';
 import axios from 'axios';
+import './SmurfForm.css';
+
 
 class SmurfForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      smurfs: [],
       name: '',
       age: '',
-      height: ''
+      height: '',
+      id: ''
     };
   }
 
-  // componentDidMount() {
-  //   this.addSmurf()
-  // }
+  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
+  // Notice what your map function is looping over and returning inside of Smurfs.
+  // You'll need to make sure you have the right properties on state and pass them down to props.
 
-  // addSmurf = event => {
-  //   // event.preventDefault();
-  //   axios.get('http://localhost:3333/smurfs')
-  //     .then(response => this.setState({ smurfs: response.data }))
-  //     .catch(err => console.log(err))
-  // }
+  componentDidMount() {
+    this.addSmurfs()
+  }
 
-  handleInputChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
+  addSmurfs = () => {
+   axios.get('http://localhost:3333/smurfs')
+    .then(response => this.setState({ smurfs: response.data }))
+    .catch(err => console.log(err)) 
+  }
+
+  handleInputChange = (event) => { this.setState({[event.target.name]: event.target.value}) }
 
   buttonSubmit = () => {
-    const { name, age, height } = this.state
-    axios.post('http://localhost:3333/smurfs', { name, age, height })
+    const { id, name, age, height } = this.state
+    axios.post('http://localhost:3333/smurfs', { id, name, age, height })
       .then( (response) => {
-        this.setState({ smurfs: response.data, name: '', age: '', height: ''})
+        this.setState({ smurfs: response.data, id: '', name: '', age: '', height: '' })
       })
   }
 
   render() {
     return (
-      <div className="SmurfForm">
+      <div className="App">
+        <Header />
+        <br />
         <form onSubmit={this.addSmurf}>
+          <input
+            type="number"
+            onChange={this.handleInputChange}
+            placeholder="Enter id"
+            value={this.state.id}
+            name="id"
+          />
           <input
             type="text"
             onChange={this.handleInputChange}
@@ -61,6 +78,7 @@ class SmurfForm extends Component {
           />
           <button type="submit" onClick={this.buttonSubmit}>Add to the village</button>
         </form>
+
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
