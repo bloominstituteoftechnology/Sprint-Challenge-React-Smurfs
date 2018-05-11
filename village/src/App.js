62e689490fd3e04082ff3a4503dd811dee3c27d6
Lavell Burton <taylormade800@gmail.com>
2018-05-11 08:44:39 -0700

    added the header to the app

--- village/src/App.js
+++ village/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 import './App.css';
+import Header from './components/Header';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 
@@ -21,11 +22,51 @@ class App extends Component {
       .then(response => this.setState({smurfs: response.data}))
   }
 
+  gatherData() {
+    axios.get('http://localhost:5000/smurfs')
+      .then(response => this.setState({smurfs: response.data}))
+  }
+
+  // updtaeData() {
+  //   this.setState({ smurfs: smurfs }) 
+  // }
+
+  handleInput(e) {
+    this.setState({ [e.target.name]: e.target.value }, { [e.target.age]: e.target.value }, { [e.target.height]: e.target.value })
+  }
+
+  // handleInput(e) {
+  //   this.setState({ [e.target.age]: e.target.value })
+  // }
+
+  // handleInput(e) {
+  //   this.setState({ [e.target.height]: e.target.value })
+  // }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    let newSmurf = {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height
+    }
+    axios.post('http://localhost:5000/smurfs', newSmurf)
+      .then(response => {
+        let newSmurfArray = response.data.slice();
+        this.setState({
+          smurfs: newSmurfArray,
+          name: "",
+          age: 0,
+          height: ""
+        });
+      });
+  }
 
 
   render() {
     return (
       <div className="App">
+      <Header />  
         <SmurfForm />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
