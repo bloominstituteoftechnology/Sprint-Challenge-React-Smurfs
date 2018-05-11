fe5841bd6358dc2eefe236b2a0cf9fd6f1803df2
maxwashington <mnwashington18@gmail.com>
2018-05-11 10:52:40 -0500

    -Mapping over name, height, and age for each smurf to display them -Deleted Smurf.defaultProps

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Smurf = props => {
   return (
     <div className="Smurf">
-      <h3>{props.name}</h3>
-      <strong>{props.height} tall</strong>
-      <p>{props.age} smurf years old</p>
+      {props.smurf.map(x => {
+        return (
+          <div>
+            <h3>{x.name}</h3>
+            <strong>{x.height} tall</strong>
+            <p>{x.age} smurf years old</p>
+            <Link></Link>
+            <Link></Link>
+          </div>
+        );
+      })}
     </div>
   );
 };
 
-Smurf.defaultProps = {
-  name: '',
-  height: '',
-  age: ''
-};
 
 export default Smurf;
-
