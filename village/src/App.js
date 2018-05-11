b75500a2d42e4c9791b31958aeafc160e18c82b2
Ryan Hamblin <ryanhca@gmail.com>
2018-05-10 22:50:17 -0700

    adds default props, changes direction

--- village/src/App.js
+++ village/src/App.js
@@ -5,6 +5,12 @@ import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      smurfs: [],
+    };
+  }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
@@ -12,7 +18,7 @@ class App extends Component {
     return (
       <div className="App">
         <SmurfForm />
-        <Smurfs />
+        <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
   }
