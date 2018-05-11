7fe6c0200a64788dc42812ee53f83449cc2ae75b
Bradiowave <divebombwalker@gmail.com>
2018-05-11 09:16:55 -0700

    added routes and link to navigate pages

--- village/src/App.js
+++ village/src/App.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import Header from './components/Header';
+import { Route, Link } from 'react-router-dom';
 
 class App extends Component {
   constructor(props) {
@@ -25,8 +27,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm smurfs={this.state.smurfs}/>
-        <Smurfs smurfs={this.state.smurfs} />
+        <Route exact path='/' component={Header} />
+        <Route path='/smurfs' component={SmurfForm} />
+        <Route path='/smurfs' render={props => (<Smurfs {...props} smurfs={this.state.smurfs} />)} />
       </div>
     );
   }
