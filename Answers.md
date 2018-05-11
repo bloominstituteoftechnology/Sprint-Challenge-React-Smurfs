0c12c8544b2ebebb3cb78ae98951e1a04d988454
april <martinezapril56@yahoo.com>
2018-05-11 13:44:41 -0400

    COMMIT

--- Answers.md
+++ Answers.md
@@ -1,5 +1,13 @@
 1.  Explain the differences between `client-side routing` and `server-side routing`.
+client-side routing is When browsing, the adjustment of a URL can make a lot of things happen. This will happen regularly by clicking on a link.
+server-side routing is when the route is handled internally by the JavaScript that is loaded on the page.It is important to note that the whole page won’t refresh when using client-side routing. There are just some elements inside the application that will change.
+
 1.  What does HTTP stand for?
+Hyper Text Transfer Protocol
 1.  What does CRUD stand for?
+Create Read Update Delete
+
 1.  Which HTTP methods can be mapped to the CRUD acronym that we use when interfacing with APIs/Servers.
+PUT,POST,GET,DELETE
 1.  Mention three tools we can use to make AJAX requests
+GET,FETCH,JQUERY
--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -2,31 +2,30 @@ import React, { Component } from 'react';
 
 import Smurf from './Smurf';
 
-class Smurfs extends Component {
-  render() {
-    return (
-      <div className="Smurfs">
-        <h1>Smurf Village</h1>
-        <ul>
-          {this.props.smurfs.map(smurf => {
-            return (
-              <Smurf
-                name={smurf.name}
-                id={smurf.id}
-                age={smurf.age}
-                height={smurf.height}
-                key={smurf.id}
-              />
-            );
-          })}
-        </ul>
-      </div>
-    );
-  }
+
+const Smurfs = props => {
+
+  return (
+    <div className="Smurfs">
+      <h1>Smurf Village</h1>
+      <ul>
+        {props.smurfs.map(smurf => {
+          return (
+            <Smurf
+              name={smurf.name}
+              id={smurf.id}
+              age={smurf.age}
+              height={smurf.height}
+              key={smurf.id}
+              deleteSmurf={props.deleteSmurf}
+            />
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
-Smurf.defaultProps = {
- smurfs: [],
-};
+
 
 export default Smurfs;
