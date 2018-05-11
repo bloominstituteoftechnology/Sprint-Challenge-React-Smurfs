a7ee0f62ae424e8535e303916366b96cad63e37a
Xang Thao <xangthao24@yahoo.com>
2018-05-11 09:23:19 -0700

    added get and post

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from "axios";
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -13,12 +14,21 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
-
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
+    let newSmurf = {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height
+    };
+    axios.post("http://localhost:3333/smurfs", newSmurf)
+      .then(response => {
+        let newArray = response.data.slice();
+        this.setState({
+          smurfs: newArray,
+          name: "",
+          age: "",
+          height: ""
+        }); })
+      .catch(err => console.log(err));
   }
 
   handleInputChange = e => {
@@ -34,18 +44,24 @@ class SmurfForm extends Component {
             placeholder="name"
             value={this.state.name}
             name="name"
+            type="text"
+            required
           />
           <input
             onChange={this.handleInputChange}
             placeholder="age"
             value={this.state.age}
             name="age"
+            type="text"
+            required
           />
           <input
             onChange={this.handleInputChange}
             placeholder="height"
             value={this.state.height}
             name="height"
+            type="text"
+            required
           />
           <button type="submit">Add to the village</button>
         </form>
