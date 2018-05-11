392b55f4d4f52dde3895ebe516ae3c4b055b869d
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-05-11 11:20:52 -0700

    implement delete functionality

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import SmurfCard from './SmurfCard';
+import { Redirect } from 'react-router-dom';
 
 class Smurf extends Component {
   constructor(props) {
@@ -23,36 +24,39 @@ class Smurf extends Component {
   fetchSmurf = (id) => {
     console.log(`fetching smurf with id=${id}`)
     axios
-      .get(`http://localhost:3333/smurfs/${id}`, id)
+      .get(`http://localhost:3333/smurfs/${id}`)
       .then((res) => {
         this.setState(() => ({smurf: res.data}))
       })
-      .catch(err => console.log(err))
+      .catch(err => console.log(err.name))
     console.log(this.state)
   }
 
   deleteSmurf = () => {
-    const id = 1;
+    const id = this.state.smurf.id;
     axios
         .delete(`http://localhost:3333/smurfs/${id}`)
         .then(resp => this.setState({deleted: true}))
         .catch(err => console.log(err))
   }
 
-  render() { 
-    return (
-      <div className="single-smurf">
-        <SmurfCard 
-          name={this.state.smurf.name}
-          age={this.state.smurf.age}
-          height={this.state.smurf.height}
-          id={this.state.smurf.id}
-        />
-        <div className="update-delete">
-          <h2>Update Smurf information</h2>
-          <button onClick={this.deleteSmurf}>Delete</button>
+  render() {
+    return (this.state.deleted ? (
+        <Redirect to="/smurfs"/>
+      ) : (
+        <div className="single-smurf">
+          <SmurfCard 
+            name={this.state.smurf.name}
+            age={this.state.smurf.age}
+            height={this.state.smurf.height}
+            id={this.state.smurf.id}
+          />
+          <div className="update-delete">
+            <h2>Update Smurf information</h2>
+            <button onClick={this.deleteSmurf}>Delete</button>
+          </div>
         </div>
-      </div>
+      )
     )
   }
 }
