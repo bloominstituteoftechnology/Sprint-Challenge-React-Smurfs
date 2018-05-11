929e230739f9a5653cb70f50420ed1759b323f3a
Bee C <b.choeung@gmail.com>
2018-05-11 14:18:42 -0400

    sprint chalenge

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { react } from 'react-router-dom';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -11,6 +13,28 @@ class App extends Component {
       smurfs: [],
     };
   }
+
+  componentDidMount() {
+    axios.get('http://localhost:3333/smurfs')
+    .then(response => {
+      this.setState({notes: response.data});
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+
+  saveNoteData = () => {
+    const note = { age: this.state.age, email: this.state.email, name: this.state.name };
+    axios.post('http://localhost:3333/smurfs', note)
+    .then(savedNote => {
+      console.log(savedNote);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+    this.setState({age: '', email: '', name: ''});
+  };
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
