06566aa864cd48737c7e6e66d19d3549c5c72684
Zach Campbell <beazlebubbles@gmail.com>
2018-05-11 10:28:54 -0600

    Added greeting and enter button to header

--- village/src/components/Header.js
+++ village/src/components/Header.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return <h2>I AM A HEADER</h2>
+  return (
+    <div>
+      <h2>Welcome to Smurf Village!</h2>
+      <Link to="/smurfs"><button>Enter!</button></Link>
+    </div>
+  )
 }
 
 export default Header;
