6138bb6125a6c0e019d73f3aebed5f10f1750346
Anne Courtney <annecourtney_1@yahoo.com>
2018-05-11 12:37:21 -0500

    Form posts new smurfs to DB

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class SmurfForm extends Component {
   constructor(props) {
