05f93988b08f8275fd089ad26896fffd3bdcb6b5
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-05-11 14:26:41 -0500

    added style to home page

--- village/src/Home.js
+++ village/src/Home.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
-import App from './App'
+import App from './App';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-        <h1>Welcome to Smurf Village</h1>
-        <Link to="/smurfs">Enter Village</Link>
+        <div className="home-container">
+        <h1 style={homeStyles}>Welcome to Smurf Village</h1>
+        {/* <button style={homeBtn}><Link style={homeBtn} to="/smurfs">Enter Village</Link></button> */}
+        <Link to="/smurfs" ><button style={homeBtn}>Enter Village</button></Link>
+        <img className="homeImage" src="http://www.liketotally80s.com/wp-content/uploads/2014/08/smurfs-characters.png" alt="The Smurfs"/>
         </div>
     )
 }
+
+const homeStyles = {
+    fontFamiy: "Titan-One", 
+    color: "#4286f4",
+    fontSize: "80px"
+}
+
+const homeBtn = {
+    textDecoration: "none",
+    fontSize: "60px", 
+    borderRadius: "20px",
+    backgroundColor:" #b899e5",
+    color: "white",
+}
+
 
 export default Home;
