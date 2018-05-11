28aa0388605a706916dd0a9d904f153d3f7ba319
Bee C <b.choeung@gmail.com>
2018-05-11 17:33:07 -0400

    added et and post requests

--- village/src/App.js
+++ village/src/App.js
@@ -5,6 +5,7 @@ import { react } from 'react-router-dom';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+const URL = 'http://localhost:3333/smurfs';
 
 class App extends Component {
   constructor(props) {
@@ -17,21 +18,34 @@ class App extends Component {
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   componentDidMount() {
-    const myPromise = axios.get('http://localhost:3333/smurfs');
+    const myPromise = axios.get(URL);
     // pending state - we're getting the data..
     // resolved state - we have the data or a success..
     // rejected state - something went wrong with the data.
-    myPromise.then(response => {
-      console.log(response);
+    myPromise
+      .then(response => {
+        this.setState({ smurfs: response.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  addSmurf = smurf => {
+    const myPromise = axios.post(URL, smurf);
+    myPromise
+    .then(response => {
+      this.setState({ smurfs: response.data });
     })
     .catch(err => {
-      console.log(err);
-    })
+      console.logerror(err);
+    });
   }
+
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm addSmurf={this.addSmurf} />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
