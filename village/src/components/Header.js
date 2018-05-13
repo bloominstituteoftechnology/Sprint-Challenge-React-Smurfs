d0ec991cdd818471c210b35be97874d8ac4c86e9
Matthias Bakken <matthias.bakken@gmail.com>
2018-05-13 22:11:23 +0700

    added more styling to header and smurf cards

--- village/src/components/Header.js
+++ village/src/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
   return (
-    <div>
+    <div className="Header">
       <h2>Welcome to Smurf Village!</h2>
-      <Link to="/smurfs">Enter the Village</Link>
+      <img src="http://www.impulsegamer.com/articles/wp-content/uploads/2015/07/smurfs-620x350.jpg" />
+      <Link to="/smurfs" className="link">Enter the Village</Link>
     </div>
   )
 }
