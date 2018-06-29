a2d57f8f1c9b66dc50173f81e4a375ebfea5b1c7
Kamry Bowman <kamry.bowman@gmail.com>
2018-06-29 09:53:24 -0600

    Add Get and Post functionality

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -7,17 +8,47 @@ import Smurfs from './components/Smurfs';
 class App extends Component {
   constructor(props) {
     super(props);
+    this.url = 'http://localhost:3333/smurfs';
     this.state = {
       smurfs: [],
     };
   }
+
+  componentDidMount() {
+    const context = this;
+    axios
+      .get(this.url)
+      .then((response) => {
+        context.setState({
+          smurfs: response.data,
+        });
+      })
+      .catch((response) => {
+        console.log('Failed ', response.data);
+      });
+  }
+
+  addSmurf = (smurf) => {
+    const context = this;
+    axios
+      .post(this.url, smurf)
+      .then((response) => {
+        context.setState({
+          smurfs: response.data,
+        });
+      })
+      .catch((response) => {
+        console.log('Failed ', response.data);
+      });
+  }
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm addSmurf={this.addSmurf} />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,32 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Smurf from './Smurf';
 
-class Smurfs extends Component {
-  render() {
-    return (
-      <div className="Smurfs">
-        <h1>Smurf Village</h1>
-        <ul>
-          {this.props.smurfs.map(smurf => {
-            return (
-              <Smurf
-                name={smurf.name}
-                id={smurf.id}
-                age={smurf.age}
-                height={smurf.height}
-                key={smurf.id}
-              />
-            );
-          })}
-        </ul>
-      </div>
-    );
-  }
+function Smurfs(props) {
+  return (
+    <div className="Smurfs">
+      <h1>Smurf Village</h1>
+      <ul>
+        {props.smurfs.map(smurf => (
+            <Smurf
+              name={smurf.name}
+              id={smurf.id}
+              age={smurf.age}
+              height={smurf.height}
+              key={smurf.id}
+            />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 Smurf.defaultProps = {
- smurfs: [],
+  smurfs: [],
 };
 
 export default Smurfs;
