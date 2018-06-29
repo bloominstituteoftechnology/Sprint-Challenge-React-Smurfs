c458fa122cab2387d32bf2c3ed01d1033ed6c7d8
CLN <cln@Sags-MacBook-Pro.local>
2018-06-29 13:50:59 -0400

    Added home button to village,.

--- village/src/components/Header.js
+++ village/src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return( 
     <div>
   <h2>You are a welcomed guest in the smurf village.</h2>
-  {/* <Link className="village-button" to={"/"}>
+  {/* <Link className="home-button" to={"/"}>
   <button>Home</button></Link> */}
   <Link className="village-button" to={"/smurfs"}>
   <button>Village Entrance</button></Link>
