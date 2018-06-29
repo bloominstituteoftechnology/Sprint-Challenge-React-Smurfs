8f766baaea3d52797ad88c382bf3e09b2cf467a3
Brad Traversy <mercado7946@gmail.com>
2018-06-29 10:41:52 -0700

    mvp

--- village/src/App.js
+++ village/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import Header from './components/Header';
+import { Route } from "react-router-dom";
 
 class App extends Component {
   constructor(props) {
@@ -14,11 +16,25 @@ class App extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+  componentDidMount() {
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(response => {
+        console.log('response data', response.data);
+        this.setState({
+          smurfs: response.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
+    console.log('state', this.state.smurfs);
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Route exact path="/" component={Header} />
+        <Route path="/smurfs" component={SmurfForm} />
+        <Route path="/smurfs" render={() => <Smurfs smurfs={this.state.smurfs} />} />
       </div>
     );
   }
