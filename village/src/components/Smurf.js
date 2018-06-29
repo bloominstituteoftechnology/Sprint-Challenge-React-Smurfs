54438c1285b83e1d027d1240bc6f54b6bb6f74c2
Nicky Chen <nicky.chen0@gmail.com>
2018-06-29 10:15:05 -0700

    stretch completed

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,14 +1,38 @@
 import React from "react";
+import axios from "axios";
 
 class Smurf extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      editState: false,
       editedName: "",
       editedHeight: "",
       editedAge: ""
     };
   }
+
+  toggleSmurf = () => {
+    this.setState({ editState: !this.state.editState });
+  };
+  editSmurf = event => {
+    this.setState({ [event.target.name]: event.target.value });
+    console.log("editedName: ", this.state.editedName);
+    console.log("editedAge: ", this.state.editedAge);
+  };
+  saveEditSmurf = () => {
+    const smurf = {
+      name: this.state.editedName,
+      height: this.state.editedHeight,
+      age: this.state.editedAge
+    };
+    axios
+      .put(`http://localhost:3333/smurfs/${this.props.id}`, smurf)
+      .then(response => {
+        this.setState({ editedName: "" });
+      })
+      .catch(error => console.log(error));
+  };
 
   render() {
     return (
@@ -16,14 +40,34 @@ class Smurf extends React.Component {
         <h3>{this.props.name}</h3>
         <strong>{this.props.height} tall</strong>
         <p>{this.props.age} smurf years old</p>
+
+        {this.state.editState ? (
+          <form>
+            <input
+              placeholder="edit name"
+              type="text"
+              onChange={this.editSmurf}
+              name="editedName"
+            />
+            <input
+              placeholder="edit height"
+              type="text"
+              onChange={this.editSmurf}
+              name="editedHeight"
+            />
+            <input
+              placeholder="edit age"
+              type="text"
+              onChange={this.editSmurf}
+              name="editedAge"
+            />
+            <button onClick={this.saveEditSmurf}>Submit Changes</button>
+          </form>
+        ) : null}
         <button onClick={() => this.props.deleteSmurf(this.props.id)}>
           Delete Smurf
         </button>
-        <form>
-          <input placeholder="edit name" />
-          <input placeholder="edit height" />
-          <input placeholder="edit age" />
-        </form>
+        <button onClick={this.toggleSmurf}>Edit Smurf</button>
       </div>
     );
   }
