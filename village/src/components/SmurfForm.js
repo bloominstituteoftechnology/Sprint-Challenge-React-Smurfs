14b4ef90d3d0d50f58656e3853b786125201f8d1
Mark Hermansen <markwilliams.aus@gmail.com>
2018-06-29 10:35:09 -0500

    completed GET and POST functionality

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -13,12 +14,21 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
-
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
+    const smurf = {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height,
+    }
+    axios
+      .post(this.props.URL, smurf)
+      .then(
+        this.setState({
+        name: '',
+        age: '',
+        height: ''
+      }))
+      .catch(err => console.log(err))
+      window.location.reload();
   }
 
   handleInputChange = e => {
