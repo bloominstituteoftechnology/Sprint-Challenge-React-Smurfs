6ca48dcf80bf0499eb6b4742ada3bff0c84019fa
chainchompa <chainchompa@gmail.com>
2018-06-29 12:16:02 -0400

    added home page

---
+++ village/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="homepage">
+      <h1>Welcome to the Smurf Village</h1>
+      <h3>Use the navigation bar up top to travel though our village</h3>
+    </div>
+  );
+};
+
+export default Home;
