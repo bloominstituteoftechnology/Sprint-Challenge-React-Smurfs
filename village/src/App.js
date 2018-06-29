35eebc6c50142e405c2d89146739166076ed17b2
CLN <cln@Sags-MacBook-Pro.local>
2018-06-29 12:53:03 -0400

    I have smurfs adding Router wrapping app and a home button link so far, .

--- village/src/App.js
+++ village/src/App.js
@@ -1,24 +1,49 @@
 import React, { Component } from 'react';
-
+import axios from "axios";
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import Header from './components/Header';
+import {  Route,  } from "react-router-dom";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       smurfs: [],
+      smurf: ''
     };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+  componentDidMount() {
+    axios
+    .get('http://localhost:3333/smurfs')
+    .then(response => {
+      console.log("Get Response",response);
+      this.setState({smurfs: response.data });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+  
+  
+  handleSmurfData = data => {
+    this.setState({ smurf: data });
+  };
+
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+      <Header />
+        <SmurfForm 
+        handleSmurfData={this.handleSmurfData}
+        />
+        <Smurfs smurfs={this.state.smurfs}
+        handleSmurfData={this.handleSmurfData} />
+        {/* <Route exact path= "/" component={SmurfForm}/> */}
       </div>
     );
   }
--- village/src/components/Header.js
+++ village/src/components/Header.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Link,  } from "react-router-dom";
 
 const Header = () => {
-  return <h2>Describe your header here.</h2>
+  return( 
+    <div>
+  <h2>You are a welcome guest in the smurf village.</h2>
+  
+  <Link className="home-button" to={"/"}className="home-button">Home</Link>
+  </div>
+  );
 }
  
 export default Header;
