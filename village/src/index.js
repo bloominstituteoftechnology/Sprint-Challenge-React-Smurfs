1542bec19200045b8fc49d3c26d597a8605dbc41
Kevin <brackkevin@gmail.com>
2018-06-29 11:13:20 -0500

    Fixed react router import to BrowserRouter

--- village/src/index.js
+++ village/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ReactRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
