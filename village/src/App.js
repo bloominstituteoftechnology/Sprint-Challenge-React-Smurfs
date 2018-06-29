eef239b79e58030db21a316aae606931058b1956
Tyson Gushiken <tysonism@gmail.com>
2018-06-29 08:35:29 -0700

    Create get functionality

--- village/src/App.js
+++ village/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-
-import './App.css';
+import axios from 'axios';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+
+const API_URL = 'http://localhost:3333/smurfs';
 
 class App extends Component {
   constructor(props) {
@@ -11,9 +12,17 @@ class App extends Component {
       smurfs: [],
     };
   }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  componentDidMount() {
+    axios.get(API_URL)
+    .then(response => {
+      this.setState({smurfs: response.data});
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+
   render() {
     return (
       <div className="App">
