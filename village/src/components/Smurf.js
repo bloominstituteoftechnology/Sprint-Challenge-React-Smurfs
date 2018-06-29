f666c7f51ab529d66be14315275afda64239f057
Jameson <jamesonbrown@neolivity.com>
2018-06-29 11:42:32 -0600

    Minor CSS styling

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -9,7 +9,7 @@ const Smurf = props => {
       <h3>{props.name}</h3>
       <strong>{props.height} tall</strong>
       <p>{props.age} smurf years old</p>
-      <button className="delete-btn" onClick={() => deleteSmurf(props.id)}>X</button>
+      <span className="delete-btn" onClick={() => deleteSmurf(props.id)}>X</span>
     </div>
   );
 };
