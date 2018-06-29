c6d89045ebbc0e6bdd70c7c4f6c7696b96cebc43
Allen Hai <allenhai03@gmail.com>
2018-06-29 12:32:43 -0500

    add edit button and route (still need to add update functionality), add header component to root route, add /smurfs route

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from './Button';
 
-const Smurf = props => {
+const Smurf = ({ name, height, age, id }) => {
   return (
     <div className="Smurf">
-      <h3>{props.name}</h3>
-      <strong>{props.height} tall</strong>
-      <p>{props.age} smurf years old</p>
+      <h3>{name}</h3>
+      <strong>{height} tall</strong>
+      <p>{age} smurf years old</p>
+      <Link 
+        to={`/edit/${id}`} 
+        style={{textDecoration: 'none'}}
+      >
+        <Button>edit</Button>
+      </Link>
     </div>
   );
 };
