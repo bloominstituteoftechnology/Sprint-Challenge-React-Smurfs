f61d7312a877669ec1ba09b090594be0bd85f066
Jameson <jamesonbrown@neolivity.com>
2018-06-29 11:49:49 -0600

    Fixes

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -3,9 +3,6 @@ import React, { Component } from 'react';
 import Smurf from './Smurf';
 
 class Smurfs extends Component {
-  constructor(props) {
-    super(props)
-  }
   render() {
     return (
       <div className="Smurfs">
