572fc91da3103f7d829a19db42dbab05634acece
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2018-06-29 11:28:54 -0400

    routing added to index.js and made smurf village and smurfs show

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -11,6 +12,22 @@ class App extends Component {
       smurfs: [],
     };
   }
+
+  componentDidMount = () => {
+    this.getSmurfs();
+  }
+
+  getSmurfs = () => {
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(response => {
+        this.setState(() => ({ smurfs: response.data }));
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
