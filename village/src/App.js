48cf145b2ba2123d037074a03a9d31759d3ebfda
Luis <luis@Luiss-MacBook-Air.local>
2018-06-29 08:29:46 -0700

    inital comiit

--- village/src/App.js
+++ village/src/App.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import './App.css'
+import SmurfForm from './components/SmurfForm'
+import Smurfs from './components/Smurfs'
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
-      smurfs: [],
-    };
+      smurfs: []
+    }
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
-  render() {
+  render () {
     return (
-      <div className="App">
+      <div className='App'>
         <SmurfForm />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
