5d1430ee7c832cd10364531a4b897c73d07c8417
Ciara Burkett <ciaraburkett@gmail.com>
2018-06-29 14:20:26 -0400

    Finished routes

--- village/src/App.js
+++ village/src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {Route} from 'react-router-dom';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import Header from './components/Header';
 
 const URL = 'http://localhost:3333/smurfs';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       smurfs: [],
     };
@@ -44,8 +46,25 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm handleSubmitSmurf={this.handleSubmitSmurf}/>
-        <Smurfs smurfs={this.state.smurfs} />
+
+        <Route exact path="/" render={props => <Header {...props} />} />
+
+        <Route path="/smurfs" render={props => {
+          return (
+            <div>
+              <SmurfForm {...props} handleSubmitSmurf={this.handleSubmitSmurf} />
+            </div>
+          );
+        }}/>
+
+        <Route path="/smurfs" render={props => {
+          return (
+            <div>
+            <Smurfs {...props} smurfs={this.state.smurfs}/>
+            </div>
+          );
+        }}/>
+        
       </div>
     );
   }
