54d6c86cd3322ae6abdffe2585f12351cccbfd53
Alec <afroenygam@gmail.com>
2018-06-29 11:36:56 -0400

    Get functionality updated

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
@@ -11,6 +11,21 @@ class App extends Component {
       smurfs: [],
     };
   }
+
+  /*const URL = 'http://localhost:3333/smurfs'; Not sure why this is giving me trouble, will examine later*/
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(response => {
+        console.log('Get Response:', response.data);
+        this.setState({smurfs: response.data});
+      })
+      .catch(err => {
+        console.log('You have error:', err);
+      })
+  }
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
