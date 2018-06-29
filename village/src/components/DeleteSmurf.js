e15eee4ef5369b5cebca6aa9a46a3b0acb7c3723
Kevin <brackkevin@gmail.com>
2018-06-29 12:21:12 -0500

    Finished Delete functionality

--- village/src/components/DeleteSmurf.js
+++ village/src/components/DeleteSmurf.js
@@ -1,12 +1,29 @@
 import React from "react";
+import axios from "axios";
 
 class DeleteSmurf extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  handleDeleteSmurf = event => {
+    let URL = `http://localhost:3333/smurfs/${this.props.smurfId}`;
+    console.log("DELETE SMURF BUTTON PROPS", this.props);
+    console.log("DELETING SMURF ID", this.props.smurfId);
+    // Get the individual smurf by id
+    axios
+      .delete(URL)
+      .then(response => {
+        console.log(`Smurf ${this.props.smurfId} has been deleted..`);
+        window.location.reload();
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   render() {
-    return <button>Delete</button>;
+    return <button onClick={this.handleDeleteSmurf}>Delete</button>;
   }
 }
 
