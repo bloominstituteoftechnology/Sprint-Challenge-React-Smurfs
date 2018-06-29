f17e32ced58ccaac73724d7259c47122a3678396
Sukhada Gholba <sukhadagholba@gmail.com>
2018-06-29 08:28:13 -0700

    initial commit-added AXIOS GET to display the list of smurfs

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from "axios";
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
@@ -14,6 +14,25 @@ class App extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+
+ componentDidMount() {
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(response => {
+        console.log("GET RESPONSE: ", response);
+        this.setState({ smurfs: response.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+
+
+
+
+
+
   render() {
     return (
       <div className="App">
