9a1f94d6f1ab14cf2ae99b07473f19dcbb541c57
Joe Stanfield <jlstan06@hotmail.com>
2018-06-29 13:35:50 -0400

    mvp completed

---
+++ village/answers.md
@@ -0,0 +1,9 @@
+1.  client side routing is usually handled by the history api and is accessed using links and server side is worked by a server and is accessed by entering a url. 
+
+2. Hypertext transfer protocol
+
+3.  Create, Read, Update, and Destroy. 
+
+4.  GET, Post, Put, Delete
+
+5.  Axios, vanilla JS and jQuery.  
--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 import Smurf from './Smurf';
 
 class Smurfs extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      smurfs: []
+    };
+  }
+
+    componentDidMount() {
+      axios 
+        .get("http://localhost:3333/smurfs")
+        .then(response => {
+          this.setState({ smurfs: response.data });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
   render() {
     return (
+      
       <div className="Smurfs">
         <h1>Smurf Village</h1>
+        
+        
         <ul>
-          {this.props.smurfs.map(smurf => {
+          {this.state.smurfs.map(smurf => {
             return (
+              <Link to={`/smurfs/${this.state.smurfs.id}`}>
+              
               <Smurf
                 name={smurf.name}
                 id={smurf.id}
@@ -17,11 +40,16 @@ class Smurfs extends Component {
                 height={smurf.height}
                 key={smurf.id}
               />
+              </Link>
+              
             );
           })}
         </ul>
+        
       </div>
+      
     );
+
   }
 }
 
