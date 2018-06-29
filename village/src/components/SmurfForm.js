c64bbeadba4e88f6cad0163f2afafa77a04a03e8
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-06-29 08:56:11 -0700

    post is working

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -13,12 +14,23 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
-
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
+    const newObj = {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height
+    }
+    axios
+    .post('http://localhost:3333/smurfs', newObj)
+    .then(res => {
+      console.log('add smurf: ', res);
+      this.props.handleSetData(res.data);
+      this.setState({
+          name: '',
+          age: '',
+          height: ''
+        })
+    })
+    .catch(err => console.log(err))
   }
 
   handleInputChange = e => {
