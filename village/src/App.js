d950b7da90bad44f3e4a6d563ff0d6dcfd8829d6
Alec <afroenygam@gmail.com>
2018-06-29 12:24:19 -0400

    Delete functionality updated

--- village/src/App.js
+++ village/src/App.js
@@ -34,6 +34,8 @@ class App extends Component {
 
 
 
+
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
@@ -46,7 +48,8 @@ class App extends Component {
         <Route exact path="/" component={Header} />
         <Route path="/smurfs" render={props => <SmurfForm {...props}
         setData={this.setData} />} />
-        <Route path="/smurfs" render={props => <Smurfs {...props} smurfs={this.state.smurfs} />} />
+        <Route path="/smurfs" render={props => <Smurfs {...props}
+        smurfs={this.state.smurfs} setData={this.setData} />} />
       </div>
     );
   }
--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,14 +1,38 @@
 import React from 'react';
+import axios from 'axios';
 
-const Smurf = props => {
+class Smurf extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+    deleteSmurf = () => {
+      axios
+        .delete(`http://localhost:3333/smurfs/${this.props.id}`)
+        .then(response => {
+          console.log(response.data);
+          this.props.setData(response.data);
+        })
+        .catch(err => {
+          console.log(`DELETE Error: ${err}`);
+        })
+    }
+
+  render() {
   return (
     <div className="Smurf">
-      <h3>{props.name}</h3>
-      <strong>{props.height} tall</strong>
-      <p>{props.age} smurf years old</p>
+      <h3>{this.props.name}</h3>
+      <strong>{this.props.height} tall</strong>
+      <p>{this.props.age} smurf years old</p>
+      <button onClick={this.deleteSmurf}>Snuff Smurf</button>
     </div>
   );
-};
+}
+
+}
 
 Smurf.defaultProps = {
   name: '',
@@ -17,4 +41,3 @@ Smurf.defaultProps = {
 };
 
 export default Smurf;
-
