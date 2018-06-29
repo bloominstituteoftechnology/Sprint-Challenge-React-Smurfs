ee440a9b18d734032d9104f8195b581dfae80723
TangledTessellations <mcevoya@oregonstate.edu>
2018-06-29 10:16:05 -0700

    Add routing and buttons to navigate site

--- village/src/App.js
+++ village/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 import axios from 'axios';
+import Header from './components/Header';
 
 class App extends Component {
   constructor(props) {
@@ -31,14 +33,16 @@ class App extends Component {
   }
 
   
+  
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
       <div className="App">
-        <SmurfForm getData={this.getData} />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Route exact path='/' component={Header} />
+        <Route path='/smurfs' render={props => <SmurfForm {...props} getData={this.getData} /> } />
+        <Route path='/smurfs' render={props => <Smurfs {...props} smurfs={this.state.smurfs} />} />
       </div>
     );
   }
