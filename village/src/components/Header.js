3466db2524fc4876518b208b5a478de1e9c4b06d
tommycam <tommycam67@gmail.com>
2018-07-13 13:15:19 -0500

    MVP

--- village/src/components/Header.js
+++ village/src/components/Header.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Header = () => {
-  return <h2>Describe your header here.</h2>
+  return (
+    <div>
+      <h2>Welcome to Smurf Land</h2>
+      <Link to="/smurfs">Smurf Village</Link>
+    </div>
+
+  )
+
 }
- 
+
 export default Header;
