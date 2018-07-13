dccd98a8dd769843b5db59eeea824c056c384502
Daniel Chamorro <dan@dcitllc.com>
2018-07-13 18:19:41 -0400

    dchamorro- MVP Completed

--- village/src/components/Header.js
+++ village/src/components/Header.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-  return <h2>Describe your header here.</h2>
-}
- 
+  return <h2>Long live the Smurfs</h2>;
+};
+
 export default Header;
