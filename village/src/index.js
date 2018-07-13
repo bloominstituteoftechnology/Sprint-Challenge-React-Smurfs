dc5b47c0d7b7f28f27a58b20d4670cbab0c27789
sean chavez <seanchavez@seanchavezsMBP.home>
2018-07-13 10:30:35 -0700

    Add Routes and Link to enter village

--- village/src/index.js
+++ village/src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
+import Header from './components/Header'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={Header}/>
+      <Route path="/smurfs" component={App}/>
+    </div>
+  </BrowserRouter>, 
+  document.getElementById('root')
+);
