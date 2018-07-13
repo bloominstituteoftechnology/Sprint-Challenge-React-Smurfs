2fc79d58b2157882154e4a057f71f4c6324e68f1
Michael Real <mreal126@gmail.com>
2018-07-13 11:44:59 -0500

    Working, not perfectly displaying but I have to run! Hopefully good enough.

--- village/src/App.js
+++ village/src/App.js
@@ -1,24 +1,51 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import {Route} from 'react-router-dom';
+import Header from './components/Header';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       smurfs: [],
+      errorMessage: null
     };
   }
+  componentDidMount(){
+    axios.get('http://localhost:3333/smurfs')
+    .then(response => {
+      console.log(response);
+      this.setState({smurfs: response.data, errorMessage: null});
+    })
+    .catch(err => {this.setState({errorMessage: "Missing Smurf!"})
+  })
+};
+
+setSmurf = data => this.setState({smurfs: data});
+setErrorHandler = error => {this.setState({errorMessage: error})
+setTimeout(() => {
+  this.setState({errorMessage: null})
+}, 2500)
+};
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+      <Route exact path="/" component={Header}/>
+      <Route path="/smurfs" render={props => <Smurfs {...props} 
+      smurfs={this.state.smurfs} 
+      setSmurf={this.setSmurf} 
+      setErrorHandler={this.setErrorHandler}/>}
+      />
+      {this.state.errorMessage !== null ? 
+      (<span>{this.state.errorMessage}</span>) : null}
+      <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
   }
--- village/src/components/Header.js
+++ village/src/components/Header.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Header = () => {
-  return <h2>Describe your header here.</h2>
+  return (
+    <div>
+    <h2>Welcome to Smurfington</h2>
+    <Link to="/smurfs">Smurfington</Link>
+  
+    </div>
+)
 }
  
 export default Header;
