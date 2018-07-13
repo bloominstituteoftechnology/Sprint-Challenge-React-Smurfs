b7ba95debe4cc56108c1070eaf7eb7fb9de546a4
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-07-13 17:17:51 -0600

    adding Route path

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Route } from 'react-router-dom';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
@@ -49,6 +50,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <Route exact path="/" />
         <SmurfForm smurfData={this.smurfData} />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
