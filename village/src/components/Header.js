1e4e67331030a15262622afe0aae77e24b06219b
JacobLayton <jlayjlay3@gmail.com>
2018-07-13 10:52:32 -0700

    MVP complete

--- village/src/components/Header.js
+++ village/src/components/Header.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return <h2>Get Your Smurf Butt In Here!</h2>
+  return (
+    <div>
+      <h2>Get Your Smurf Butt In Here!</h2>
+      <h4>Click The Button To Enter</h4>
+      <Link to="/smurfs">
+        <button>Smurf Village</button>
+      </Link>
+    </div>
+  )
 }
  
 export default Header;
