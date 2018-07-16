bb732538bf5ff325f30e7f13cbe08bdcd1f3167b
Adrian Adames <adrianadames@gmail.com>
2018-07-15 23:49:36 -0400

    Set up axios get and post. Get confirmed working. Post not yet working due to having to set up the other components.

--- village/src/App.js
+++ village/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-
 import './App.css';
+import axios from 'axios'
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 
@@ -9,8 +9,75 @@ class App extends Component {
     super(props);
     this.state = {
       smurfs: [],
+      smurf: {
+        name: "",
+        age: "", 
+        height: ""
+      }
     };
   }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(response => {
+        console.log("GET RESPONSE: ", response);
+        this.setState({smurfs: response.data});
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  addNewSmurf = (e) => {
+    const smurfsCopy = this.state.smurfs.slice();
+    const smurfBlank = {
+      name: "",
+      age: "", 
+      height: ""
+    }
+    axios
+      .post("http://localhost:3333/smurfs", smurfsCopy)
+      .then(response => {
+        console.log("POST RESPONSE: ", response);
+        this.setState({smurfs: response.data, smurf: smurfBlank});
+      })
+      .catch(error => console.log(error));
+  }
+
+  handleNewSmurfName = e => {
+    console.log(e.target.value);
+    const smurfCopy = this.state.smurf.slice();
+
+    this.setState({smurf: {
+      name:e.target.value,
+      age: smurfCopy.age,
+      height: smurfCopy.email
+    }})
+  }
+
+  handleNewSmurfAge = e => {
+    console.log(e.target.value);
+    const smurfCopy = this.state.smurf.slice();
+
+    this.setState({smurf: {
+      name:smurfCopy.name,
+      age: e.target.value,
+      height: smurfCopy.email
+    }})
+  }
+  
+  handleNewSmurfHeight = e => {
+    console.log(e.target.value);
+    const smurfCopy = this.state.smurf.slice();
+
+    this.setState({smurf: {
+      name:smurfCopy.name,
+      age: e.target.value,
+      height: e.target.value
+    }})
+  }
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
