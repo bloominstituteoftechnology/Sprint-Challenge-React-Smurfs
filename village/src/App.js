790fc394c8d149a58456ccc22bedc279540418c8
Danny Vail <danny_vail@outlook.com>
2018-08-03 21:02:16 -0400

    Prettier formatting

--- village/src/App.js
+++ village/src/App.js
@@ -11,29 +11,45 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
   }
-  updateSmurfs = (newSmurfs) => {
+  updateSmurfs = newSmurfs => {
     this.setState({ smurfs: newSmurfs });
-  }
-  deleteSmurf = (event) => {
+  };
+  deleteSmurf = event => {
     const smurfId = event.target.dataset.id;
-    axios.delete(`http://localhost:3333/smurfs/${smurfId}`)
+    axios
+      .delete(`http://localhost:3333/smurfs/${smurfId}`)
       .then(response => this.setState({ smurfs: response.data }))
       .catch(err => console.error(err));
-  }
-  componentDidMount(){
-    axios.get('http://localhost:3333/smurfs')
-      .then(response => this.setState({ smurfs:response.data }))
+  };
+  componentDidMount() {
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(response => this.setState({ smurfs: response.data }))
       .catch(err => console.error(err));
   }
   render() {
     return (
       <div className="App">
-        <Route exact path='/' component={Header}/>
-        <Route path='/smurfs' render={(props) => <SmurfForm {...props} updateSmurfs={this.updateSmurfs} />} />
-        <Route path='/smurfs' render={(props) => <Smurfs {...props} smurfs={this.state.smurfs} deleteSmurf={this.deleteSmurf} />} />
+        <Route exact path="/" component={Header} />
+        <Route
+          path="/smurfs"
+          render={props => (
+            <SmurfForm {...props} updateSmurfs={this.updateSmurfs} />
+          )}
+        />
+        <Route
+          path="/smurfs"
+          render={props => (
+            <Smurfs
+              {...props}
+              smurfs={this.state.smurfs}
+              deleteSmurf={this.deleteSmurf}
+            />
+          )}
+        />
       </div>
     );
   }
