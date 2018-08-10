a9042507fceec4d7ab260d39df003588d3541c5c
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-08-10 11:00:33 -0600

    MVP

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Route } from 'react-router-dom';
+import Header from './components/Header';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -14,7 +16,7 @@ class App extends Component {
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
-  
+
   // You'll need to make sure you have the right properties on state and pass them down to props.
 
   componentDidMount() {
@@ -31,8 +33,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Route exact path="/" component={Header} />
+        <Route path="/smurfs" render={props =>
+          (<div>
+            <SmurfForm />
+            <Smurfs smurfs={this.state.smurfs} />
+          </div>
+          )}
+        />
       </div>
     );
   }
--- village/src/components/Header.js
+++ village/src/components/Header.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return <h2>Describe your header here.</h2>
+  return (
+    <div>
+      <h2>Welcome to the Smurf Village! <br/> Record incoming smurfs below.</h2>
+      <button>
+        <Link to="/smurfs">Click Here to Enter the Smurf Village!</Link>
+      </button>
+    </div>
+
+  )
 }
  
 export default Header;
