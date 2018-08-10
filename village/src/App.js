a60ab3d678ba23bb96a630826f3376fc2f924f6f
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-08-10 11:10:37 -0600

    MVP

--- village/src/App.js
+++ village/src/App.js
@@ -16,7 +16,6 @@ class App extends Component {
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
-
   // You'll need to make sure you have the right properties on state and pass them down to props.
 
   componentDidMount() {
@@ -36,8 +35,9 @@ class App extends Component {
         <Route exact path="/" component={Header} />
         <Route path="/smurfs" render={props =>
           (<div>
-            <SmurfForm />
-            <Smurfs smurfs={this.state.smurfs} />
+            <SmurfForm {...props} />
+            <Smurfs {...props}
+            smurfs={this.state.smurfs} />
           </div>
           )}
         />
--- village/src/components/Header.js
+++ village/src/components/Header.js
@@ -4,12 +4,11 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <div>
-      <h2>Welcome to the Smurf Village! <br/> Record incoming smurfs below.</h2>
+      <h2>Welcome to the Smurf Village! <br/> Click to enter.</h2>
       <button>
         <Link to="/smurfs">Click Here to Enter the Smurf Village!</Link>
       </button>
     </div>
-
   )
 }
  
