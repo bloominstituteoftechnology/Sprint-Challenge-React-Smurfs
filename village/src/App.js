36a4723e66fdbaaa120f8e5dd36be7898d84b48d
keegn <keegan.t.burkett@gmail.com>
2018-08-10 11:55:17 -0500

    got routes and links working

--- village/src/App.js
+++ village/src/App.js
@@ -5,6 +5,7 @@ import { Route } from 'react-router-dom';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import Header from './components/Header'
 
 const url = "http://localhost:3333/smurfs";
 
@@ -26,14 +27,20 @@ class App extends Component {
         console.log(err);
       })
   }
+
+  updateHandler = smurfs => {
+    this.setState({ smurfs });
+  };
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Route exact path="/" component={Header} />
+        <Route path="/smurfs" render={props => <Smurfs {...props} smurfs={this.state.smurfs} />} />
+        <Route path="/smurfs" render={props => <SmurfForm {...props} update={this.updateHandler} />} />
       </div>
     );
   }
--- village/src/components/Header.js
+++ village/src/components/Header.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Header = () => {
-  return <h2>Describe your header here.</h2>
+  return (
+    <div>
+      <h2>Welcome to the land of Smurfs</h2>
+      <Link to="/smurfs">Go to the Smurf Village</Link>
+    </div>
+  )
 }
- 
 export default Header;
