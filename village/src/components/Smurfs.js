1b4253516e88daf70de903e32356fdd17fc1b715
rodacoram <rodacoram8@gmail.com>
2018-08-10 10:41:56 -0600

    made Smurfs.js a functional component

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,32 +1,27 @@
 import React, { Component } from 'react';
 import Smurf from './Smurf';
 
-class Smurfs extends Component {
-  constructor(props){
-    super(props);
-  }
-  render() {
-    return (
-      <div className="Smurfs">
-        <h1>Smurf Village</h1>
-        <ul>
-          {this.props.smurfs.map(smurf => {
-            return (
-              <div className="SmurfCard" key={smurf.id}>
-                <Smurf
-                  name={smurf.name}
-                  id={smurf.id}
-                  age={smurf.age}
-                  height={smurf.height}
-                />
-                <button onClick={()=>this.props.func(smurf.id)}>Poof!</button>
-              </div>
-            );
-          })}
-        </ul>
-      </div>
-    );
-  }
+const Smurfs= props=> {
+  return (
+    <div className="Smurfs">
+      <h1>Smurf Village</h1>
+      <ul>
+        {props.smurfs.map(smurf => {
+          return (
+            <div className="SmurfCard" key={smurf.id}>
+              <Smurf
+                name={smurf.name}
+                id={smurf.id}
+                age={smurf.age}
+                height={smurf.height}
+              />
+              <button onClick={()=>props.func(smurf.id)}>Poof!</button>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 Smurf.defaultProps = {
