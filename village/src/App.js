25c37a4474bab0ee7b7de7028790c023530472d3
Trevor <trevorfehrman@gmail.com>
2018-08-10 08:55:06 -0700

    MVP complete

--- village/src/App.js
+++ village/src/App.js
@@ -1,37 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import axios from "axios";
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import { Route } from "react-router-dom";
+
+import "./App.css";
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
+import Header from "./components/Header";
 
 let API_URL = "http://localhost:3333/smurfs";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      smurfs: [],
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			smurfs: []
+		};
+	}
 
-  componentDidMount() {
-    axios.get(API_URL).then(response => {
-      this.setState({
-        smurfs : response.data,
-      })
-    })
-  }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
-  render() {
-    return (
-      <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
-      </div>
-    );
-  }
+	componentDidMount() {
+		axios.get(API_URL).then(response => {
+			this.setState({
+				smurfs: response.data
+			});
+		});
+	}
+	// add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
+	// Notice what your map function is looping over and returning inside of Smurfs.
+	// You'll need to make sure you have the right properties on state and pass them down to props.
+	render() {
+		return (
+			<div className="App">
+				<Route exact path="/" component={Header} />
+				<Route
+					path="/smurfs"
+					render={() => (
+						<div>
+							<SmurfForm />
+							<Smurfs smurfs={this.state.smurfs} />
+						</div>
+					)}
+				/>
+			</div>
+		);
+	}
 }
 
 export default App;
