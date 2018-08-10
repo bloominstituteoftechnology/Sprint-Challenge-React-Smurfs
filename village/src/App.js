82a7666ccee8a9cebe5bb7da2dfbdb789047d479
codejoncode <jonathanjholloway@gmail.com>
2018-08-10 10:23:23 -0500

    started the project for a pr

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'; 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
@@ -9,11 +9,16 @@ class App extends Component {
     super(props);
     this.state = {
       smurfs: [],
-    };
+    }
+  }
+  componentDidMount () {
+
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+
+
   render() {
     return (
       <div className="App">
