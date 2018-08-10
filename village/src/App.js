b7e0f65bdde36188d736465ea263e7f60a000b67
sliw <slioweng@gmail.com>
2018-08-11 01:06:49 +0800

    Steven L

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Route, NavLink } from 'react-router-dom';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -11,14 +13,26 @@ class App extends Component {
       smurfs: [],
     };
   }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  handleSyncingDB = () => {
+    axios.get('http://localhost:3333/smurfs')
+    .then(response => response.data)
+    .then(data => this.setState({ smurfs: data }))
+    .catch(e => console.error(e));
+  }
+
+  componentDidMount() {
+    this.handleSyncingDB();
+  }
+
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <NavLink to="/form" >Add Smurfs</NavLink>
+        <NavLink to="/smurfs" >Smurfs</NavLink>
+
+        <Route exact path="/form" render={ props => <SmurfForm handleSyncingDB={this.handleSyncingDB} {...props} />} />
+        <Route path="/smurfs" render={ props => <Smurfs smurfs={this.state.smurfs} {...props} />} />
       </div>
     );
   }
