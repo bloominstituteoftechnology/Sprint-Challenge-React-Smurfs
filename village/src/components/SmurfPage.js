933f5b7fd546b3a1ab789d4b34533b7a3d2d148a
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-08-10 13:25:32 -0400

    Redirect after deleting the smurf

--- village/src/components/SmurfPage.js
+++ village/src/components/SmurfPage.js
@@ -9,10 +9,24 @@ const SmurfPage = props => {
       <strong>{smurf.height} tall</strong>
       <p>{smurf.age} smurf years old</p>
       <button onClick={()=>props.edit({isEditing: true, editingID: smurf.id})}>Edit Smurf</button>
-      <button onClick={()=>props.deleteASmurf(smurf.id)}>Delete Smurf</button>
+      <button
+        onClick={
+          ()=>{
+            props.deleteASmurf(smurf.id); 
+            props.history.push('/smurfs');
+          }
+        }>
+        Delete Smurf
+      </button>
       <Link to="/smurfs">Go Back to the Village</Link>
     </div>
   );
 }
+
+SmurfPage.defaultProps = {
+  name: '',
+  height: '',
+  age: ''
+};
 
 export default SmurfPage;
