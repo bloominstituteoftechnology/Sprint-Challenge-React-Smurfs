66ef04e835e15911be2390aa3745bcc413e02728
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2018-08-10 10:31:18 -0500

    Working
    Wokring on the MVP, forgot to do the Answers.MD will do that after.

--- village/src/App.js
+++ village/src/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
-
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import axios from 'axios'; 
+
+const url = "http://localhost:3333/smurfs"; 
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       smurfs: [],
+
     };
   }
+  componentDidMount(){
+    axios
+      .get(url)
+        .then(response => {
+          console.log(response);
+          this.setState({
+            smurfs: response.data
+          });
+        })
+        .catch(err => console.log(err));
+    }
+    
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
@@ -23,5 +38,6 @@ class App extends Component {
     );
   }
 }
+
 
 export default App;
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios'; 
+const url = "http://localhost:3333/smurfs"; 
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -14,12 +16,20 @@ class SmurfForm extends Component {
     event.preventDefault();
     // add code to create the smurf using the api
 
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
-  }
+    const newSmurf = {
+      name: this.state.name,
+      age: this.state.age,
+      email: this.state.email
+    };
+      axios
+        .post(url, newSmurf)
+        .then(response => {
+          this.setState({
+            smurfs: response.date
+          });
+        })
+        .catch(err => console.log(err));
+    };
 
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
@@ -47,7 +57,7 @@ class SmurfForm extends Component {
             value={this.state.height}
             name="height"
           />
-          <button type="submit">Add to the village</button>
+          <button onClick={this.state.addSmurf}>Add to the village</button>
         </form>
       </div>
     );
