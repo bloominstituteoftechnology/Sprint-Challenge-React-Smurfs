7f1683eb83129691af343d7d7d789d577534db91
Sean <sean47900@gmail.com>
2018-08-10 10:42:58 -0500

    got smurf data on page, working on form now

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
@@ -12,6 +12,17 @@ class App extends Component {
     };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
+  componentDidMount() {
+    let url = 'http://localhost:3333/smurfs';
+    axios.get(url)
+      .then(res => {
+        this.setState({smurfs:res.data});
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props..
   render() {
