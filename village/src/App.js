00365be15f6000b6d9795c4d0f0a1501941b6b59
Lauren <llwor94@gmail.com>
2018-08-10 09:00:37 -0700

    able to add smurf

--- village/src/App.js
+++ village/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+
+const url = 'http://localhost:3333/smurfs' 
 
 class App extends Component {
   constructor(props) {
@@ -11,13 +14,34 @@ class App extends Component {
       smurfs: [],
     };
   }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  componentDidMount() {
+    axios.get(url)
+    .then(response => {
+      this.setState({
+        smurfs: response.data
+      })
+    })
+    .catch(response => {
+      console.log(`error ${response}`);
+    });
+
+  }
+  addSmurf = (name, age, height) => {
+    axios
+    .post(url, {name: name, age: age, height: height})
+    .then(response => {
+      this.setState({smurfs: response.data})
+    })
+    .catch(response => {
+      console.log(`error ${response}`);
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm addSmurf={this.addSmurf}/>
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
