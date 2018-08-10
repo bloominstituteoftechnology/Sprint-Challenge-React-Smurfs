468a8c5e7e2871252bf2a325135f3c9e5e65732c
Lauren <llwor94@gmail.com>
2018-08-10 09:46:01 -0700

    react router implemented

--- village/src/index.js
+++ village/src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './index.css';
+import Header from './components/Header';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route exact path='/' component={Header} />
+      <Route exact path='/smurfz' component={App} />
+    </div>
+  </Router>, 
+  document.getElementById('root'));
