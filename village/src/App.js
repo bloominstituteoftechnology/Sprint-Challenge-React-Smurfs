bc122be203a43d6f485b65c3decd40887f3a3506
sca4cs <shannon.atkinson.ca@gmail.com>
2018-08-10 11:44:25 -0400

    get and post working

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -11,13 +12,24 @@ class App extends Component {
       smurfs: [],
     };
   }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(response => {
+        this.setState(() => ({
+          smurfs: response.data
+        }));
+      })
+      .catch(error => {
+        console.error('Server Error', error);
+      });
+  }
+  
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm smurfs={this.state.smurfs} />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
