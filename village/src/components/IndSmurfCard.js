b169997288bcc0b45b398c2610fb36ea0f2cf1e5
szincone <sawyerzincone@gmail.com>
2018-08-10 11:03:40 -0600

    got individual pages working from stretch

--- village/src/components/IndSmurfCard.js
+++ village/src/components/IndSmurfCard.js
@@ -1,26 +1,35 @@
 import React, { Fragment } from "react";
 
 const IndSmurfCard = props => {
+  // // console.log("props.match.params.id", props.match.params.id);
+  // // console.log("IndSmurf Card", props.smurfs[2].id);
   // let indName = "";
+  // let height = "";
   // let age = 0;
-  // let email = "";
+  // let currentId = 0;
   // // console.log("IndSmurfCardRoute", props.match.params.name, props.data);
-  // for (let i = 0; i < props.data.length; i++) {
-  //   let name = props.data[i].name;
-  //   let activeName = props.match.params.name;
-  //   if (activeName === name) {
+  // for (let i = 0; i < props.smurfs.length; i++) {
+  //   let id = props.smurfs[i].id;
+  //   let activeId = props.match.params.id;
+  //   console.log("ID", id);
+  //   console.log("paramsID", activeId);
+  //   if (activeId === id) {
   //     console.log("Match");
-  //     indName = name;
-  //     age = props.data[i].age;
-  //     email = props.data[i].email;
+  //     currentId = currentId + activeId;
+  //     console.log("Current ID", currentId);
+  //     // indName = props.smurfs[i].name;
+  //     // height = props.smurfs[i].height;
+  //     // age = props.smurfs[i].age;
   //   }
   // }
+  console.log(props.smurfs);
+  console.log(props.match.params.id);
   return (
     <Fragment>
       <div>
-        <h1>Fill</h1>
-        <h2>55 years</h2>
-        <h2>6 ft 9</h2>
+        <h1>{props.smurfs[props.match.params.id].name}</h1>
+        <h2>{props.smurfs[props.match.params.id].height} smurf feet tall</h2>
+        <h2>{props.smurfs[props.match.params.id].age} smurf years old</h2>
       </div>
     </Fragment>
   );
