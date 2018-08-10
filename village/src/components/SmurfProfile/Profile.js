f71eaca8409f8753f0473f31a21de37980c2e0d6
Erik Kimsey <erik@erikkimsey.com>
2018-08-10 15:03:51 -0400

    Completed sketchy implementation of Route to an interpolated Profile component

--- village/src/components/SmurfProfile/Profile.js
+++ village/src/components/SmurfProfile/Profile.js
@@ -7,7 +7,13 @@ const Profile = (props) => {
     
     let smurf = props.getSmurf(props.match.params.id);
     console.log(smurf);
-    return <div>{props.match.params.id}</div>
+    return (
+        <div>
+            <h1>{smurf.name}</h1>
+            <h3>{smurf.age}</h3>
+            <h3>{smurf.height}</h3>
+        </div>
+    )
 }
 
 export default Profile;
