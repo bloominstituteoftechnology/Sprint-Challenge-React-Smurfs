71b09a4e76ec086e57e0648149dd526927bc8ef8
Jurgen Kela <jurgenkela@Jurgens-MacBook-Pro.local>
2018-08-10 12:05:24 -0400

    Created GET to list smurfs, and POST to create - POST very buggy

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -13,16 +15,26 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
-
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
+    const newSmurf = {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height
+    }
+    axios.post('http://localhost:3333/smurfs', newSmurf)
+    .then(response => {
+      this.setState({
+        name: response.data,
+        age: response.data,
+        height: response.data
+      })
+    })
+    .catch((error) => console.log(error))
   }
 
   handleInputChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+    this.setState({ 
+      [e.target.name]: e.target.value 
+    });
   };
 
   render() {
@@ -47,7 +59,7 @@ class SmurfForm extends Component {
             value={this.state.height}
             name="height"
           />
-          <button type="submit">Add to the village</button>
+          <button onClick={this.addSmurf} type="submit">Add to the village</button>
         </form>
       </div>
     );
