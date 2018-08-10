9e4d102fb217932b5739a0bb15ed6a2f58ca2eb3
Naaz Shaikh <naazhome@Naazs-MBP-3.hsd1.or.comcast.net>
2018-08-10 09:54:29 -0700

    done with mvp

--- Answers.md
+++ Answers.md
@@ -1,5 +1,26 @@
 1.  Explain the differences between `client-side routing` and `server-side routing`.
+
+
+
 1.  What does HTTP stand for?
+is a network protocol a set of rules that govern the way clients like a browser communicate with web servers over the internet. 
+Hyper text transfer protocol 
+
 1.  What does CRUD stand for?
+create
+read
+update
+delete
+
+when we build APIs we want our models to provide four basic types of functionality. 
+
+
 1.  Which HTTP methods can be mapped to the CRUD acronym that we use when interfacing with APIs/Servers.
+post creates a new resource, 
+get reads a resource,
+put updates a resource, 
+and delete removes a resource
+
+
 1.  Mention three tools we can use to make AJAX requests
+axios, fetch,http
--- village/src/App.js
+++ village/src/App.js
@@ -1,24 +1,100 @@
-import React, { Component } from 'react';
-
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
-
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
+import Header from "./components/Header";
+import { Route } from "react-router-dom";
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       smurfs: [],
+      name: "",
+      age: "",
+      height: ""
     };
   }
+  componentDidMount() {
+    this.getSmurfs();
+  }
+
+  getSmurfs = () => {
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(response => {
+        this.setState(() => ({ smurfs: response.data }));
+      })
+      .catch(error => {
+        console.error("Server Error", error);
+      });
+  };
+
+  handleName = event => {
+    event.preventDefault();
+    this.setState({
+      name: event.target.value
+    });
+  };
+
+  handleAge = event => {
+    event.preventDefault();
+    this.setState({
+      age: event.target.value
+    });
+  };
+
+  handleHeight = event => {
+    event.preventDefault();
+    this.setState({
+      height: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:3333/smurfs", {
+        name: this.state.name,
+        age: this.state.age,
+        height: this.state.height
+      })
+      .then(response => {
+        this.getSmurfs();
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Route exact path="/" component={Header} />
+
+        <Route
+          path="/smurfs"
+          render={props => (
+            <SmurfForm
+              {...props}
+              smurfs={this.state.smurfs}
+              handleSubmit={this.handleSubmit}
+              handleName={this.handleName}
+              handleAge={this.handleAge}
+              handleHeight={this.handleHeight}
+              name={this.state.name}
+              age={this.state.age}
+              height={this.state.height}
+            />
+          )}
+        />
+        <Route
+          path="/smurfs"
+          render={props => <Smurfs {...props} smurfs={this.state.smurfs} />}
+        />
       </div>
     );
   }
