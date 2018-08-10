d59de603e11fd92d71593afeb964c610d0e0f65e
keegn <keegan.t.burkett@gmail.com>
2018-08-10 10:45:41 -0500

    got GETrequest working

--- village/src/App.js
+++ village/src/App.js
@@ -6,12 +6,25 @@ import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 
+const url = "http://localhost:3333/smurfs";
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       smurfs: [],
     };
+  }
+
+  componentDidMount() {
+    axios
+      .get(url)
+      .then(response => {
+        this.setState( { smurfs: response.data });
+      })
+      .catch(err => {
+        console.log(err);
+      })
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
