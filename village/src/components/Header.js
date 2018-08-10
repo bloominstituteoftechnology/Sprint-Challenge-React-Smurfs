6f600151b3f49e0b6d9c9cd036b0e209fe2bed71
Kairi <kairijb@gmail.com>
2018-08-10 13:02:47 -0500

    added links to header

--- village/src/components/Header.js
+++ village/src/components/Header.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return <h2>Describe your header here.</h2>
+  return <div>
+  <h2>Welcome to Smurf village</h2>
+  <Link to='/smurfs'><button>View Smurfs</button></Link>
+  <Link to='/smurf'><button>Add Smurfs</button></Link>
+  </div>
 }
  
 export default Header;
