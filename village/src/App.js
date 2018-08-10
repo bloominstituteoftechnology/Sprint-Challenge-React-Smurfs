d0623290ddb3b8785c041bb554e1c1af7ef41a16
Grant Reighard <grant.reighard@gmail.com>
2018-08-10 12:06:32 -0400

    Finisehd all objectives.

--- village/src/App.js
+++ village/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Route } from 'react-router-dom';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import Header from './components/Header';
 
 class App extends Component {
   constructor(props) {
@@ -14,11 +17,19 @@ class App extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+  componentDidMount = () => {
+    axios.get('http://localhost:3333/smurfs')
+      .then(response => {
+        this.setState({smurfs: response.data})
+      })
+  }
+
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Route exact path="/" component={Header} />
+        <Route path="/smurfs" render={props => (<SmurfForm didMount={this.componentDidMount} {...props} />)} />
+        <Route path="/smurfs" render={props => (<Smurfs smurfs={this.state.smurfs} {...props} />)} />
       </div>
     );
   }
--- village/src/components/Header.js
+++ village/src/components/Header.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return <h2>Describe your header here.</h2>
+  return <div>
+    <h2>Hello! You are a welcome guest in Smurf Village.</h2>
+    <Link to="/smurfs">Come on in</Link>
+    </div>
 }
  
 export default Header;
