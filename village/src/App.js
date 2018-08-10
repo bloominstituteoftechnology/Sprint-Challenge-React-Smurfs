01a3a876982f2707c253daad5460983050e20176
johnoro <officialjohnorourke@gmail.com>
2018-08-10 11:29:48 -0400

    Implemented functional post and get requests.

--- village/src/App.js
+++ village/src/App.js
@@ -1,23 +1,64 @@
-import React, { Component } from 'react';
-
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import React, { Component } from "react";
+import axios from "axios";
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
   }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  addSmurf = e => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3333/smurfs", {
+        name: this.state.name,
+        age: this.state.age,
+        height: this.state.height
+      })
+      .then(res => {
+        this.setState({
+          smurfs: res.data,
+          name: "",
+          age: "",
+          height: ""
+        });
+      })
+      .catch(err => {
+        console.error("Server Post", err);
+      });
+  };
+
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  /* Lifecycle methods */
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(res => {
+        this.setState({ smurfs: res.data });
+      })
+      .catch(err => {
+        console.error("Server Get", err);
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm
+          name={this.state.name}
+          age={this.state.age}
+          height={this.state.height}
+          onChange={this.handleInputChange}
+          addSmurf={this.addSmurf}
+        />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
