84e80bcccf1e6aafe839e1391659257dbf26a902
adamcglee <adamcglee@gmail.com>
2018-08-10 09:42:03 -0700

    updated code to complete MVP.

--- village/src/components/Header.js
+++ village/src/components/Header.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+
 
 const Header = () => {
-  return <h2>Describe your header here.</h2>
+  return (
+    <div>
+      <h2>The Smurf-tastic Smurf Directory!</h2>
+      <NavLink to={`/smurfs/`}>
+        ENTER!
+      </NavLink>
+    </div>
+    
+  ) 
+
 }
  
 export default Header;
