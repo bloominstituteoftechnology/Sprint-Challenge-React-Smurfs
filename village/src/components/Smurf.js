399f414c61303b220943d27f5506025581fb0e55
Charles Cortinas <crcortinas@gmail.com>
2018-08-10 14:12:26 -0500

    Add a smurf
    Added functionality to add a Smurf. Also added a button for Gargamel to unleash his fury and burn the Smurf village clean of Smurfs and styled home page

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Smurf = props => {
   return (
-    <div className="Smurf">
+    <Link to={`/edit/${props.id}`}><div className="Smurf">
       <h3>{props.name}</h3>
       <strong>{props.height} tall</strong>
       <p>{props.age} smurf years old</p>
-    </div>
+    </div></Link>
   );
 };
 
@@ -17,4 +18,3 @@ Smurf.defaultProps = {
 };
 
 export default Smurf;
-
