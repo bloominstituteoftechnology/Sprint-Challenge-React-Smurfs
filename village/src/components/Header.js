6344fb7d599c8c0fe247842dada373acdda5987e
codejoncode <jonathanjholloway@gmail.com>
2018-08-10 16:58:47 -0500

    changed header styling the button font couldn't be seen in white also added the hover over effect.

--- village/src/components/Header.js
+++ village/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 const Header = () => {
   return (
-    <div>
+    <div id ="color-button" className ="Smurf">
       <h2>Welcome to Smurf Village!</h2>
       <button><Link to = '/smurfvillage'>Enter Now</Link></button>
     </div>
