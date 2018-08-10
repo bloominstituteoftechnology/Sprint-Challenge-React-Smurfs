199e1c7b0c88837a6e4be3ac0f9f508948e264b5
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-08-10 08:30:18 -0700

    add routes

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
@@ -29,11 +30,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Route exact path="/" component={Home} />
+        <Route path="/smurfs/add" component={SmurfForm} />
+        <Route exact path="/smurfs" render={props => (
+          <Smurfs {...props} smurfs={this.state.smurfs} />
+        )} />
       </div>
     );
   }
+}
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Link to="/smurfs"> All smurfs! </Link>
+      <Link to="/smurfs/add"> Add a smurf! </Link>
+    </div>
+  )
 }
 
 export default App;
