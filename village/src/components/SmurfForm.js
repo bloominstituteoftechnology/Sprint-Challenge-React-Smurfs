b51fb7c5051fdd8ada3c133c6946df5a3b5f2bef
Bryce Evans <bryceevans@Bryces-MacBook-Pro.local>
2018-08-10 10:25:42 -0600

    Add Smurf working... but only when page refreshes, so not really.

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -12,14 +13,25 @@ class SmurfForm extends Component {
 
   addSmurf = event => {
     event.preventDefault();
+    const smurf = {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height
+    }
     // add code to create the smurf using the api
-
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
-  }
+    axios
+      .post('http://localhost:3333/smurfs', smurf)
+      .then(response => {
+        this.setState({
+          name: '',
+          age: '',
+          height: ''
+        });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
 
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
