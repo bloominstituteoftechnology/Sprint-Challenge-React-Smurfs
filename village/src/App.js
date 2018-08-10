b49848d28b53b2f0c7689eb0e5e7d47dfc70a18e
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-08-10 08:21:49 -0700

    implement GET / POST methods

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
@@ -11,6 +11,18 @@ class App extends Component {
       smurfs: [],
     };
   }
+
+  componentDidMount() {
+    let url = 'http://localhost:3333/smurfs';
+    axios.get(url)
+      .then(res => {
+        this.setState({smurfs:res.data});
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -13,6 +14,18 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
+    let url = 'http://localhost:3333/smurfs';
+    axios.post(url, {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height
+    })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      })
 
     this.setState({
       name: '',
