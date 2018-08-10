c13ab3a52dfe53c5718eb0a21f69d0ff2d851f06
szincone <sawyerzincone@gmail.com>
2018-08-10 10:29:39 -0600

    mvp finished

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Smurf = props => {
   return (
@@ -11,10 +12,9 @@ const Smurf = props => {
 };
 
 Smurf.defaultProps = {
-  name: '',
-  height: '',
-  age: ''
+  name: "",
+  height: "",
+  age: ""
 };
 
 export default Smurf;
-
