6378054d1a966a426b2364db5c2ab735c1191c5a
luis salazar <luissalazar@luiss-MacBook-Pro.local>
2018-08-10 10:43:14 -0700

    Started styling Smurfs App

--- village/src/App.js
+++ village/src/App.js
@@ -8,10 +8,7 @@ import {
 } from "react-router-dom";
 
 import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
 import Header from './components/Header/Header';
-import Home from './components/Home/Home';
 
 class App extends Component {
   constructor() {
--- village/src/components/Header/Header.js
+++ village/src/components/Header/Header.js
@@ -3,6 +3,8 @@ import Home from '../Home/Home';
 import Smurfs from '../Smurfs';
 import SmurfForm from '../SmurfForm';
 
+import './Header.css';
+
 import {
   BrowserRouter as Router,
   Route,
@@ -16,12 +18,15 @@ class Header extends Component {
   }
   render() {
     return(
-      <div className="home-container">
-        <ul>
-          <li><NavLink exact to="/" className="nav-text">Home</NavLink></li>
-          <li><NavLink to="/village/" className="nav-text">Go to village!</NavLink></li>
-          <li><NavLink to="/add-smurf/" className="nav-text">Create your smurf!</NavLink></li>
-        </ul>
+      <div className="container">
+        <div className="navlink-container">
+          <ul className="nav-links">
+            <li><NavLink exact to="/" className="nav-text">Home</NavLink></li>
+            <li><NavLink to="/village/" className="nav-text">Go to village!</NavLink></li>
+            <li><NavLink to="/add-smurf/" className="nav-text">Create your smurf!</NavLink></li>
+          </ul>
+        </div>
+        
 
         <Route exact path="/" component={Home} />
         <Route exact path="/village/" component={Smurfs} />
