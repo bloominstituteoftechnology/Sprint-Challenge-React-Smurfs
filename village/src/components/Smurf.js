e33f7f4ac9148094c7aae183b21504244c952cc7
vanceleon <vanceleon44@gmail.com>
2018-08-17 14:04:22 -0400

    attempting to create smurf ids for deleting purposes

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -3,6 +3,7 @@ import React from "react";
 const Smurf = props => {
   return (
     <div className="Smurf">
+      {/* <div>{props.id}</div> */}
       <h3>{props.name}</h3>
       <strong>{props.height} tall</strong>
       <p>{props.age} smurf years old</p>
