fe6ad6747438e1aff1a37fde10aa36fae47ee0a1
Arthur Bates <aabatesj@gmail.com>
2018-08-17 13:43:34 -0400

    Header Link
    still working on links

--- village/src/components/Header.js
+++ village/src/components/Header.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-    return <h2>Describe your header here.</h2>
-}
+    <Link to='/'>
+        <h2>Welcome to Smurf Village!</h2>
+    </Link>
+};
 
 export default Header;
