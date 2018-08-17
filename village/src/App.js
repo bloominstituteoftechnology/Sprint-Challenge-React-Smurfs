f3147d1f41cd8e9076338361494586660ef509d8
JoCatherine Griggs <jocafrin@gmail.com>
2018-08-17 13:22:58 -0400

    edited addSmurf in SmurfForm with POST

--- village/src/App.js
+++ village/src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
+import axios from 'axios';
+import {Route} from 'react-router-dom';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+
+const url = ('http://localhost:3333/smurfs')
 
 class App extends Component {
   constructor(props) {
@@ -11,12 +16,23 @@ class App extends Component {
       smurfs: [],
     };
   }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  componentDidMount() {
+    axios
+    .get(url)
+    .then (response => {
+      this.setState(() => ({smurfs: response.data}));
+    })
+    .catch(err => {
+      console.error('Uh Oh!');
+    });
+  }
+
+
   render() {
     return (
       <div className="App">
+        <Route exact path="/" component={Header} />
         <SmurfForm />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -12,7 +13,7 @@ class SmurfForm extends Component {
 
   addSmurf = event => {
     event.preventDefault();
-    // add code to create the smurf using the api
+    axios.post('http://localhost:3333/smurfs', this.state)
 
     this.setState({
       name: '',
