f2e08d8c4497625af37f45e724c46ff31d9179b4
Erica Thompson <EricaThompson@iMac.lan>
2018-08-17 10:56:24 -0700

    Lunch

--- village/src/App.js
+++ village/src/App.js
@@ -1,24 +1,53 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
       smurfs: [],
+      name: '',
+      age: '',
+      height: ''
     };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+ 
+  
+  // componentDidMount(){
+  //   axios
+  //   .get('http://localhost:3333/smurfs')
+  //   .then(response => {
+  //     this.setState({ smurfs: response.data }))
+  //   .catch(error => console.log(error))
+  // }
+
+
+componentDidMount(){
+  axios
+      .get('http://localhost:3333/smurfs')
+      .then(response => {
+        this.setState(() => ({ smurfs: response.data }));
+      })
+      .catch(err => {
+        console.error('Error', err);
+      });
+}
+
+
+
   render() {
+    console.log("app render", this.state.smurfs)
     return (
       <div className="App">
         <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+    <div>{this.state.smurfs.map(each => <Smurfs data={each} key={each.id} />)} </div>
       </div>
     );
   }
--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,20 +1,20 @@
-import React from 'react';
+// import React from 'react';
 
-const Smurf = props => {
-  return (
-    <div className="Smurf">
-      <h3>{props.name}</h3>
-      <strong>{props.height} tall</strong>
-      <p>{props.age} smurf years old</p>
-    </div>
-  );
-};
+// const Smurf = props => {
+//   return (
+//     <div className="Smurf">
+//       <h3>{props.name}</h3>
+//       <strong>{props.height} tall</strong>
+//       <p>{props.age} smurf years old</p>
+//     </div>
+//   );
+// };
 
-Smurf.defaultProps = {
-  name: '',
-  height: '',
-  age: ''
-};
+// Smurf.defaultProps = {
+//   name: '',
+//   height: '',
+//   age: ''
+// };
 
-export default Smurf;
+// export default Smurf;
 
