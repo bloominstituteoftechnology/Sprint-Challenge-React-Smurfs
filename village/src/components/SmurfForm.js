1a293bb7b4ea96ff87493f8aa42037d0cc650392
AI2EJAY <JHammett1314@gmail.com>
2018-08-17 10:47:25 -0500

    Data Displayed and ability to post new smurfs implemented

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -11,14 +12,15 @@ class SmurfForm extends Component {
   }
 
   addSmurf = event => {
-    event.preventDefault();
+    
     // add code to create the smurf using the api
-
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
+    axios.post("http://localhost:3333/smurfs", 
+    {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height
+  })
+  .then(response => response.data)
   }
 
   handleInputChange = e => {
