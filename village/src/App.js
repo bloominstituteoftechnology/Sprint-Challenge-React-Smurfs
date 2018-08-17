cd28ecad9d96d9292f8968ea2f0530d9be4d4041
Arthur Bates <aabatesj@gmail.com>
2018-08-17 11:42:51 -0400

    App.js
    initial setup
    
    axios .get

--- village/src/App.js
+++ village/src/App.js
@@ -9,11 +9,26 @@ class App extends Component {
     super(props);
     this.state = {
       smurfs: [],
+      name: '',
+      age: '',
+      height: ''
     };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(response => {
+        this.setState({ friends: response.data });
+      })
+      .catch(error => {
+        console.log('Error: ', error);
+      });
+  }
+
   render() {
     return (
       <div className="App">
