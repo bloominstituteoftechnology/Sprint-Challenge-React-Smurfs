ed887b6a7647d492db647c223810c8c61efe6d3d
AlexanderCaves <ahcaves@gmail.com>
2018-08-17 09:13:14 -0700

    get gotten, post posted

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -13,7 +14,13 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
-
+    const { name, age, height } = this.state;
+    const newSmurf = { name, age, height };
+    axios.post(`http://localhost:3333/smurfs`, newSmurf)
+      .then(response => {
+        this.props.setSmurfData(response.data);
+      })
+      
     this.setState({
       name: '',
       age: '',
