b7ecccc4e85e019f98bf00f274d7c0076f9aaf63
JeanetteFernandez <jeanfern555@gmail.com>
2018-09-14 10:45:13 -0500

    Get request retrieves smurfs data.

--- village/src/App.js
+++ village/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import './App.css';
+import axios from 'axios';
 
+import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 
@@ -9,11 +10,30 @@ class App extends Component {
     super(props);
     this.state = {
       smurfs: [],
+      smurf:{
+        name:'',
+        age:"",
+        height:'',
+      }
     };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  componentDidMount() {
+    axios
+    .get('http://localhost:3333/smurfs')
+    .then(response => {
+      this.setState({smurfs: response.data})
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+
+
   render() {
     return (
       <div className="App">
@@ -24,6 +44,5 @@ class App extends Component {
   }
 }
 
-Change to be able to commit initial PR;
 
 export default App;
