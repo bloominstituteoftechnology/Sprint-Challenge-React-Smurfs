e1732e6ac8e3cb025dd22a99b9338b228f3bdc05
Max <maxkajiwara@gmail.com>
2018-09-14 09:59:27 -0700

    MVP

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -13,28 +13,23 @@ class SmurfForm extends Component {
 
 	addSmurf = event => {
 		event.preventDefault();
-		// add code to create the smurf using the api
-		if (this.state.name && this.state.age && this.state.height) {
-			axios
-				.post('http://localhost:3333/smurfs', {
-					name: this.state.name,
-					age: this.state.age,
-					height: this.state.height
-				})
-				.then(() => {
-					this.setState({
-						name: '',
-						age: '',
-						height: ''
-					});
-					this.props.reload();
-				})
-				.catch(error => {
-					console.log(error);
+		axios
+			.post('http://localhost:3333/smurfs', {
+				name: this.state.name,
+				age: this.state.age,
+				height: this.state.height
+			})
+			.then(() => {
+				this.setState({
+					name: '',
+					age: '',
+					email: ''
 				});
-		} else {
-			alert('FINISH THE SMURF');
-		}
+				this.props.history.push('/');
+			})
+			.catch(error => {
+				console.log(error);
+			});
 	};
 
 	handleInputChange = e => {
