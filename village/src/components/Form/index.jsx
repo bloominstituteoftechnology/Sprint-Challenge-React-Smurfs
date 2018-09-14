6463a23377aa3d2ef74b74f415d799616dca6779
decagondev <decagondevelopment@gmail.com>
2018-09-14 17:54:40 +0100

    (MVP) : refactored the form in to a simple presentational component and rstructured the files for jsx

---
+++ village/src/components/Form/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import '../../App.css';
+
+// refactored in to a basic presentational component
+const Form = props => {
+  return (
+    <div className="form-wrapper">
+      <form>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={props.name}
+          onChange={props.handleInputChange}
+
+        />
+        <input
+          type="text"
+          name="age"
+          placeholder="Age"
+          value={props.age}
+          onChange={props.handleInputChange}
+        />
+        <input
+          type="text"
+          name="height"
+          placeholder="Height"
+          value={props.height}
+          onChange={props.handleInputChange}
+        />
+        <button onClick={props.handleSmurfSubmit}>Submit</button>
+        <button onClick={props.handleCancel}>Cancel</button>
+      </form>
+    </div>
+  );
+}
+
+export default Form;
