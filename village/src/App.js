4fbca0ab9f44de1c5838c4e930f4a17fbd08d2d8
Csillag61 <csillagok@gmail.com>
2018-09-14 13:33:48 -0400

    Close to the end

--- village/src/App.js
+++ village/src/App.js
@@ -1,27 +1,108 @@
 import React, { Component } from 'react';
-
+import Header from './components/Header';
+import axios from 'axios';
+import { Link, Route } from 'react-router-dom';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 
+const url = "http://localhost:3333/smurfs";
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       smurfs: [],
+      name:"",
+      age:"",
+      height:"",
     };
   }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
-  render() {
+ 
+  componentDidMount() {
+    this.getSmurfs();
+  }
+  getSmurfs=()=>{
+
+   axios
+     .get(`http://localhost:3333/smurfs`)
+
+      .then(response => {this.setState(()=>({ smurfs: response.data }));
+    })
+      //.catch(err => console.log(err));
+      .catch (error => {
+      console.log(error);
+    })
+  }
+      
+  
+
+
+handleName = event => {
+  event.preventDefault();
+  this.setState({
+    name: event.target.value
+  });
+};
+
+
+handleAge = event => {
+  event.preventDefault();
+  this.setState({
+    age: event.target.value
+  });
+};
+
+
+handleHeight = event => {
+  event.preventDefault();
+  this.setState({
+    height: event.target.value
+  });
+};
+
+
+handleSubmit = event => {
+  event.preventDefault();
+  axios
+    .post("http://localhost:3333/smurfs", {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height
+    })
+    .then(response => {
+      this.getSmurfs();
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+  render(){
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
-      </div>
-    );
-  }
+      <Route exact path="/" component={Header}/>
+      <Route  path="/smurfs" 
+      render={(props)=>(
+        <SmurfForm{...props}
+            smurfs={this.state.smurfs}
+            handleSubmit={this.handleSubmit}
+            handleName={this.handleName}
+            handleAge={this.handleAge}
+            handleHeight={this.handleHeight}
+            name={this.state.name}
+            age={this.state.age}
+            height={this.state.height}
+          />
+        )}
+      />
+      <Route
+        path="/smurfs"
+        render={props => <Smurfs {...props} smurfs={this.state.smurfs} />}
+      />
+    </div>
+    )};
 }
 
+
+
 export default App;
+
--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const Smurf = props => {
   return (
@@ -6,15 +6,28 @@ const Smurf = props => {
       <h3>{props.name}</h3>
       <strong>{props.height} tall</strong>
       <p>{props.age} smurf years old</p>
+      <button
+        onClick={() => {
+          props.delete(props.id)
+        }}
+      >
+        Remove from village
+      </button>
+      <button
+        onClick={() => {
+          props.update(props.id)
+        }}
+      >
+        Update Info
+      </button>
     </div>
   );
 };
 
 Smurf.defaultProps = {
-  name: '',
-  height: '',
-  age: ''
+  name: "",
+  height: "",
+  age: ""
 };
 
 export default Smurf;
-
