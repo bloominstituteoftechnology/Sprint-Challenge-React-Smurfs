e0f42ecf335632d08fbb535adb86b1281a015916
Caleb Kirkwood <calebkirkwood@calebs-mbp.attlocal.net>
2018-09-14 12:01:08 -0400

    Get smurfs from server, and post new smurfs complete

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -14,10 +15,27 @@ class App extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+  componentDidMount() {
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then((response) => {
+        this.setState({ smurfs: response.data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  addNewSmurf = (smurf) => {
+    axios
+      .post('http://localhost:3333/smurfs', smurf)
+      .then((response) => this.setState({ smurfs: response.data }));
+  };
+
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm addNewSmurf={this.addNewSmurf} />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
