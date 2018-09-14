f38dc37b1aae733defb2327cbe6da21f79dffe43
decagondev <decagondevelopment@gmail.com>
2018-09-14 17:56:46 +0100

    (MVP) : refactored the Header for a link and jsx workflow

--- village/src/components/Header.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Header = () => {
-  return <h2>Welcome to the Village.</h2>
-}
- 
-export default Header;
---
+++ village/src/components/Header/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="home-link">
+        <Link to="/">Home</Link>
+      </div>
+      <div className="header-title">
+        <h2>Welcome to Smurf Village!</h2>
+      </div>
+      <div className="add-smurf-link">
+        <Link to="/smurfs/add">Add smurf</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
