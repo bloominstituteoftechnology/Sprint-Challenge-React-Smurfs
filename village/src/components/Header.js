75584701e0b7bdd292c54ae9aa00686555eb37b2
lyang9 <lyang9@buffalo.edu>
2018-09-14 13:03:40 -0400

    Imported Link

--- village/src/components/Header.js
+++ village/src/components/Header.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <div>
-      <Link to='/smurfs'>Smurf Village</Link>
       <h2>Welcome to Smurfs Village!</h2>
+      <Link to='/smurfs'>Smurf Village</Link>
     </div>
   );
 }
