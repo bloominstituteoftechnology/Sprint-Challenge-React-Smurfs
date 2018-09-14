01d36b0390cb235027b8c4df60c8757ddc307946
lndubose <lndubose13@gmail.com>
2018-09-14 12:29:38 -0500

    Added a delete

--- village/src/App.js
+++ village/src/App.js
@@ -28,6 +28,12 @@ class App extends Component {
     .catch(err => console.log(err));
   }
 
+  deleteSmurf = smurfId => {
+    console.log(smurfId)
+    return axios.delete(`http://localhost:3333/smurfs/${smurfId}`)
+    .then(response => this.setState({smurfs: response.data}))
+  }
+
   render() {
     return (
       <div className="App">
@@ -35,13 +41,19 @@ class App extends Component {
           <div className="navigation container">
             <h4>Welcome to the Smurf Village!</h4>
             <p>Please come in:</p>
-            <NavLink to='/'>Smurf Village</NavLink>
+            <NavLink exact to='/' activeClassName="active">Smurf Village</NavLink>
             <p>Or join the Smurfs village:</p>
-            <NavLink to='/smurfform'>New Smurf</NavLink>
+            <NavLink to='/smurfform' activeClassName="active">New Smurf</NavLink>
           </div>
         </header>
         <section className="main-content container">
-          {this.state.smurfs.length === 0 ? <Route exact path='/' component={Intro} />: <Route exact path='/' render={props => <Smurfs {...props} smurfs={this.state.smurfs} />} />}
+          {this.state.smurfs.length === 0 ? <Route exact path='/' component={Intro} />: <Route exact path='/' render={props =>
+             <Smurfs 
+             {...props} 
+             smurfs={this.state.smurfs} 
+             deleteSmurf={this.deleteSmurf}
+             />} 
+          />}
           <Route path='/smurfform' component={SmurfForm} />
         </section> 
       </div>
--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const Smurf = props => {
+  
+  function handleDelete() {
+    props.deleteSmurf(props.id);
+  }
+
   return (
     <div className="Smurf">
+      <button onClick={handleDelete}>X</button>
       <h3>{props.name}</h3>
       <strong>{props.height} tall</strong>
       <p>{props.age} smurf years old</p>
