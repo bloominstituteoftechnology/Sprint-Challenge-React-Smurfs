024a249df00f768dae0973b225de3f6522b04c79
ezra davis davis <ezra.rr.davis@gmail.com>
2018-09-14 10:32:58 -0700

    mvps finished

--- village/src/components/Header.js
+++ village/src/components/Header.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 const Header = () => {
-  return <h2>Describe your header here.</h2>
+  return(
+<nav>
+<Link to = '/'>home</Link>
+<Link to = '/smurfs-form'>form</Link>
+  </nav>
+
+  )
+  
 }
  
 export default Header;
