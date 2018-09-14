1418eb8e0bd4f4ef26694d2219683c8387bec379
Nicocchi <nicocchilover@gmail.com>
2018-09-14 11:25:37 -0400

    get request complete

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -11,6 +12,23 @@ class App extends Component {
       smurfs: [],
     };
   }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  // Fetch the data for friends list
+  fetchData() {
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(response => {
+        this.setState({ smurfs: response.data})
+      })
+      .catch(error => {
+        console.error('Server Error', error);
+      })
+  }
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
