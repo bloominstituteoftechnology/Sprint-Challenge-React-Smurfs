b1d167e93bb8abd56fe85be541902fd95e410a3b
alejandrok93 <alejandrok93@gmail.com>
2018-09-14 10:53:13 -0500

    HTTP requests to GET smurfs data and POST new smurf completed

--- village/src/App.js
+++ village/src/App.js
@@ -1,20 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import "./App.css";
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
+import axios from "axios";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
+
+  componentDidMount() {
+    //make GET request and assign to state sumrfs array
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(response => this.setState({ smurfs: response.data }))
+      .catch(err => console.log(err));
+  }
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
+    {
+      console.log(this.state);
+    }
     return (
       <div className="App">
         <SmurfForm />
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,31 +1,41 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import axios from "axios";
 class SmurfForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      age: '',
-      height: ''
+      name: "",
+      age: "",
+      height: ""
     };
   }
 
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
-
+    let smurf = this.state;
+    axios
+      .post("http://localhost:3333/smurfs", smurf)
+      .then
+      //need to setState with response.data
+      ()
+      .catch(err => console.log(err));
+    console.log("smurf added!");
     this.setState({
-      name: '',
-      age: '',
-      height: ''
+      name: "",
+      age: "",
+      height: ""
     });
-  }
+  };
 
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
   render() {
+    {
+      console.log(this.props);
+    }
     return (
       <div className="SmurfForm">
         <form onSubmit={this.addSmurf}>
