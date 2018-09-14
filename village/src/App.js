d7fba2e81c3b72a5f4dd2335f099623e61913519
MikeRGarrity <mikergarrity@gmail.com>
2018-09-14 11:25:17 -0500

    adding post request

--- village/src/App.js
+++ village/src/App.js
@@ -24,14 +24,16 @@ class App extends Component {
       });
   }
 
+  handleChange = data => this.setState({ smurfs: data })
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
       <div className="App">
-        <SmurfForm/>
-        <Smurfs smurfs={this.state.smurfs} />
+          <SmurfForm handleChange={this.handleChange}/>
+          <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
   }
