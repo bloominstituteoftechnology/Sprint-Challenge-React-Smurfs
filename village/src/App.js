2186c8ea15d90a9606f84b45d93043acc34ea0e4
A2i <abdulfatah.a.ahmad@gmx.com>
2018-09-21 11:37:16 -0400

    Answers completed

--- village/src/App.js
+++ village/src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
