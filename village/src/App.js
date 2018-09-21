0b4ba7203e7c8cc450a170c21dfd14f0cc89bcf0
verydecent <wnjhwng@gmail.com>
2018-09-21 08:53:06 -0700

    Get request functional

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import Axios from 'axios';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
@@ -10,6 +10,17 @@ class App extends Component {
     this.state = {
       smurfs: [],
     };
+  }
+
+  componentDidMount() {
+    Axios
+      .get(`http://localhost:3333/smurfs`).then(res => {
+        console.log('API running!')    
+        console.log(res);
+        this.setState({ smurfs: res.data })
+        }).catch(err => {
+          console.log('Error trying to get API', err)
+    })
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
