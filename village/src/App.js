e875cc1a9a5c403c2e4a513e5d5bc265c60c169a
Marious Davis <mlancelot88@gmail.com>
2018-09-21 10:44:29 -0500

    imported Axios, BrowserRouter, and built out my App.js so far

--- village/src/App.js
+++ village/src/App.js
@@ -1,25 +1,70 @@
-import React, { Component } from 'react';
-
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import React, { Component } from "react";
+import Axios from "axios";
+import "./App.css";
+import { BrowserRouter as Route, Router, NavLink } from "react-router-dom";
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
   }
+
+  componentDidMount() {
+    Axios.get("http://localhost:3333/smurfs")
+      .then(response => {
+        console.log(response);
+        this.setState({ smurfs: response.data });
+      })
+      .catch(err => console.log(err));
+  }
+
+  onCreate = smurfs => {
+    console.log("smurfs", smurfs);
+    this.setState({
+      smurfs: smurfs
+    });
+  };
+
+  deleteSmurf = id => {
+    Axios.delete(`http://localhost:3333/smurfs/${id}`)
+      .then(response => this.setState({ smurfs: response.data }))
+      .catch(err => console.log(err));
+  };
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
-      <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
-      </div>
+      <AppWrapper>
+        <NavWrapper>
+          <NavElement>
+            <NavLink to="/"> Village </NavLink>
+          </NavElement>
+          <NavElement>
+            <NavLink to="/smurf-form"> Add a Smurf </NavLink>
+          </NavElement>
+        </NavWrapper>
+        <Route
+          path="/smurf-form"
+          render={props => <SmurfForm {...props} onCreate={this.onCreate} />}
+        />
+        <Route
+          exact
+          path="/"
+          render={props => (
+            <Smurfs
+              {...props}
+              smurfs={this.state.smurfs}
+              deleteSmurf={this.deleteSmurf}
+            />
+          )}
+        />
+      </AppWrapper>
     );
   }
 }
