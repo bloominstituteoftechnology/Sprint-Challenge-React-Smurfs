c40b3f8ea3c62319e7b3f30d629777acbb407c64
unknown <quan.nguyen199712@gmail.com>
2018-10-19 15:38:27 -0400

    check if this works, post works now

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -14,7 +14,7 @@ class SmurfForm extends Component {
   }
 
   addSmurf = event => {
-    event.preventDefault();
+
     // add code to create the smurf using the api
     axios
       .post("http://localhost:3333/smurfs", this.state.smurf)
