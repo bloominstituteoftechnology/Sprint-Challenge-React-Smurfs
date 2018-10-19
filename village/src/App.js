a41341d876dbca9ab7e85f3c583898f151ff6b1f
Brandon Vilaychith <blvilaychith@outlook.com>
2018-10-19 00:35:30 -0500

    Created GET request / Added state data

--- village/src/App.js
+++ village/src/App.js
@@ -1,6 +1,9 @@
+// React and axios imports
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
+// Component imports
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 
@@ -8,8 +11,15 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
+  }
+  // Sends get request to server to grab the data for the state
+  componentDidMount() {
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(response => this.setState({ smurfs: response.data }))
+      .catch(error => console.log(error));
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
