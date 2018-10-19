f2322a2605cf4ee7bba8d5e325e313d06da7eefb
Hunter315 <hunter.mckay.smith@gmail.com>
2018-10-19 10:31:06 -0600

    SmurfForm add new smurf set up

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -6,24 +7,39 @@ class SmurfForm extends Component {
     this.state = {
       name: '',
       age: '',
-      height: ''
+      height: '',
+      isUpdate: false
     };
   }
 
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
+    if (this.state.name && this.state.age && this.state.height){
+      if (!this.state.isUpdate){
+        axios
+          .post('http://localhost:3333/smurfs',
+                { name: this.state.name,
+                  age: this.state.age,
+                  height: this.state.height })
+          .then(res => {this.props.handleChange})
+          .catch(err => console.log(err));
+      } else{
+        console.log('work on this update function later');
+        // axios
+        //   .put('http://localhost:3333/smurfs')
+      }
+      this.setState({
+        name: '',
+        age: '',
+        height: ''
+      });
+    }
 
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
+    handleInputChange = e => {
+      this.setState({ [e.target.name]: e.target.value });
+    };
   }
-
-  handleInputChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
 
   render() {
     return (
@@ -47,7 +63,7 @@ class SmurfForm extends Component {
             value={this.state.height}
             name="height"
           />
-          <button type="submit">Add to the village</button>
+          <button type="submit" onClick={this.props.handleChange}>Add to the village</button>
         </form>
       </div>
     );
