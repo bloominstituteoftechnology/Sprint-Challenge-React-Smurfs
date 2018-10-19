b88456c4e642358c7b1aa719ecfecb48f0df5e0b
SavannahGreuel <smallidgesavannah@gmail.com>
2018-10-19 10:36:56 -0600

    POST functionality is working

--- village/src/App.js
+++ village/src/App.js
@@ -11,21 +11,30 @@ class App extends Component {
       smurfs: [],
     };
   }
+  componentDidMount() {
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(response => {
+        console.log(response);
+        this.setState({smurfs: response.data})
+      })
+      .catch(error => {
+        console.error('Error', error);
+      });
+  }
+
+  addNewSmurf = data => {
+    this.setState({smurfs:data})
+  }
+
+  
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
-componentDidMount()  {
-  axios
-    .get('http://localhost:3333/smurfs')
-    .then (response => { this.setState({ smurfs:response.data})})
-    .catch(error => {console.log(error)})
-}
-
-
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm addNewSmurf={this.addNewSmurf} />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
