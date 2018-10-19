a30b9355c1bac62792d9e25a9b01a9937711d311
Edwin Torres <edwin.torres.jr2@gmail.com>
2018-10-19 13:21:05 -0400

    Route and NavLinks

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from "axios"
+import {Route, NavLink} from "react-router-dom"
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
@@ -27,9 +28,15 @@ class App extends Component {
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
-      <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+      <div>
+      <div>
+        <NavLink to="/">Smurfs</NavLink>
+        <NavLink to="/smurf-form">Add Smurf</NavLink>
+      </div>
+      <div>
+                  <Route exact path="/" render={props => <Smurfs {...props} smurfs= {this.state.smurfs}/>} />
+                  <Route path="/smurf-form" component={SmurfForm} />
+      </div>
       </div>
     );
   }
