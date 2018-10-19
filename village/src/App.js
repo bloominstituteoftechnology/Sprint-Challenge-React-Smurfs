5d05543b3fe7ddf1ebfc1a447c0d8a118b8f18fe
pedro <pedroluismontesinosrivera@gmail.com>
2018-10-19 14:30:47 -0400

    fixed some bugs, new smurfs were not being displayed to the screen

--- village/src/App.js
+++ village/src/App.js
@@ -9,7 +9,12 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: []
+      smurfs: [],
+      newSmurf:{
+        name:'',
+        age:'',
+        height: ''
+      }
     };
   }
   componentDidMount() {
@@ -19,6 +24,30 @@ class App extends Component {
       .catch(error => console.log(error))
   }
 
+
+  changeHandler = event => {
+    this.setState({
+      newSmurf: {
+        ...this.state.newSmurf,
+        [event.target.name]: event.target.value
+      }
+    });
+  }
+  addSmurf = event => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:3333/smurfs", this.state.newSmurf)
+      .then(response => {
+        this.setState({
+          smurfs: response.data,
+            newSmurf:{
+            name: "",
+            age: "",
+            height: ""
+          }
+        });
+      })
+  }
   render() {
     return (
       <div className="App">
@@ -26,8 +55,16 @@ class App extends Component {
           <NavLink to="/">Smurfs</NavLink>
           <NavLink to="/smurfs-form">Create Smurf</NavLink>
         </div>
-        <Route exact path="/" render={props => <Smurfs {...props} smurfs={this.state.smurfs}/>}/>
-        <Route path="/smurfs-form" component={SmurfForm}/>
+        <Route 
+          exact path="/"
+          render={props => <Smurfs {...props} 
+          smurfs={this.state.smurfs}/>}/>
+        <Route 
+          path="/smurfs-form"
+          render={props => <SmurfForm {...props} 
+          newSmurf={this.state.newSmurf}
+          addSmurf={this.addSmurf}
+          changeHandler={this.changeHandler} />} />
       </div>
     );
   }
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,54 +1,26 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
 class SmurfForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      age: '',
-      height: ''
-    };
-  }
-
-  addSmurf = event => {
-    event.preventDefault();
-    axios
-      .post("http://localhost:3333/smurfs", this.state)
-      .then(response => {
-        this.setState({
-          smurfs: response.data,
-          name: "",
-          age: "",
-          height: ""
-        });
-      })
-  }
-
-  handleInputChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
   render() {
     return (
       <div className="SmurfForm">
-        <form onSubmit={this.addSmurf}>
+        <form onSubmit={this.props.addSmurf}>
           <input
-            onChange={this.handleInputChange}
+            onChange={this.props.changeHandler}
             placeholder="name"
-            value={this.state.name}
+            value={this.props.newSmurf.name}
             name="name"
           />
           <input
-            onChange={this.handleInputChange}
+            onChange={this.props.changeHandler}
             placeholder="age"
-            value={this.state.age}
+            value={this.props.newSmurf.age}
             name="age"
           />
           <input
-            onChange={this.handleInputChange}
+            onChange={this.props.changeHandler}
             placeholder="height"
-            value={this.state.height}
+            value={this.props.newSmurf.height}
             name="height"
           />
           <button type="submit">Add to the village</button>
