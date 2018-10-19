b330aaa493d35386502fcfb066a2782b9febe2be
Adam <ajmboosh@gmail.com>
2018-10-19 10:36:18 -0500

    finished building inital axios.get request to set smurf data on state. included if statement in render method to handle case of data not being loaded yet

--- village/src/App.js
+++ village/src/App.js
@@ -1,26 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import "./App.css";
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+  componentDidMount() {
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(res => this.setState({ smurfs: res.data }))
+      .catch(err => console.log(err, "Smurfs not found"));
+  }
+
   render() {
-    return (
-      <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
-      </div>
-    );
+    if (!this.state.smurfs.length) {
+      return <h1>Retrieving the smurfing data, hold on...</h1>;
+    } else {
+      return (
+        <div className="App">
+          <SmurfForm />
+          <Smurfs smurfs={this.state.smurfs} />
+        </div>
+      );
+    }
   }
 }
 
