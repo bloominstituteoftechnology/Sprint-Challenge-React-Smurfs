9cfa47f3a701526e4975b0eb72b3e4c111d5c69d
Rahul Desai <rd825@georgetown.edu>
2018-10-19 11:41:29 -0400

    Added GET, POST, and Routes, but server is broken and don't know how to resolve.

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -13,6 +14,9 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
+    axios.post('http://localhost:3333/smurfs', this.state)
+         .then(res => console.log(res.data))
+         .catch(err => console.log(err))
 
     this.setState({
       name: '',
