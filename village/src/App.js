a83c9fc9a457c7cda79a8f6c3a720a93e99ca6e0
Axolotl <samuelmachat@gmail.com>
2018-10-19 11:37:51 -0400

    get smurfs working

--- village/src/App.js
+++ village/src/App.js
@@ -1,27 +1,34 @@
-import React, { Component } from 'react';
-
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import React, { Component } from 'react'
+import axios from 'axios'
+import './App.css'
+import SmurfForm from './components/SmurfForm'
+import Smurfs from './components/Smurfs'
 
 class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      smurfs: [],
-    };
+      smurfs: []
+    }
   }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  componentDidMount() {
+    axios
+      .get('/smurfs')
+      .then(res => this.setState({ smurfs: res.data }))
+      .catch(err => console.log(err))
+  }
+
   render() {
+    const { smurfs } = this.state
+
     return (
       <div className="App">
         <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Smurfs smurfs={smurfs} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
