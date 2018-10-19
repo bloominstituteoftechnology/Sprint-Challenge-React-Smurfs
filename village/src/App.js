3444b9b7ee577e8d9901c11dcad46b5dbcc7a6b9
Lajawanti <lajawanti.dhake@gmail.com>
2018-10-19 11:41:06 -0400

    axios.get() Done displaying smurfs

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -6,19 +7,34 @@ import Smurfs from './components/Smurfs';
 
 class App extends Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      smurfs: [],
-    };
+      super(props);
+      this.state = {
+          smurfs: [],
+      };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+  componentDidMount() {
+     console.log("In CDM");
+     axios
+      .get('http://localhost:3333/smurfs')
+      .then(response => this.setState({ smurfs : response.data }))
+      .catch(error => console.log(error));
+  }
+
+  updateSmurfs = (smurf) => {
+      this.setState({ smurfs : smurf })
+  }
+
+
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <SmurfForm 
+            updateSmurfs = {this.updateSmurfs}
+        />
+        <Smurfs smurfs = {this.state.smurfs} />
       </div>
     );
   }
