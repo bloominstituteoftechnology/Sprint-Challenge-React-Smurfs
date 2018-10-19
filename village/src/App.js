9d4e19f30f0656e501b63ad08cab4570c502e91a
Hunter315 <hunter.mckay.smith@gmail.com>
2018-10-19 10:04:21 -0600

    route path in app.js to Smurfs and SmurfForm

--- village/src/App.js
+++ village/src/App.js
@@ -27,8 +27,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+      
+      <Route path="/smurf-form"
+        render = {props =>
+          <SmurfForm 
+            {...props} /> } 
+      />
+
+      <Route exact path="/"
+        render={props =>
+          <Smurfs 
+            {...props}
+            smurfs={this.state.smurfs} /> } 
+        />
       </div>
     );
   }
