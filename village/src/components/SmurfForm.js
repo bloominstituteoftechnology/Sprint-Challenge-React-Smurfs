9aa9b05c7b76fb288d504ba44da279fd4d6ce211
Brad Mortensen <b.mort86@gmail.com>
2018-10-19 08:56:02 -0600

    initial commit

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -13,12 +14,20 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
-
+    axios
+      .post("http://localhost:3333/smurfs", {
+        name: this.state.name,
+        age: this.state.age,
+        height: this.state.height
+      })
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
     this.setState({
       name: '',
       age: '',
       height: ''
     });
+    window.location.reload();
   }
 
   handleInputChange = e => {
@@ -47,7 +56,7 @@ class SmurfForm extends Component {
             value={this.state.height}
             name="height"
           />
-          <button type="submit">Add to the village</button>
+          <button onClick={this.addSmurf} type="submit">Add to the village</button>
         </form>
       </div>
     );
