bb3a84ee7f59182b8e4c0d6710a809d0d535c9b9
Blake <koenneckerb@gmail.com>
2018-10-19 08:36:14 -0700

    Answers.md complete.

--- village/src/App.js
+++ village/src/App.js
@@ -11,6 +11,10 @@ class App extends Component {
       smurfs: [],
     };
   }
+  componentDidMount() {
+    
+  }
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
