4132d7821e5c5a46f543bcc6c3e0c1c484526978
frankbenevento <frankbenevento123@gmail.com>
2018-10-19 09:29:48 -0700

    i have a working post and a working get. committing

--- village/src/App.js
+++ village/src/App.js
@@ -3,21 +3,59 @@ import React, { Component } from 'react';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import Axios from 'axios';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       smurfs: [],
+      name: '',
+      age: '',
+      height: ''
+
     };
   }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  handleNewSmurf = event =>{
+
+    let newSmurf = {
+                    name:  this.state.name,
+                    age:   this.state.age,
+                    height:this.state.height     
+                   }
+
+    Axios.post('http://localhost:3333/smurfs',newSmurf)
+    this.setState({
+      name: '',
+      age: '',
+      height: ''
+    });
+    this.getAllSmurfs();
+  }
+
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  getAllSmurfs(){
+    return Axios.get('http://localhost:3333/smurfs')
+    .then((res)=>{
+      this.setState({smurfs:res.data},()=>{console.log('whats on state? ', this.state.smurfs)})
+    })
+    .catch(error => {console.log('an error occured', error)})
+  }
+  componentDidMount(){
+    this.getAllSmurfs();
+  }
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm handleInputChange={this.handleInputChange} 
+                   handleNewSmurf = {this.handleNewSmurf}
+                   age={this.state.age} 
+                   height={this.state.height} 
+                   name={this.state.name} 
+                   />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -3,9 +3,7 @@ import React from 'react';
 const Smurf = props => {
   return (
     <div className="Smurf">
-      <h3>{props.name}</h3>
-      <strong>{props.height} tall</strong>
-      <p>{props.age} smurf years old</p>
+      {props.name}{props.height}{props.age}
     </div>
   );
 };
