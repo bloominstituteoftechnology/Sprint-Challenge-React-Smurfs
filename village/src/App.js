7aa8844f8972395d7f504ae56600e256f4eb17be
Matthew Wright <matthewbwright@gmail.com>
2018-10-19 09:12:59 -0700

    adds routing

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, NavLink } from 'react-router-dom';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 import axios from 'axios';
@@ -30,11 +31,38 @@ class App extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+
   render() {
     return (
       <div className="App">
-        <SmurfForm smurfs={this.state.smurfs} />
-        <Smurfs smurfs={this.state.smurfs} />
+        <div className="nav-bar">
+          <NavLink
+            exact
+            to="/smurfs"
+            className="nav-button"
+            activeClassName="active-nav-button"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            exact
+            to="/smurf-form"
+            className="nav-button"
+            activeClassName="active-nav-button"
+          >
+            Add a Smurf!
+          </NavLink>
+        </div>
+        <Route
+          exact
+          path="/smurf-form"
+          render={props => <SmurfForm {...props} smurfs={this.state.smurfs} />}
+        />
+        <Route
+          exact
+          path="/smurfs"
+          render={props => <Smurfs {...props} smurfs={this.state.smurfs} />}
+        />
       </div>
     );
   }
