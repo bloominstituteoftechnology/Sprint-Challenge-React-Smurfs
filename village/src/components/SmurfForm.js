74dfb272df42e02902751f01a0046e5ca636ec51
Blake <koenneckerb@gmail.com>
2018-10-19 09:06:57 -0700

    Implemented GET and POST methods with axios.

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,25 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
 class SmurfForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      age: '',
-      height: ''
+      name: "",
+      age: "",
+      height: ""
     };
   }
 
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
-
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
-  }
+    axios
+      .post("http://localhost:3333/smurfs", this.state)
+      .then(() =>
+        this.setState({
+          name: "",
+          age: "",
+          height: ""
+        })
+      )
+      .catch(error => console.log(error));
+    window.location.reload();
+  };
 
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
