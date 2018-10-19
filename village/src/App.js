efc3fe9500e9160ccd276bcff1d3ae9377ae1fb3
Carlos Lantigua <clantigua954@gmail.com>
2018-10-19 11:52:22 -0400

    completed MVP, need to figure out how to add smurf without needing to refresh

--- village/src/App.js
+++ village/src/App.js
@@ -9,24 +9,37 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			smurfs: []
+			smurfs: [],
+			smurf: props.smurf
 		};
 	}
 
 	componentDidMount() {
-		axios.get('http://localhost:3333/smurfs', this.state.smurf).then((res) => {
-			this.setState({
-				smurfs: res.data
-			});
-		});
+		axios
+			.get('http://localhost:3333/smurfs', this.state.smurf)
+			.then((res) => {
+				this.setState({
+					smurfs: res.data,
+					smurf: {
+						name: '',
+						age: '',
+						height: ''
+					}
+				});
+			})
+			.catch((err) => console.log(err));
 	}
+
 	// add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
 	// Notice what your map function is looping over and returning inside of Smurfs.
 	// You'll need to make sure you have the right properties on state and pass them down to props.
 	render() {
 		return (
 			<div className="App">
-				<SmurfForm />
+				<Route
+					path="/"
+					render={(props) => <SmurfForm {...props} smurfs={this.state.smurfs} addSmurf={this.addSmurf} />}
+				/>
 				<Route path="/" render={(props) => <Smurfs {...props} smurfs={this.state.smurfs} />} />
 			</div>
 		);
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,57 +1,52 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 class SmurfForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      age: '',
-      height: ''
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: '',
+			age: '',
+			height: ''
+		};
+	}
 
-  addSmurf = event => {
-    event.preventDefault();
-    // add code to create the smurf using the api
+	addSmurf = (event) => {
+		event.preventDefault();
+		// add code to create the smurf using the api
+		Axios.post('http://localhost:3333/smurfs', this.state)
+			.then((res) => {
+				this.setState({
+					smurfs: res.data,
+					name: '',
+					age: '',
+					height: ''
+				});
+			})
+			.catch((err) => console.log(err));
+	};
 
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
-  }
+	handleInputChange = (e) => {
+		this.setState({ [e.target.name]: e.target.value });
+	};
 
-  handleInputChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
-  render() {
-    return (
-      <div className="SmurfForm">
-        <form onSubmit={this.addSmurf}>
-          <input
-            onChange={this.handleInputChange}
-            placeholder="name"
-            value={this.state.name}
-            name="name"
-          />
-          <input
-            onChange={this.handleInputChange}
-            placeholder="age"
-            value={this.state.age}
-            name="age"
-          />
-          <input
-            onChange={this.handleInputChange}
-            placeholder="height"
-            value={this.state.height}
-            name="height"
-          />
-          <button type="submit">Add to the village</button>
-        </form>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="SmurfForm">
+				<form onSubmit={this.addSmurf} smurf={this.state}>
+					<input onChange={this.handleInputChange} placeholder="name" value={this.state.name} name="name" />
+					<input onChange={this.handleInputChange} placeholder="age" value={this.state.age} name="age" />
+					<input
+						onChange={this.handleInputChange}
+						placeholder="height"
+						value={this.state.height}
+						name="height"
+					/>
+					<button type="submit">Add to the village</button>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default SmurfForm;
