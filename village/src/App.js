77ce0e1fb9b94159f201ecec8b18dd2bdd3b52b2
Angelo Deleon <rudy209@comcast.net>
2018-10-19 10:10:18 -0700

    trying to do stretch

--- village/src/App.js
+++ village/src/App.js
@@ -5,11 +5,13 @@ import {Route, NavLink} from 'react-router-dom';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import Smurf from './components/Smurf';
 
 
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+
   class App extends Component {
     constructor(props) {
       super(props);
@@ -32,7 +34,8 @@ import Smurfs from './components/Smurfs';
         console.log('Error', err);
       });
     }
-  
+
+    // STRETCH: delete smurfs
     handleDelete = smurf => {
       return axios.delete(`http://localhost:3333/smurfs/${smurf.id}`)
         .then(response => this.setState({ smurfs: response.data }));
--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -8,6 +8,10 @@ const Smurf = props => {
       <p>{props.age} smurf years old</p>
     </div>
   );
+  //   function handleDelete() {
+  //   props.handleDelete(props.smurfs.id);
+  //   props.history.push('/smurfs');
+  // };
 };
 
 Smurf.defaultProps = {
@@ -18,3 +22,33 @@ Smurf.defaultProps = {
 
 export default Smurf;
 
+// import React from 'react';
+
+// function Smurf (props) {
+//     console.log(props);
+//     const smurf = props.smurfs.find(
+//       smurf => `${smurf.id}` === props.match.params.id
+//     );
+//   // STRETCH: Delete smurfs
+//   function handleDelete() {
+//     props.handleDelete(props.smurfs.id);
+//     props.history.push('/smurfs');
+//   };
+ 
+//   return (
+//     <div className="Smurf">
+//       <h3>{props.name}</h3>
+//       <strong>{props.height} tall</strong>
+//       <p>{props.age} smurf years old</p>
+//       <button onClick={handleDelete} >Delete</button>
+//     </div>
+//   );
+// };
+
+// Smurf.defaultProps = {
+//   name: '',
+//   height: '',
+//   age: ''
+// };
+
+// export default Smurf;
