2e13b29516d7420cfa98c0868f0b7c38443e19e2
Katie Cleary <katiecleary@airmaster.lan>
2018-10-19 12:06:36 -0400

    created GET and POST calls in app and smurform

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -13,13 +14,31 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
+    axios     
+    .post('http://localhost:3333/smurfs',  
+    {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height
+    })
 
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
+    .then(response => {
+      console.log(response);
+      console.log(response.data);
+      this.setState(() => ({ 
+        name: '',
+        age: '',
+        height: ''
+        }));
+      })
+
+    .catch(error => {
+      console.error('Server Error', error);
     });
-  }
+    }
+
+  
+
 
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
