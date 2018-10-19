172a93b90cacd5baf157ed566678d9bccffb29a5
Drewsup123c <drewsup123c@gmail.com>
2018-10-19 11:14:40 -0600

    added an exact route onto Smurfs component set to '/'

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Route } from 'react-router-dom';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -57,9 +58,7 @@ class App extends Component {
                 addSmurf = {this.addSmurf}
                 handleInputChange = {this.handleInputChange}
         />
-        <Smurfs 
-        smurfs={this.state.smurfs}
-        />
+        <Route exact path='/' render={props=><Smurfs smurfs={this.state.smurfs} />} />
       </div>
     );
   }
