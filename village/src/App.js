32233bf3e9c20a1cf61698d0aeeaf686c20cd21f
samsisle <samko9522@gmail.com>
2018-10-19 08:29:32 -0700

    Add GET functionality

--- village/src/App.js
+++ village/src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
-import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      smurfs: [],
-    };
+    this.state = { smurfs: [] };
+    this.url = 'http://localhost:3333/smurfs';
   }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  componentDidMount() {
+    this.getSmurfs(this.url);
+  }
+
+  getSmurfs = URL => {
+    axios
+      .get(URL)
+      .then(res => this.setState({ smurfs: res.data }))
+      .catch(err => console.log(err));
+  };
+
   render() {
     return (
       <div className="App">
