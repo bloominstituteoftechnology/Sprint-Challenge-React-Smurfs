8da905a3102fd2302c3bdb072af1e211cfb302dc
Drewsup123c <drewsup123c@gmail.com>
2018-10-19 10:46:20 -0600

    add functionality sort of complete, keep getting random errors

--- village/src/App.js
+++ village/src/App.js
@@ -11,9 +11,9 @@ class App extends Component {
     this.state = {
       smurfs: [],
       newSmurf : {
-        name : '',
+        name: '',
+        age : '',
         height : '',
-        age : ''
       },
     };
   }
@@ -23,14 +23,43 @@ class App extends Component {
       .then(response => this.setState({smurfs : response.data}))
       .catch(error => console.log(error));
   }
+
+  addSmurf = event => {
+    event.preventDefault();
+    // add code to create the smurf using the api
+    axios.post('http://localhost:3333/smurfs', this.state.newSmurf)
+    .then(response => this.setState({smurfs : response.data}))
+    .catch(error => console.log(error));
+
+    this.setState({newSmurf : {
+      name: '',
+      age: '',
+      height: ''
+    }});
+  }
+
+  handleInputChange = e => {
+    this.setState(
+      {newSmurf : { 
+        ...this.state.newSmurf,
+        [e.target.name]: e.target.value 
+      }});
+      console.log(this.state.newSmurf);
+  };
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <SmurfForm 
+                newSmurf = {this.state.newSmurf}
+                addSmurf = {this.addSmurf}
+                handleInputChange = {this.handleInputChange}
+        />
+        <Smurfs 
+        smurfs={this.state.smurfs}
+        />
       </div>
     );
   }
