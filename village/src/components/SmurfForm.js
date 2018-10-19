f43fe2cf90cda1f4e3d2a594ade59d7e2c99ce2b
David247365 <ds051128@gmail.com>
2018-10-19 11:36:10 -0500

    retrieving the smurf data as well as adding smurf to the SMurf DB

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,25 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
 class SmurfForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      age: '',
-      height: ''
+      name: "",
+      age: "",
+      height: ""
     };
   }
 
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
+    let smurf = { ...this.state };
+    axios.post("http://localhost:3333/smurfs", smurf).then(response => {
+      this.setState({
+        smurf: response.data
+      });
+      window.location.reload();
+    });
 
     this.setState({
-      name: '',
-      age: '',
-      height: ''
+      name: "",
+      age: "",
+      height: ""
     });
-  }
+  };
 
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
