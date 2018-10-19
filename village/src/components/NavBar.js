d15f6512d0ef758c8e54e6689d5cbc941822425c
Carlos Lantigua <clantigua954@gmail.com>
2018-10-19 13:17:56 -0400

    added NavLink

--- village/src/components/NavBar.js
+++ village/src/components/NavBar.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = (props) => {
 	return (
 		<div className="navbar-links">
-			<Link to={props.location}>{props.name}</Link>
+			<NavLink to={props.location}>{props.name}</NavLink>
 		</div>
 	);
 };
