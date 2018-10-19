ac91165694c5cc40bf1088139715781a6e44f388
Hunter315 <hunter.mckay.smith@gmail.com>
2018-10-19 10:09:00 -0600

    handleChange function created in app.js and passed to component props

--- village/src/App.js
+++ village/src/App.js
@@ -21,24 +21,37 @@ class App extends Component {
       })
       .catch(err => console.log(err));
   }
+
+  handleChange = () => {
+    axios 
+      .post('http://localhost:3333/smurfs')
+      .then(res => {
+        this.setState({ smurfs: res.data });
+      })
+      .catch(err => console.log(err));
+  }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
       <div className="App">
-      
+
       <Route path="/smurf-form"
         render = {props =>
           <SmurfForm 
-            {...props} /> } 
+            {...props}
+            componentDidMount={this.componentDidMount}
+            handleChange={this.handleChange}
+             /> } 
       />
 
       <Route exact path="/"
         render={props =>
           <Smurfs 
             {...props}
-            smurfs={this.state.smurfs} /> } 
+            smurfs={this.state.smurfs}
+            handleChange={this.handleChange} /> } 
         />
       </div>
     );
