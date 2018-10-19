73f11a91bc133fc70c28eafbc4e3d9d2fe7a2868
JacobABrennan <jacobabrennan@gmail.com>
2018-10-19 13:12:41 -0400

    Add basic routing

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axois from 'axios';
+import {Route} from 'react-router-dom';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -25,8 +26,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm onSubmit={this.update} />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Route exact path="/" render={props => (
+          <Smurfs smurfs={this.state.smurfs} />
+        )} />
+        <Route path="/smurf-form" render={props => (
+          <SmurfForm onSubmit={this.update} />
+        )} />
       </div>
     );
   }
