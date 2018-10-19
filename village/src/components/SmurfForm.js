a29dbf69c386d7ad14d59b5907bce69fc8fd9523
Matthew Wright <matthewbwright@gmail.com>
2018-10-19 08:59:31 -0700

    creates post functionality

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+const blankSmurfForm = {
+  name: '',
+  age: '',
+  height: ''
+};
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -13,13 +20,25 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
+    const newSmurf = {
+      name: this.state.name,
+      age: Number(this.state.age, 10),
+      height: this.state.height
+    };
+
+    axios
+      .post('http://localhost:3333/smurfs', newSmurf)
+      .then(response => {
+        this.setState({ smurfs: response.data, smurf: blankSmurfForm });
+      })
+      .catch(err => console.log(err));
 
     this.setState({
       name: '',
       age: '',
       height: ''
     });
-  }
+  };
 
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
