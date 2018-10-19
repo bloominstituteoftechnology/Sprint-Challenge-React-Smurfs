20337d76f198cd6cf62ed7747af3af180165c8a7
Chad Jemmett <chad.jemmett@gmail.com>
2018-10-19 09:57:27 -0600

    can list new smurf after post request

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -13,6 +14,9 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
+    axios.post('http://localhost:3333/smurfs', {name: this.state.name,  age: this.state.age, height:  this.state.height})
+      .then((response) => this.props.newSmurf(response.data) )
+      .catch((error)=> console.log(error))
 
     this.setState({
       name: '',
@@ -22,6 +26,7 @@ class SmurfForm extends Component {
   }
 
   handleInputChange = e => {
+    console.log(e.target.value)
     this.setState({ [e.target.name]: e.target.value });
   };
 
