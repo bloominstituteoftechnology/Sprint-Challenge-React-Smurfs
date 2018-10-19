2ae9f012092dcc07fd48a30862b8d49c9adc7a00
MEBonham <programmer@mckaybonham.com>
2018-10-19 12:14:31 -0600

    Latest update for help

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -6,13 +6,15 @@ class Smurf extends Component {
     this.state = {
       name: props.name,
       height: props.height,
-      age: props.age
+      age: props.age,
+      id: props.id
     };
   }
 
   componentDidMount() {
-    if (this.props.name === undefined) {
-      const smurfInfo = this.props.smurfs.find(smurf => smurf.id === this.props.match.params.id);
+    if (this.props.name === undefined && this.props.smurfs.length) {
+      console.log(this.props.match.params.id, this.props.smurfs);
+      const smurfInfo = this.props.smurfs.find(obj => obj.id === this.props.match.params.id);
       console.log("Testing ...", smurfInfo);
       this.setState({
         name: smurfInfo.name,
@@ -28,7 +30,7 @@ class Smurf extends Component {
         <div className="first-line">
           <img
             onClick={event => {
-              this.props.deleteSmurf(event, this.props.id);
+              this.props.deleteSmurf(event, this.state.id);
             }}
             className="delX"
             src="./images/Red_x.png"
