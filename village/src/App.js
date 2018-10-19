55f8818fae3408c4b0db75a9701a9748e7c05855
txiong000 <tkx422@gmail.com>
2018-10-19 10:20:04 -0500

    Toua react smurf sprint challenge, retrieved data from server

--- village/src/App.js
+++ village/src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import Axios from 'axios';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+  componentDidMount() {
+    axios.get('http://localhost:3333/smurfs')
+          .then(res => { this.setState({ smurfs: res.data })
+          })
+          .catch(err => console.log(err))
+  }
+
   render() {
     return (
       <div className="App">
