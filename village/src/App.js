519b879d8098f905c3bea6e6d0bb61aaa4f8f7be
unknown <quan.nguyen199712@gmail.com>
2018-10-19 12:47:11 -0400

    smurfs displayed correctly

--- village/src/App.js
+++ village/src/App.js
@@ -1,23 +1,29 @@
-import React, { Component } from 'react';
-
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
   }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
+  componentDidMount() {
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(response => this.setState({ smurfs: response.data }))
+      .catch(error => console.log(error));
+  }
+  setNewSmurfState(smurfData) {
+    this.setState({ smurfs: smurtData });
+  }
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm setNewSmurfState={this.setNewSmurfState} />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
