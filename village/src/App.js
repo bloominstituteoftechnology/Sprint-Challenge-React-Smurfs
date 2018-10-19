4c68d79109891e1ee1b8db998b25864efd61dd3e
Hargobind Atwal <ethaipath@gmail.com>
2018-10-19 10:16:45 -0700

    added routes and nav bar (MVP)

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Route, NavLink } from 'react-router-dom';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -37,8 +38,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm addSmurf={this.addSmurf}/>
-        <Smurfs smurfs={this.state.smurfs} />
+        <div className="nav-bar">
+          <NavLink to='/'>Home</NavLink>
+          <NavLink exact to='/smurf-form'>Add Smurf</NavLink>
+        </div>
+        <Route path='/smurf-form' render={props => <SmurfForm addSmurf={this.addSmurf} {...props}/>}/>
+        <Route exact path='/' render={props => <Smurfs smurfs={this.state.smurfs} {...props}/>}/>
+        
       </div>
     );
   }
