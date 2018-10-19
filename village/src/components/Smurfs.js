af66d7283525f7b45f422c35670b0dcde43f5bb2
SeanOne <basement365@gmail.com>
2018-10-19 09:02:21 -0700

    added initial data get

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 
 import Smurf from './Smurf';
 
@@ -25,8 +26,8 @@ class Smurfs extends Component {
   }
 }
 
-Smurf.defaultProps = {
- smurfs: [],
+Smurf.propTypes = {
+ smurfs: PropTypes.array
 };
 
 export default Smurfs;
