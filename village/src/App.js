eb0feb8238fb70f334104bda8e91ee1bbc54ab0e
pedro <pedroluismontesinosrivera@gmail.com>
2018-10-19 13:35:21 -0400

    added a navigation bar

--- village/src/App.js
+++ village/src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
-import {Route, Link} from "react-router-dom"
+import {Route, NavLink} from "react-router-dom"
 
 class App extends Component {
   constructor(props) {
@@ -22,10 +22,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="navbar">
+          <NavLink to="/">Smurfs</NavLink>
+          <NavLink to="/smurfs-form">Create Smurf</NavLink>
+        </div>
         <Route exact path="/" render={props => <Smurfs {...props} smurfs={this.state.smurfs}/>}/>
-        <Route path="/smurf-form" component={SmurfForm}/>
-        {/*<SmurfForm />*/}
-        {/*<Smurfs smurfs={this.state.smurfs} />*/}
+        <Route path="/smurfs-form" component={SmurfForm}/>
       </div>
     );
   }
