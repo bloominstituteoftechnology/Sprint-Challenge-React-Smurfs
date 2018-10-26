86223d5c015355b6933583f2d4784660a4649fc2
mikaelacurrier <mikaela.currier@gmail.com>
2018-10-26 11:18:12 -0400

    add smurf functional but weird

--- village/src/App.js
+++ village/src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route path='/' render={(props) => <SmurfForm {...props} />} />
+        <SmurfForm />
         <Route path='/' render={(props) => <Smurfs {...props} smurfs={this.state.smurfs}/>}/>
       </div>
     );
