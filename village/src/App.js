e142361b47f8a0581cc1947ed065078c923fcfbf
mm2079 <vayne3252@gmail.com>
2018-10-26 11:54:58 -0400

    Useless commit

--- village/src/App.js
+++ village/src/App.js
@@ -10,7 +10,8 @@ class App extends Component {
     this.state = {
       smurfs: [],
     };
-  }
+  }]
+  //
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
