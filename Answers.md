bd3f177a79f00dab9ef1687abce690ab7732c32b
mm2079 <vayne3252@gmail.com>
2018-10-26 13:52:39 -0400

    MVP Done

--- Answers.md
+++ Answers.md
@@ -1,5 +1,17 @@
 1.  Explain the differences between `client-side routing` and `server-side routing`.
+    'Server-side routing' prompts a web page refresh when servers sends the data back to the user. Since new request is made for information which re-paints the web page. Only the information that is requested is returned unlike "Client-side routing" which returns the entire data when a new request is made. "Client-side routing" refers to routing being done internally through components. It doesn't prompt a page refresh since the new request fetches data from the state that is already available to us.
+
 1.  What does HTTP stand for?
+    HTTP is a network protocol utilized by web clients that allows them to communicate to web servers. It stands for HyperText Transfer Protocol.
+
 1.  What does CRUD stand for?
+    Create Read Update Delete
+
 1.  Which HTTP methods can be mapped to the CRUD acronym that we use when interfacing with APIs/Servers.
+    POST request---Create
+    GET request----Read
+    PUT request----Update
+    DELETE request-Delete
+
 1.  Mention three tools we can use to make AJAX requests
+    
--- village/src/App.js
+++ village/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-
+import axios from "axios";
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import { Route, NavLink } from 'react-router-dom';
+
 
 class App extends Component {
   constructor(props) {
@@ -10,16 +12,33 @@ class App extends Component {
     this.state = {
       smurfs: [],
     };
-  }]
-  //
+    this.setSmurfsState = this.setSmurfsState.bind( this );
+  }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+  componentDidMount()
+  {
+    axios
+      .get( "http://localhost:3333/smurfs" )
+      .then( response => this.setState( { smurfs: response.data } ) )
+      .catch( error => console.log( error ) ); 
+  }
+
+  setSmurfsState( data )
+  {
+    this.setState( { smurfs: data } );
+  }
+
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <NavLink to = "/">Home</NavLink>
+        <NavLink to = "/smurf-form"> Add Smurf</NavLink>
+        <Route exact path = "/" render = { () => <Smurfs smurfs = { this.state.smurfs }/> }/>
+        <Route path = "/smurf-form" render = { () => <SmurfForm setSmurfsState = { this.setSmurfsState }/> } />
+        {/* <SmurfForm setSmurfsState = { this.setSmurfsState }/>
+        <Smurfs smurfs={this.state.smurfs} /> */}
       </div>
     );
   }
