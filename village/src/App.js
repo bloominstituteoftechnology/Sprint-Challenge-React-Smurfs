76dfee4ffcf5754f4423bccd88805ae5ddf33917
Levi Appenfelder <levi.appenfelder@gmail.com>
2018-10-27 11:14:22 -0600

    axios.get implemented
    I retrieved data using axios.get()

--- village/src/App.js
+++ village/src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
-
+import axios from '../node_modules/axios';
 class App extends Component {
   constructor(props) {
     super(props);
@@ -13,7 +13,10 @@ class App extends Component {
   }
 
   componentDidMount() {
-
+    axios
+    .get("http://localhost:3333/smurfs")
+    .then(response => {this.setState({smurfs: response.data})})
+    .catch(err => console.log(err))
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
