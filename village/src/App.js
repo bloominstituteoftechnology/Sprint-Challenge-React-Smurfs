66bea2282af4d09a99222a5aa14441d452dfd0a8
Jason Hedrick <jasonleehedrick@gmail.com>
2018-10-27 09:20:13 -0700

    Added in the get and post methods. Passed along the new smurf data to app.js so our state can properly refresh when adding a new smurf.

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -11,13 +12,32 @@ class App extends Component {
       smurfs: [],
     };
   }
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
+  componentDidMount() {
+    // Request data to read 
+    axios.get('http://localhost:3333/smurfs/')
+    .then(res => {
+      this.setState({
+        smurfs: res.data, // Update our app's state with the new data.
+      })
+    })
+    .catch(err => console.log(err))
+  }
+  
+  updateSmurfData = (newSmurfData) => {
+    this.setState({
+      smurfs: newSmurfData,
+    })
+  }
+
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+      {/* Let's edit SmurfForm and pass along an update function for the returned smurf data when we create a new smurf. */}
+        <SmurfForm updateSmurfData={this.updateSmurfData}/>
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
