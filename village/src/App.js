3564b77ffdfda423d8f2d57164b5cebceea31f06
Dewayne87 <dewaynelindsay87@gmail.com>
2018-10-27 09:20:31 -0700

    retrieved smurfs data with get request

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
@@ -14,7 +14,35 @@ class App extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+  componentDidMount() {
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(response => {
+        this.setState({ smurfs: response.data });
+      })
+      .catch(error => {
+        console.error('Server Error', error);
+
+      });
+  }
+  addNewSmurf = friend => {
+
+
+    axios.post('http://localhost:5000/friends', friend)
+      .then((response) => {
+        console.log(response);
+        this.setState(() => ({ friendlist: response.data }));
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+    console.log(friend)
+  }
+
+
   render() {
+    console.log(this.state)
     return (
       <div className="App">
         <SmurfForm />
