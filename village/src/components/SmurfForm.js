cbd3070c84a1edb5cf112e7c4607766f9d5ac80d
Willie Yturralde <willieino@hotmail.com>
2018-10-27 10:49:17 -0700

    finished the Get and Post section

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from "axios";
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -13,7 +14,20 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
-
+    axios.post('http://localhost:3333/smurfs', {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height
+    })
+      .then(response => {
+        console.log("in the addSmurf", this.state)
+        this.setState(() => ({ smurfs: response.data }));
+        console.log("in the addSmurf", this.state)
+      })
+      .catch(error => {
+        console.log(error);
+      });
+      /********************************* */
     this.setState({
       name: '',
       age: '',
@@ -23,6 +37,7 @@ class SmurfForm extends Component {
 
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
+    console.log("this.state: ", this.state)
   };
 
   render() {
@@ -34,18 +49,21 @@ class SmurfForm extends Component {
             placeholder="name"
             value={this.state.name}
             name="name"
-          />
+          className="input"
+         />
           <input
             onChange={this.handleInputChange}
             placeholder="age"
             value={this.state.age}
             name="age"
+            className="input"
           />
           <input
             onChange={this.handleInputChange}
             placeholder="height"
             value={this.state.height}
             name="height"
+            className="input"
           />
           <button type="submit">Add to the village</button>
         </form>
