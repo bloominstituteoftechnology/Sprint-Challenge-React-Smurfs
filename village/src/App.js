53f5693f91aeaec3d5d2af246c41c865e0539755
Bruce Cabanayan <bcabanayan@Bruces-MacBook-Pro.local>
2018-10-27 09:45:08 -0700

    Added NavLinks to Smurfs and SmurfForm components

--- village/src/App.js
+++ village/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Route } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -37,6 +37,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavLink to='/smurfform'>Add a smurf to the village!</NavLink>
+        <br/>
+        <NavLink to='smurfs'>See the smurfs in the village!</NavLink>
         <Route
           path='/smurfform'
           render={props => 
