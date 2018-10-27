c6e93ca945c2bd4cedcddc40b0fc23fd91360f9e
Obed Lorisson <Obed.lorisson@gmail.com>
2018-10-27 02:17:34 -0400

    API design completed Added Get '/smurfs' to retrieve an array of all smurfs Added Post  '/smurfs' to add smurf to smurf Db

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -13,6 +14,15 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
+    Axios
+    .post('http://localhost:3333/smurfs',{
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height,
+    })
+    .then(response =>this.setState({ smurfs: response.data}))
+    .then(window.location.reload())
+    .catch(err => console.log(err))
 
     this.setState({
       name: '',
