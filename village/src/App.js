2456c4d758d305194c8218ede5377eda9914eb8c
Emmanium <pradoe.ep@gmail.com>
2018-10-27 11:29:15 -0600

    changed Links to NavLinks in App.js

--- village/src/App.js
+++ village/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Route } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -42,8 +42,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Link to="/">Home</Link>{" "}
-        <Link to="/smurf-form">SmurfForm</Link>
+        <NavLink to="/">Home</NavLink>{" "}
+        <NavLink to="/smurf-form">SmurfForm</NavLink>
 
         <Route exact path="/" render={props => <Smurfs {...props} smurfs={this.state.smurfs} />} />
         <Route path="/smurf-form" render={props => <SmurfForm {...props}
