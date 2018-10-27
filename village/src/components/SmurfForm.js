c6c4c5041b3b810ce373b56f3bb279502e4597f9
iepoch <staclem@live.com>
2018-10-27 11:52:18 -0700

    Update the look of the Add to village and centered it.

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,31 +1,30 @@
-import React, { Component } from 'react';
-import axios from "axios"
+import React, { Component } from "react";
+import { Col, FormGroup, Form, Input, Button, Row } from "reactstrap";
 class SmurfForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      age: '',
-      height: ''
+      name: "",
+      age: "",
+      height: ""
     };
   }
 
-  addSmurf = (event) => {
+  addSmurf = event => {
     event.preventDefault();
     const addSmurf = {
       name: this.state.name,
       age: this.state.age,
       height: this.state.height
-    }
+    };
 
-    this.props.addASmurf(addSmurf)
+    this.props.addASmurf(addSmurf);
     this.setState({
-      name: '',
-      age: '',
-      height: ''
-    })
-
-  }
+      name: "",
+      age: "",
+      height: ""
+    });
+  };
 
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
@@ -33,28 +32,40 @@ class SmurfForm extends Component {
 
   render() {
     return (
-      <div className="SmurfForm">
-        <form onSubmit={this.addSmurf}>
-          <input
-            onChange={this.handleInputChange}
-            placeholder="name"
-            value={this.state.name}
-            name="name"
-          />
-          <input
-            onChange={this.handleInputChange}
-            placeholder="age"
-            value={this.state.age}
-            name="age"
-          />
-          <input
-            onChange={this.handleInputChange}
-            placeholder="height"
-            value={this.state.height}
-            name="height"
-          />
-          <button type="submit">Add to the village</button>
-        </form>
+      <div className="d-flex justify-content-center">
+        <Form>
+          <Row form>
+            <Col lg={6}>
+              <form onSubmit={this.addSmurf}>
+                <FormGroup>
+                  <Input
+                    onChange={this.handleInputChange}
+                    placeholder="name"
+                    value={this.state.name}
+                    name="name"
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Input
+                    onChange={this.handleInputChange}
+                    placeholder="age"
+                    value={this.state.age}
+                    name="age"
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Input
+                    onChange={this.handleInputChange}
+                    placeholder="height"
+                    value={this.state.height}
+                    name="height"
+                  />
+                </FormGroup>
+                <Button type="submit">Add to the village</Button>
+              </form>
+            </Col>
+          </Row>
+        </Form>
       </div>
     );
   }
