b1c8a58a3d231b165b7d0b43ca5d5c40fbef0b5b
Tommy Collison <tommy@collison.ie>
2018-10-27 11:20:16 -0700

    delete functionality... sorta?

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Smurf from './Smurf';
 
 class Smurfs extends Component {
+
+  deleteSmurf = (event, id) => {
+    axios.delete(`http://localhost:3333/smurfs/${id}`)
+    .then (response => {
+      this.setState({smurfs: response.data})
+    })
+  }
+
   render() {
     return (
       <div className="Smurfs">
@@ -10,6 +19,7 @@ class Smurfs extends Component {
         <ul>
           {this.props.smurfs.map(smurf => {
             return (
+              <div className="smurfdiv">
               <Smurf
                 name={smurf.name}
                 id={smurf.id}
@@ -17,6 +27,9 @@ class Smurfs extends Component {
                 height={smurf.height}
                 key={smurf.id}
               />
+              <button type="x" onClick={event => this.deleteSmurf(event, smurf.id)}>Delete from the village</button>
+              </div>
+
             );
           })}
         </ul>
