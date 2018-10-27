75adcf4b647af7d38902f89e648819553fdceef1
Jess <jessdev83@gmail.com>
2018-10-27 13:09:07 -0400

    POST added, functioning

--- village/src/App.js
+++ village/src/App.js
@@ -8,7 +8,10 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: []
+      smurfs: [],
+      name: "",
+      age: "",
+      height: ""
     };
   }
 
@@ -20,13 +23,37 @@ class App extends Component {
       })
       .catch(err => console.log(err));
   }
+  addSmurf = event => {
+    event.preventDefault();
+    const newSmurf = {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height
+    };
+    axios
+      .post("http://localhost:3333/smurfs", newSmurf)
+      .then(response => {
+        this.setState({ smurfs: response.data, name: "", age: "", height: "" });
+      })
+      .catch(err => console.log(err));
+  };
+
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm
+          addSmurf={this.addSmurf}
+          handleInputChange={this.handleInputChange}
+          name={this.state.name}
+          age={this.state.age}
+          height={this.state.height}
+        />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
