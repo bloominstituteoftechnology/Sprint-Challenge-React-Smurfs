3c9fff507b9d98c2388f64c13343b7b8bc88a820
Jess <jessdev83@gmail.com>
2018-10-27 13:48:32 -0400

    Formatting

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,35 +1,44 @@
-import React, { Component } from "react";
-import { Route, Link, NavLink } from "react-router-dom";
+import React, {
+    Component
+} from "react";
 import Smurf from "./Smurf";
 
 class Smurfs extends Component {
-  render() {
-    return (
-      <div className="Smurfs">
-        <h1>Smurf Village</h1>
-        <ul>
-          {this.props.smurfs.map(smurf => {
-            return (
-              <Smurf
-                name={smurf.name}
-                id={smurf.id}
-                age={smurf.age}
-                height={smurf.height}
-                key={smurf.id}
-              />
-            );
-          })}
-        </ul>
-        <Link to={"/smurf-form"} className="link">
-          <div className="button">Add a Smurf!</div>
-        </Link>
-      </div>
-    );
-  }
+    render() {
+        return ( <
+            div className = "Smurfs" >
+            <
+            h1 > Smurf Village < /h1> <
+            ul > {
+                this.props.smurfs.map(smurf => {
+                    return ( <
+                        Smurf name = {
+                            smurf.name
+                        }
+                        id = {
+                            smurf.id
+                        }
+                        age = {
+                            smurf.age
+                        }
+                        height = {
+                            smurf.height
+                        }
+                        key = {
+                            smurf.id
+                        }
+                        />
+                    );
+                })
+            } <
+            /ul> < /
+            div >
+        );
+    }
 }
 
 Smurf.defaultProps = {
-  smurfs: []
+    smurfs: []
 };
 
 export default Smurfs;
