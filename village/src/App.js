a264f407d4cf1bd8f2e0ae46a3c7631478ce3890
Julie Jonak <juliejonak10@gmail.com>
2018-10-27 12:03:54 -0500

    Completed MVP. Router added

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {Route, NavLink} from 'react-router-dom';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
@@ -39,8 +40,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm addNewSmurf={this.addNewSmurf} />
-        <Smurfs smurfs={this.state.smurfs} />
+        <nav>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/smurf-form'>Add New Smurf</NavLink>
+        </nav>
+
+        <Route path='/smurf-form' render={(props)=> <SmurfForm {...props} addNewSmurf={this.addNewSmurf} />} />
+        <Route path='/' render={(props)=> <Smurfs {...props} smurfs={this.state.smurfs} /> } />
+
       </div>
     );
   }
