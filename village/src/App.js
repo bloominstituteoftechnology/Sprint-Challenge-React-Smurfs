fcf56f2c6e432572c491e29dec06c66998bd24a3
Louis <7.louislee@gmail.com>
2018-10-27 11:11:00 -0700

    last commit

--- village/src/App.js
+++ village/src/App.js
@@ -22,14 +22,18 @@ class App extends Component {
       })
       .catch(err => console.log(err));
   }
-
+  
   deleteSmurfHandler = (id) => {
     return () => {
       axios.delete(`http://localhost:3333/smurfs/${id}`)
-      .then( response => console.log(response))
+      .then( response => 
+        this.setState({
+          smurfs: response.data
+        }))
       .catch ( err => console.log(err))
     }
   }
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -5,6 +5,7 @@ class SmurfForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      smurfs: props.smurfs,
       name: "",
       age: "",
       height: ""
@@ -14,27 +15,28 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
-      // const newSmurf = {
-      //   id: this.props.smurfs.length + 1,
-      //   name: this.state.name,
-      //   age: this.state.age,
-      //   height: this.state.height
-      // };
-      axios
-        .post("http://localhost:3333/smurfs", { 
-          name: this.state.name,
-          age: this.state.age,
-          height: this.state.height
-         })
-        .then(response => {
-          console.log(response.data);
-          this.setState({
-            name: "",
-            age: "",
-            height: ""
-          })}
-        )
-        .catch(err => console.log(err));
+    // const newSmurf = {
+    //   id: this.props.smurfs.length + 1,
+    //   name: this.state.name,
+    //   age: this.state.age,
+    //   height: this.state.height
+    // };
+    axios
+      .post("http://localhost:3333/smurfs", {
+        name: this.state.name,
+        age: this.state.age,
+        height: this.state.height
+      })
+      .then(response => {
+        console.log(response.data);
+        this.setState({
+          smurfs: response.data,
+          name: "",
+          age: "",
+          height: ""
+        });
+      })
+      .catch(err => console.log(err));
   };
 
   handleInputChange = e => {
