d720e9794b1610c5db32c49bb760362a31494f16
Abigayle Franklin <abifanklin@gmail.com>
2018-10-27 13:56:19 -0500

    Added some styling. Final commit

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
 
+
+const SmurfWrapper = styled.div`
+  width: 300px;
+  height: 300px;
+  background-color: white;
+  border: 1px solid black;
+  border-radius: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 3px 3px 6px grey;
+  background-image: radial-gradient(#FF63477F 50%, transparent 51%), radial-gradient(#FF63477F 30%, transparent 31%);
+  background-size: 60px 60px;
+  background-position: 0 0, 30px 30px;
+  margin: 20px;
+`
 
 
 
@@ -18,13 +36,13 @@ class Smurf extends React.Component {
 
   render() {
   return (
-    <div className="Smurf">
+    <SmurfWrapper>
       <button onClick={this.deleteSmurf}>X</button>
       <h4>Smurf ID: {this.props.id}</h4>
       <h3>{this.props.name}</h3>
       <strong>{this.props.height} tall</strong>
       <p>{this.props.age} smurf years old</p>
-    </div>
+    </SmurfWrapper>
   );
 }
 };
