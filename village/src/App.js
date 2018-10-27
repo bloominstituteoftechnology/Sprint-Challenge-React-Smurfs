06ec67ca8123f48568c92434d39c1ccc9f73fdd6
BrendonKootz <brendonkootz@gmail.com>
2018-10-27 12:16:43 -0500

    Get/Post functionality in place

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
@@ -7,17 +7,39 @@ import Smurfs from './components/Smurfs';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      smurfs: [],
+    this.state = { 
+	smurfs: [] 
     };
+    this.url = 'http://localhost:3333/smurfs';
   }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
+  componentDidMount() {
+    this.getSmurfs(this.url);
+  }
+
+
+getSmurfs = URL => {
+	axios
+		.get(URL)
+		.then(res => this.setState({smurfs : res.data}))
+		.catch(err => console.log(err));
+};
+
+deleteSmurf = id => {
+	axios
+		.delete(`${this.url}/${id}`)
+		.then(res => this.setState({smurfs : res.data}))
+		.catch(err => console.log(err));
+};
+
+
+updateSmurfs = smurfs => {
+	this.setState({smurfs})
+};
+
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm updateSmurfs = {this.updateSmurfs} URL={this.url}/>
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
