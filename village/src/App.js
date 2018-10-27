0b8ec867fdb96628b94b00d859fab99bd40e90c8
FreeKeyBoy <jjhutton81@gmail.com>
2018-10-27 09:58:32 -0700

    Got the smurfing smurf array to smurfing render. Had to do a bit of debugging do to messed up cammel casing.

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
@@ -10,6 +10,12 @@ class App extends Component {
     this.state = {
       smurfs: [],
     };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3333/smurfs')
+      .then(res => this.setState({smurfs: res.data}))
+      .catch(err => console.log(err))
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
