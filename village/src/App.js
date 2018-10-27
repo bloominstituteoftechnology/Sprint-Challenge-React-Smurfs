0829ad34ca50f5fc4468573ca315b84cffea3001
Ryntak94 <ryan.matthews.professional@gmail.com>
2018-10-27 12:39:45 -0400

    Add post functionality for form

--- village/src/App.js
+++ village/src/App.js
@@ -14,21 +14,27 @@ class App extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
-  componentDidMoun()    {
+  componentDidMount()    {
       axios.get(`http://localhost:3333/smurfs`)
       .then(data    =>  this.setState((state)   =>  ({
-          smurfs: data,
+          smurfs: data.data
       })))
       .catch(err    =>  {
           console.log("In Catch", err);
       })
   }
 
+  updateSmurfs  =   (newSmurfList)  =>  {
+      this.setState((state) =>  ({
+          smurfs: newSmurfList.data,
+      }))
+  }
+
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <SmurfForm updateSmurfs={this.updateSmurfs}/>
+        <Smurfs smurfs={this.state.smurfs}/>
       </div>
     );
   }
