70fc45db6e6f002383e243ffa8f7cfeef9cf6f07
shannonhealy <sahealy525@gmail.com>
2018-10-27 12:49:17 -0400

    Installed all yarn packages and complete GET request

--- village/src/App.js
+++ village/src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import axios from 'axios'; /*Importing AXIOS to make AJAX requests*/
 
 class App extends Component {
   constructor(props) {
@@ -11,6 +12,14 @@ class App extends Component {
       smurfs: [],
     };
   }
+
+  componentDidMount(){ /*When the component loads*/
+    axios.get('http://localhost:3333/smurfs').then(response => { /*Making a get request to the server to get the smurfs array*/
+      this.setState({ 
+        smurfs: response.data /*Setting the data retrieved from the sever to the smurfs array on the state*/
+      })
+    }).catch(err => {console.log(err)}); /*If there is an error, log what the error was*/
+  };
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
