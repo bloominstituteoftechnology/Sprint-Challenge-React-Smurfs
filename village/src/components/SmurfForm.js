b23808af6644cee424b2e5155002f00da9c16da9
unknown <kyleteeter12@gmail.com>
2018-10-27 12:53:23 -0500

    Router added to application

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -10,9 +10,14 @@ class SmurfForm extends Component {
     };
   }
 
-  addSmurf = event => {
+  addSmurf = (event) => {
     event.preventDefault();
-    // add code to create the smurf using the api
+    const newSmurf = {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height,
+    }
+    this.props.addNewSmurf(newSmurf);
 
     this.setState({
       name: '',
