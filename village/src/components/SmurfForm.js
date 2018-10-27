2263dc6fc6329409b3d58c9bf72bbc4178b3704b
Mitchell Robles <mitchellr92@live.com>
2018-10-27 09:56:23 -0700

    created .get and .post requests. app displays list of smurfs and has the ability to add new ones

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -13,6 +14,13 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
+    const newSmurf = {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height
+    }
+
+    this.props.smurf(newSmurf)
 
     this.setState({
       name: '',
