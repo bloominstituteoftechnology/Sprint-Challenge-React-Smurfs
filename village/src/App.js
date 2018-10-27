9d1c36eaf888d7954f45f3508cbcfa2230bd4903
Bruce Cabanayan <bcabanayan@Bruces-MacBook-Pro.local>
2018-10-27 09:37:15 -0700

    Added Route to Smurfs component

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios'
+import axios from 'axios';
+import { Route } from 'react-router-dom';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -37,7 +38,15 @@ class App extends Component {
     return (
       <div className="App">
         <SmurfForm addSmurfToDB={this.addSmurfToDB} />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Route
+          path='/smurfs'
+          render={props => 
+            <Smurfs
+              {...props}
+              smurfs={this.state.smurfs}
+            />
+          }
+        />
       </div>
     );
   }
