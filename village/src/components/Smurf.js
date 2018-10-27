14f1008482f0cdd686651565c22fabfc4a895039
jbrock1981 <jbrock1981@gmail.com>
2018-10-27 13:11:48 -0500

    some styling

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const SmurfBox = styled.div`
+  border: 1px solid black;
+  width: 200px;
+  display: flex;
+  flex-direction: column;
+  margin: 2% auto;
+`
 
 const Smurf = props => {
   return (
-    <div className="Smurf">
+    <SmurfBox>
       <h3>{props.name}</h3>
       <strong>{props.height} tall</strong>
       <p>{props.age} smurf years old</p>
       <button onClick = {() => props.deleteSmurf(props.id)}>Delete</button>
-    </div>
+    </SmurfBox>
   );
 };
 
