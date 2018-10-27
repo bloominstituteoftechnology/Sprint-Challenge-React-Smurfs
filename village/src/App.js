6caa495d272d3a0ca87c824f5fd4b796c733bf03
Denise Escobar <descobar1490@gmail.com>
2018-10-27 13:12:22 -0500

    added delete functionality
    -I created a deleteHandler in my app component and passed it down the props to each indivdual smurf
    -I added a paragraph tag to serve as a delete button that onClick runs the deleteHandler

--- village/src/App.js
+++ village/src/App.js
@@ -24,6 +24,17 @@ class App extends Component {
       })
       .catch(err => console.log(err));
   }
+
+  deleteHandler = (id) =>{
+    return()=>{
+      axios
+        .delete(`http://localhost:3333/smurfs/${id}`)
+        .then(response => {
+          this.setState({smurfs:response.data})
+        })
+        .catch(err => console.log(err))
+    }
+  }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
@@ -35,7 +46,7 @@ class App extends Component {
           <NavLink to="/smurf-form">Add a Smurf</NavLink>  
         </div>
         <Route path="/smurf-form" component={SmurfForm} />
-        <Route exact path="/" render={(props) => (<Smurfs {...props} smurfs={this.state.smurfs}/>)} />
+        <Route exact path="/" render={(props) => (<Smurfs {...props} smurfs={this.state.smurfs} deleteHandler={this.deleteHandler}/>)} />
       </div>
     );
   }
--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -11,6 +11,7 @@ class Smurfs extends Component {
           {this.props.smurfs.map(smurf => {
             return (
               <Smurf
+                deleteHandler={this.props.deleteHandler}
                 name={smurf.name}
                 id={smurf.id}
                 age={smurf.age}
