8d6cb23754ec542864dd43691277f5f2abe23ff6
TexaCaliCoder <c.roth.lambda@gmail.com>
2018-10-27 11:50:18 -0500

    Sprint Challenge HTTP-AJAX intial commit for pull request completed Answers.md

--- village/src/App.js
+++ village/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+
 
 class App extends Component {
   constructor(props) {
@@ -10,6 +12,14 @@ class App extends Component {
     this.state = {
       smurfs: [],
     };
+  }
+  componentDidMount=()=>{
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(response => {
+        this.setState({ 
+        })
+      })
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
