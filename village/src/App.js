0b672830f6f9b3c4021789ffa467183af8e99815
Emmanium <pradoe.ep@gmail.com>
2018-10-27 11:04:04 -0600

    added working post request moved addSmurf function to App.js from SmurfForm.js passed down props from SmurfForm.js to App.js

--- village/src/App.js
+++ village/src/App.js
@@ -10,6 +10,9 @@ class App extends Component {
     super(props);
     this.state = {
       smurfs: [],
+      name: '',
+      age: '',
+      height: ''
     };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
@@ -22,10 +25,27 @@ class App extends Component {
       .catch(err => console.log(err))
   }
 
+  addSmurf = event => {
+    event.preventDefault();
+    // add code to create the smurf using the api
+    axios.post("http://localhost:3333/smurfs", {name: this.state.name, age: this.state.age, height: this.state.height})
+      .then(response => this.setState({ smurfs: response.data }))
+      .catch(err => console.log(err))
+  }
+
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm 
+          addSmurf={this.addSmurf} 
+          handleInputChange={this.handleInputChange} 
+          name={this.state.name} age={this.state.age} 
+          height={this.state.height} 
+        />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
