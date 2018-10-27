1c15d9f38030a4f891fb711fa9fa5644f8bcb2d7
Michael Trew <michaeltrew@Michaels-iMac.attlocal.net>
2018-10-27 13:34:46 -0400

    added Post, and Route Paths

--- village/src/App.js
+++ village/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 import axios from 'axios'
+import { Route, Link, NavLink } from 'react-router-dom'
 
 class App extends Component {
   constructor(props) {
@@ -18,14 +18,15 @@ class App extends Component {
       .then(response => this.setState({ smurfs: response.data }))
       .catch(err => console.log(err));
   }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
+  stateChangeHandler = (stateFromChild) => {
+    this.setState({ smurfs: stateFromChild })
+  }
+
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Route path="/smurf-form" render={props =>(<SmurfForm stateChangeHandler={this.stateChangeHandler}/>)} />
+        <Route exact path ="/" render= {props => (<Smurfs smurfs={this.state.smurfs}/>)} />
       </div>
     );
   }
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -12,7 +13,16 @@ class SmurfForm extends Component {
 
   addSmurf = event => {
     event.preventDefault();
-    // add code to create the smurf using the api
+    Axios
+      .post('http://localhost:3333/smurfs', {
+        name: this.state.name,
+        age: this.state.age,
+        height: this.state.height
+      })
+      .then(response => {
+        this.props.stateChangeHandler(response.data)
+      })
+      .catch(err => console.log(err));
 
     this.setState({
       name: '',
