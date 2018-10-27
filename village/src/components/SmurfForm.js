ac46700278882c1508149ffd7482cd5ef5c0b9b0
TheGameFreak720 <luis.alvarez@commithub.com>
2018-10-27 12:39:20 -0400

    you can post new smurfs but you need to reload to see the new one

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -13,12 +14,22 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
-
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
+    axios.post('http://localhost:3333/smurfs', {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height
+    })
+    .then(res => {
+      console.log(res.status);
+      this.setState({
+        name: '',
+        age: '',
+        height: ''
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }
 
   handleInputChange = e => {
