368eec4def24f490cf50a9d2681fc0496d72eaed
Tommy Collison <tommy@collison.ie>
2018-10-27 09:53:57 -0700

    fixed why smurfs weren't automatically appearing

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -12,7 +12,7 @@ class SmurfForm extends Component {
   }
 
   addSmurf = event => {
-    event.preventDefault();
+    // event.preventDefault();
     // add code to create the smurf using the api
     const newSmurf = {name: this.state.name, age: this.state.age, height: this.state.height}
 
