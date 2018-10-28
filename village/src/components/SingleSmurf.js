5a82c249bc592a7eb53abd71a983ceadeadf9519
Andrés Rivera <andres.rivera.toro@gmail.com>
2018-10-28 05:41:49 -0400

    DRY refactoring

--- village/src/components/SingleSmurf.js
+++ village/src/components/SingleSmurf.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 import Smurf from './Smurf';
+import SmurfForm from './SmurfForm';
 
 export default class SingleSmurf extends Component {
   constructor(props) {
@@ -26,7 +27,8 @@ export default class SingleSmurf extends Component {
       )
       .catch(err => console.log(err));
 
-    // Using GET because <Smurf /> doesn't render when using direct link http://localhost:3000/smurf/:id
+    // Using GET because <Smurf /> doesn't render when using direct link
+    // http://localhost:3000/smurf/:id
 
     // const smurf = this.props.smurfs.find(
     //   smurf => `${smurf.id}` === this.props.match.params.id
@@ -34,63 +36,26 @@ export default class SingleSmurf extends Component {
     // this.setState({ smurf });
   }
 
-  editSmurf = e => {
-    e.preventDefault();
-    const id = this.state.smurf.id;
-    this.props.editSmurf(id, {
-      name: this.state.name,
-      age: this.state.age,
-      height: this.state.height,
-    });
-    this.setState({
-      name: '',
-      age: '',
-      height: '',
-    });
-    this.props.history.push('/');
-  };
-
-  handleInputChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
   render() {
     return (
       <div>
-        <form onSubmit={this.editSmurf}>
-          <input
-            onChange={this.handleInputChange}
-            placeholder="name"
-            value={this.state.name}
-            name="name"
-          />
-          <input
-            onChange={this.handleInputChange}
-            placeholder="age"
-            value={this.state.age}
-            name="age"
-          />
-          <input
-            onChange={this.handleInputChange}
-            placeholder="height"
-            value={this.state.height}
-            name="height"
-          />
-          <button type="submit">Edit Smurf</button>
-        </form>
-
         {/* Checking for state here will prevent
         TypeError: Cannot read property 'id' of undefined
         when using direct link http://localhost:3000/smurf/:id */}
-
         {this.state.smurf ? (
-          <Smurf
-            id={this.state.smurf.id}
-            name={this.state.smurf.name}
-            age={this.state.smurf.age}
-            height={this.state.smurf.height}
-            goodbyeSmurf={this.props.goodbyeSmurf}
-          />
+          <React.Fragment>
+            <SmurfForm
+              id={this.state.smurf.id}
+              editSmurf={this.props.editSmurf}
+            />
+            <Smurf
+              id={this.state.smurf.id}
+              name={this.state.smurf.name}
+              age={this.state.smurf.age}
+              height={this.state.smurf.height}
+              goodbyeSmurf={this.props.goodbyeSmurf}
+            />
+          </React.Fragment>
         ) : null}
       </div>
     );
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import axios from 'axios';
 
 class SmurfForm extends Component {
@@ -37,10 +38,32 @@ class SmurfForm extends Component {
     this.setState({ [e.target.name]: e.target.value });
   };
 
+  editSmurf = e => {
+    e.preventDefault();
+    const id = this.props.id;
+    this.props.editSmurf(id, {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height,
+    });
+    this.setState({
+      name: '',
+      age: '',
+      height: '',
+    });
+    this.props.history.push('/');
+  };
+
   render() {
     return (
       <div className="SmurfForm">
-        <form onSubmit={this.addSmurf}>
+        <form
+          onSubmit={
+            this.props.location.pathname === '/smurf-form'
+              ? this.addSmurf
+              : this.editSmurf
+          }
+        >
           <input
             onChange={this.handleInputChange}
             placeholder="name"
@@ -59,11 +82,30 @@ class SmurfForm extends Component {
             value={this.state.height}
             name="height"
           />
-          <button type="submit">Add to the village</button>
+          {/* Adding a smurf:
+          User will be unable to submit form unless all input fields are 
+          populated. This prevents a 422 Unprocessable Entity error.*/}
+          {/* Editing a smurf:
+          User will be unable to submit form unless 1 or more input fields are 
+          populated. */}
+          <button
+            disabled={
+              this.props.location.pathname === '/smurf-form'
+                ? !this.state.name || !this.state.age || !this.state.height
+                : !this.state.name && !this.state.age && !this.state.height
+            }
+            type="submit"
+          >
+            {this.props.location.pathname === '/smurf-form'
+              ? `Add to the village`
+              : `Edit Smurf`}
+          </button>
         </form>
       </div>
     );
   }
 }
 
-export default SmurfForm;
+// withRouter is needed here to be able to use Route props, namely the history object, location object, and match object.
+
+export default withRouter(SmurfForm);
