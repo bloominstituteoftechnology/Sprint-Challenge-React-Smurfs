7302f70fb1d972e33b16d5f836dba0a02096ecbb
Andrés Rivera <andres.rivera.toro@gmail.com>
2018-10-29 17:27:07 -0400

    refactored SingleSmurf to simplify code

--- village/src/components/SingleSmurf.js
+++ village/src/components/SingleSmurf.js
@@ -1,63 +1,26 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
 
 import Smurf from './Smurf';
 import SmurfForm from './SmurfForm';
 
-export default class SingleSmurf extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      smurf: [],
-      name: '',
-      age: '',
-      height: '',
-    };
-  }
+const SingleSmurf = props => {
+  const smurf = props.smurfs.find(
+    smurf => `${smurf.id}` === props.match.params.id
+  );
 
-  componentDidMount() {
-    axios
-      .get(`http://localhost:3333/smurfs/`)
-      .then(response =>
-        this.setState({
-          smurf: response.data.find(
-            smurf => `${smurf.id}` === this.props.match.params.id
-          ),
-        })
-      )
-      .catch(err => console.log(err));
+  if (!smurf) return null;
+  return (
+    <div>
+      <SmurfForm id={smurf.id} editSmurf={props.editSmurf} />
+      <Smurf
+        id={smurf.id}
+        name={smurf.name}
+        age={smurf.age}
+        height={smurf.height}
+        goodbyeSmurf={props.goodbyeSmurf}
+      />
+    </div>
+  );
+};
 
-    // Using GET because <Smurf /> doesn't render when using direct link
-    // http://localhost:3000/smurf/:id
-
-    // const smurf = this.props.smurfs.find(
-    //   smurf => `${smurf.id}` === this.props.match.params.id
-    // );
-    // this.setState({ smurf });
-  }
-
-  render() {
-    return (
-      <div>
-        {/* Checking for state here will prevent
-        TypeError: Cannot read property 'id' of undefined
-        when using direct link http://localhost:3000/smurf/:id */}
-        {this.state.smurf ? (
-          <React.Fragment>
-            <SmurfForm
-              id={this.state.smurf.id}
-              editSmurf={this.props.editSmurf}
-            />
-            <Smurf
-              id={this.state.smurf.id}
-              name={this.state.smurf.name}
-              age={this.state.smurf.age}
-              height={this.state.smurf.height}
-              goodbyeSmurf={this.props.goodbyeSmurf}
-            />
-          </React.Fragment>
-        ) : null}
-      </div>
-    );
-  }
-}
+export default SingleSmurf;
