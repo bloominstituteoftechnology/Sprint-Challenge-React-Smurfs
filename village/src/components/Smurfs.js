50a87432417a2d69b09fc35dcf42027b6f03526b
Willie Yturralde <willieino@hotmail.com>
2018-10-29 19:17:37 -0700

    remove an apostrophe

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -7,7 +7,7 @@ class Smurfs extends Component {
     return (
       <div className="Smurfs">
         <h1>Smurf Village</h1>
-        <h4>Click on the Smurf's Text you would like to update</h4>
+        <h4>Click on the Smurfs Text you would like to update</h4>
         <ul>
           {this.props.smurfs.map(smurf => {
             return (
