933683b99589d0347e7132c0bc56f9ca05e68dcc
Jesse <theiseja@gmail.com>
2018-11-03 13:42:26 -0500

    Styled the NavBar links a bit using style={} MVP & Delete stretch complete

--- village/src/components/NavBar.js
+++ village/src/components/NavBar.js
@@ -4,8 +4,8 @@ import {NavLink} from 'react-router-dom';
 const NavBar = () => {
     return( 
         <div className='NavBar'>
-            <NavLink to ='/'>Smurf Village</NavLink>
-            <NavLink to ='/smurf-form'>Add a Smurf</NavLink>
+            <NavLink to ='/' style={{ textDecoration: 'none' }}>Smurf Village</NavLink>
+            <NavLink to ='/smurf-form' style={{ textDecoration: 'none' }}>Add a Smurf</NavLink>
         </div>
         )
     }
