4ebbe4b7827c908d02b857b6a1a18c2ab31d20ff
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-11-03 12:42:11 -0400

    Completed answers, installed axios for API calls.

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
@@ -14,6 +13,11 @@ class App extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+  componentDidMount(){
+    this.setState({
+
+    })
+  }
   render() {
     return (
       <div className="App">
