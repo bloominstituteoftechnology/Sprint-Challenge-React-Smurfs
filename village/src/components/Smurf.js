1811b8b847e2fa93d470abfba9587cdcae2bd534
cocoitali <cmburatto@gmail.com>
2018-11-03 20:27:05 -0700

    STRETCH (delete is functional, only on refresh )

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
 
-const Smurf = props => {
-  return (
+class Smurf extends React.Component {
+
+  deleteSmurf = id => {
+    axios
+      .delete(`http://localhost:3333/smurfs/${this.props.id}`)
+      .then(res => {
+      this.setState({ smurfs: res.data });
+    });
+  };
+
+  render() {
+    return (
     <div className="Smurf">
-      <h3>{props.name}</h3>
-      <strong>{props.height} tall</strong>
-      <p>{props.age} smurf years old</p>
+      <h3>{this.props.name}</h3>
+      <strong>{this.props.height} tall</strong>
+      <p>{this.props.age} smurf years old</p>
+      <button onClick={this.deleteSmurf}>delete</button>
+      <button>update</button>
     </div>
-  );
+    );
+}
 };
 
 Smurf.defaultProps = {
-  name: '',
-  height: '',
-  age: ''
+  name: "",
+  height: "",
+  age: ""
 };
 
 export default Smurf;
-
