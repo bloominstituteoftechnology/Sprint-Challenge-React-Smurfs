d37c1b277cd5d29f0114f48d6ccb6293e4498e3d
Thomas Claydon <tomclaydon102@gmail.com>
2018-11-30 16:07:26 -0500

    help

--- village/src/App.js
+++ village/src/App.js
@@ -4,10 +4,12 @@ import { Route, NavLink } from "react-router-dom";
 import "./App.css";
 import SmurfForm from "./components/SmurfForm";
 import Smurfs from "./components/Smurfs";
+import Smurf from "./components/Smurf";
+
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
       smurfs: []
     };
@@ -41,6 +43,18 @@ class App extends Component {
       });
   };
 
+  deleteItem = id => {
+    axios
+      .delete(`http://localhost:3333/smurfs/${id}`)
+      .then(res => {
+        console.log(res);
+        this.setState({
+          smurfs: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  };
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
@@ -53,7 +67,9 @@ class App extends Component {
             <NavLink exact to="/">
               <button>View List</button>
             </NavLink>
-            <NavLink to="/smurf-form"><button>Add Smurf</button></NavLink>
+            <NavLink to="/smurf-form">
+              <button>Add Smurf</button>
+            </NavLink>
           </div>
         </nav>
 
@@ -67,7 +83,24 @@ class App extends Component {
         <Route
           exact
           path="/"
-          render={props => <Smurfs {...props} smurfs={this.state.smurfs} />}
+          render={props => (
+            <Smurfs
+              {...props}
+              smurfs={this.state.smurfs}
+              
+            />
+          )}
+        />
+
+        <Route
+          path="/smurfs/:smurfId"
+          render={props => {
+            <Smurf
+              {...props}
+              smurfs={this.state.smurfs}
+              deleteItem={this.deleteItem}
+            />;
+          }}
         />
       </div>
     );
--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,20 +1,58 @@
-import React from 'react';
+import React from "react";
+import { Route, NavLink, Link } from "react-router-dom";
+import axios from "axios";
 
-const Smurf = props => {
-  return (
-    <div className="Smurf">
-      <h3>{props.name}</h3>
-      <strong>{props.height} tall</strong>
-      <p>{props.age} smurf years old</p>
-    </div>
-  );
-};
+class Smurf extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      smurf: null
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        `http://localhost:3333/smurfs/${
+          this.props.match.params.smurfId
+        }`
+      )
+      .then(response => {
+        console.log(response);
+        this.setState({
+          smurf: response.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    console.log(this.props.id);
+    // if (!this.state.smurfs) return <h2>Loading...</h2>;
+    return (
+      <div className="Smurf">
+        <h3>{this.props.name}</h3>
+        <strong>{this.props.height} tall</strong>
+        <p>{this.props.age} smurf years old</p>
+        <button
+          onClick={() => {
+            this.props.deleteItem(
+              this.props.id
+            )
+          }}
+          className="md-button"
+        >
+          Delete Item
+        </button>
+      </div>
+    );
+  }
+}
 
 Smurf.defaultProps = {
-  name: '',
-  height: '',
-  age: ''
+  name: "",
+  height: "",
+  age: ""
 };
 
 export default Smurf;
-
