e209104d6b395aa495980e351eb49e8734d89284
David Freitag <freitagdavid@outlook.com>
2018-11-30 12:09:50 -0500

    village/src/App: Added in routes and navbar

--- village/src/App.js
+++ village/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 import axios from 'axios';
+import { Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
 
 class App extends Component {
     constructor(props) {
@@ -36,8 +38,20 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <SmurfForm send={this.addNewSmurf} />
-                <Smurfs smurfs={this.state.smurfs} />
+                <NavBar />
+                <Route
+                    path="/smurf-form"
+                    render={props => (
+                        <SmurfForm {...props} smurfs={this.state.smurfs} />
+                    )}
+                />
+                <Route
+                    exact
+                    path="/"
+                    render={props => (
+                        <Smurfs {...props} smurfs={this.state.smurfs} />
+                    )}
+                />
             </div>
         );
     }
