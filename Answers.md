0c4699478b17cdf47ff20389e3c98a1d78ddffde
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2018-11-30 10:48:47 -0600

    Awnsers.md and can add smurfs to village

--- Answers.md
+++ Answers.md
@@ -1,5 +1,22 @@
 1.  Explain the differences between `client-side routing` and `server-side routing`.
+Client-side routing can rerender parts of a webpage through javascript without reloading the page. longer inital load time but shorter between pages.
+    better for a good user experience.
+Server-side routing sends new HTML to the client, making it reload the page when routing to a different url. shorter inital load time, but longer between pages/routes.
+    better for search engines
+
 1.  What does HTTP stand for?
+Hyper Text Transfer Protocol.
+
 1.  What does CRUD stand for?
+Create Read Update Delete.
+
 1.  Which HTTP methods can be mapped to the CRUD acronym that we use when interfacing with APIs/Servers.
+axios.post
+     .get
+     .put
+     .delete
+
 1.  Mention three tools we can use to make AJAX requests
+a server.js file/database 
+yarn start to host the data/methods
+Http methods
--- village/src/App.js
+++ village/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 
+import { Route } from "react-router-dom";
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
@@ -11,13 +13,38 @@ class App extends Component {
       smurfs: [],
     };
   }
+
+componentDidMount(){
+  axios
+    .get('http://localhost:3333/smurfs')
+    .then(response => {
+      console.log(response);
+      this.setState({ 
+        smurfs: response.data 
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+addToList = (obj) => {
+  axios.post('http://localhost:3333/smurfs', obj)
+  .then(response => {
+    this.setState({
+      smurfs: response.data
+    })
+  })
+  .catch(err => console.log(err))
+}
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm addToList={this.addToList} />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
