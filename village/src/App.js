bdc895d82fed176429c23bd5e405497d3e35355b
David Freitag <freitagdavid@outlook.com>
2018-11-30 11:46:27 -0500

    village/src/App: get and post working

--- village/src/App.js
+++ village/src/App.js
@@ -1,27 +1,46 @@
 import React, { Component } from 'react';
-
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import axios from 'axios';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      smurfs: [],
+    constructor(props) {
+        super(props);
+        this.state = {
+            smurfs: []
+        };
+    }
+
+    componentDidMount() {
+        axios
+            .get('http://localhost:3333/smurfs')
+            .then(response => {
+                console.log(response);
+                this.setState({ smurfs: response.data });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    addNewSmurf = (e, data) => {
+        e.preventDefault();
+        axios.post('http://localhost:3333/smurfs', data).then(response => {
+            console.log(response);
+            this.setState({
+                smurfs: response.data
+            });
+        });
     };
-  }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
-  render() {
-    return (
-      <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <SmurfForm send={this.addNewSmurf} />
+                <Smurfs smurfs={this.state.smurfs} />
+            </div>
+        );
+    }
 }
 
 export default App;
