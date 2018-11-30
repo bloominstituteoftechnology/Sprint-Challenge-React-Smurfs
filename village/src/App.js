45e61722e334352a370f19c5a79be67fc94dfb31
Trai Lynne <trailynne@gmail.com>
2018-11-30 11:00:38 -0700

    Declared routes Imported Route from React Router used render property instead of component in routes to pass props

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Route } from 'react-router-dom';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -42,8 +43,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm add={this.addNewSmurf}/>
-        <Smurfs smurfs={this.state.smurfs} />
+        <Route exact path='/smurf-form' render={props => <SmurfForm {...props} add={this.add}/>}/>
+        <Route exact path='/' render={props => <Smurfs {...props} smurfs={this.state.smurfs}/>}/>
       </div>
     );
   }
