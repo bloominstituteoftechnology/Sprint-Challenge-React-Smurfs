6fc2e078197b7049a2b8d360d4174c21f6827648
dan2014 <dmen326@gmail.com>
2018-11-30 13:54:57 -0500

    completed MVP and some stretch goals

--- village/src/App.js
+++ village/src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import {Route,NavLink} from "react-router-dom";
+
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import Smurf from './components/Smurf';
 
 class App extends Component {
   constructor(props) {
@@ -11,14 +15,45 @@ class App extends Component {
       smurfs: [],
     };
   }
+
+  componentDidMount(){
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(response => {
+        // console.log(response.data);
+        this.setState({
+          smurfs:response.data
+        });
+        // console.log(() => ({smurfs:response.data}));
+        // const test = () => ({smurfs:response.data})
+        // console.log(test())
+      })
+      .catch(error => console.log(error))
+  }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+          <nav className="nav">
+            <NavLink to={"/"}>Home</NavLink>
+            <NavLink to={"/smurf-form"} >Add Smurf</NavLink>
+          </nav>
+
+          <h1 className="heading">Smurf Village</h1>
+
+        <Route exact path={"/"} 
+          render={props => 
+            (<Smurfs {...props} smurfs={this.state.smurfs} />)}>
+        </Route>
+
+        <Route path={"/smurf/:id"} render={props =>(
+          <div className="Smurfs"> 
+            <Smurf {...props} smurfs={this.state.smurfs}/>
+          </div>
+          )}/>
+        <Route path={"/smurf-form"} render={props =>( <SmurfForm {...props}/> )}/>
       </div>
     );
   }
