2a5422acfd30602edbc60dc685c8bc4263d5f0b7
Thomas Claydon <tomclaydon102@gmail.com>
2018-11-30 11:39:16 -0500

    Completed MVP

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import axios from 'axios';
+import axios from "axios";
+import { Route, NavLink } from "react-router-dom";
 import "./App.css";
 import SmurfForm from "./components/SmurfForm";
 import Smurfs from "./components/Smurfs";
@@ -12,48 +13,32 @@ class App extends Component {
     };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     axios
-      .get('http://localhost:3333/smurfs')
-      .then(res=>{
+      .get("http://localhost:3333/smurfs")
+      .then(res => {
         console.log(res);
         this.setState({
           smurfs: res.data
         });
       })
-      .catch(err=>{
+      .catch(err => {
         console.log(err);
       });
   }
 
-  // componentDidUpdate(prevProps){
-  //   if(this.state !== prevProps){
-  //     axios
-  //     .get('http://localhost:3333/smurfs')
-  //     .then(res=>{
-  //       console.log(res);
-  //       this.setState({
-  //         smurfs: res.data
-  //       });
-  //     })
-  //     .catch(err=>{
-  //       console.log(err);
-  //     });
-  //   }
-  // }
-
   addSmurfHandler = smurf => {
     axios
-      .post('http://localhost:3333/smurfs', smurf)
-      .then(res=>{
+      .post("http://localhost:3333/smurfs", smurf)
+      .then(res => {
         console.log(res);
         this.setState({
           smurfs: res.data
         });
       })
-      .catch(err=>{
+      .catch(err => {
         console.log(err);
-      })
+      });
   };
 
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
@@ -62,8 +47,28 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm addSmurfHandler={this.addSmurfHandler}/>
-        <Smurfs smurfs={this.state.smurfs} />
+        <nav>
+          <h1 className="nav-header">Smurf List</h1>
+          <div className="nav-links">
+            <NavLink exact to="/">
+              <button>View List</button>
+            </NavLink>
+            <NavLink to="/smurf-form"><button>Add Smurf</button></NavLink>
+          </div>
+        </nav>
+
+        <Route
+          path="/smurf-form"
+          render={props => (
+            <SmurfForm {...props} addSmurfHandler={this.addSmurfHandler} />
+          )}
+        />
+
+        <Route
+          exact
+          path="/"
+          render={props => <Smurfs {...props} smurfs={this.state.smurfs} />}
+        />
       </div>
     );
   }
--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import "../App.css";
 import Smurf from './Smurf';
 
 class Smurfs extends Component {
