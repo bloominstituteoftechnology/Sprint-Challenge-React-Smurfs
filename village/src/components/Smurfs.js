b45e3e2603c1fdbe0318bfa5febc5acaf82224de
Yasirah Krueng <ykrueng@MacBook-Pro.local>
2018-11-30 10:51:39 -0600

    add a loading indicator

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -7,20 +7,24 @@ class Smurfs extends Component {
     return (
       <div className="Smurfs">
         <h1>Smurf Village</h1>
-        <ul>
-          {this.props.smurfs.map(smurf => {
-            return (
-              <Smurf
-                name={smurf.name}
-                id={smurf.id}
-                age={smurf.age}
-                height={smurf.height}
-                removeSmurf={this.props.removeSmurf}
-                key={smurf.id}
-              />
-            );
-          })}
-        </ul>
+        {
+          this.props.smurfs.length === 0 ?
+          <div>Loading ... </div>:
+          <ul>
+            {this.props.smurfs.map(smurf => {
+              return (
+                <Smurf
+                  name={smurf.name}
+                  id={smurf.id}
+                  age={smurf.age}
+                  height={smurf.height}
+                  removeSmurf={this.props.removeSmurf}
+                  key={smurf.id}
+                />
+              );
+            })}
+          </ul>
+        }
       </div>
     );
   }
