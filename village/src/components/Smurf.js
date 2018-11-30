1683da7f79c1f0262592c13138e2f9f4f4efb3f0
dan2014 <dmen326@gmail.com>
2018-11-30 13:55:55 -0500

    made an edit to Smurf.js

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Smurf = props => {
   if(props.name!==undefined || props.smurfs.length !== 0){
     const name = (props.name ? props.name : props.smurfs[props.match.params.id].name)
-  const height = (props.height ? props.height : props.smurfs[props.match.params.id].height)
-  const age = (props.age ? props.age : props.smurfs[props.match.params.id].age)
+    const height = (props.height ? props.height : props.smurfs[props.match.params.id].height)
+    const age = (props.age ? props.age : props.smurfs[props.match.params.id].age)
   return (
     <div className="Smurf">
       <h3>{name}</h3>
