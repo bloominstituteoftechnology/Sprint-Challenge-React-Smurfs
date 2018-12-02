eff2439b4e4d2ce06950ecd8212b065af705bd6b
jamespagedev <jamespagedev@outlook.com>
2018-12-02 08:58:04 -0700

    delete button styling completed

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -3,6 +3,16 @@ import axios from 'axios';
 import styled from 'styled-components';
 
 /***************************************************************************************************
+ ********************************************* Variables *******************************************
+ **************************************************************************************************/
+const submitTypes = {
+  create: 'add',
+  delete: 'remove',
+  noDeleteId: 0,
+  update: 'edit'
+};
+
+/***************************************************************************************************
  ********************************************** Styles *********************************************
  **************************************************************************************************/
 const FormAddSmurf = styled.form`
@@ -28,10 +38,49 @@ const FormRemoveSmurf = styled.div`
     font-family: 'Indie Flower', cursive;
   }
 
-  select {
-    outline: none;
-    width: 200px;
-    text-align-last: center; /* Only works in chrome */
+  div {
+    margin-bottom: 25px;
+
+    select {
+      /* styling */
+      background-color: white;
+      border: thin solid blue;
+      border-radius: 5px;
+      display: inline-block;
+      font: inherit;
+      line-height: 1.5em;
+      padding: 0.5em 3.5em 0.5em 1em;
+      outline: none;
+      width: 400px;
+      text-align-last: center; /* Only works in chrome */
+
+      /* reset */
+      margin: 0;
+      -webkit-box-sizing: border-box;
+      -moz-box-sizing: border-box;
+      box-sizing: border-box;
+      -webkit-appearance: none;
+      -moz-appearance: none;
+      background-image: linear-gradient(45deg, transparent 50%, blue 50%),
+        linear-gradient(135deg, blue 50%, transparent 50%),
+        linear-gradient(to right, skyblue, skyblue);
+      background-position: calc(100% - 20px) calc(1em + 2px),
+        calc(100% - 15px) calc(1em + 2px), 100% 0;
+      background-size: 5px 5px, 5px 5px, 2.5em 2.5em;
+      background-repeat: no-repeat;
+
+      &:focus {
+        background-image: linear-gradient(45deg, white 50%, transparent 50%),
+          linear-gradient(135deg, transparent 50%, white 50%),
+          linear-gradient(to right, gray, gray);
+        background-position: calc(100% - 15px) 1em, calc(100% - 20px) 1em,
+          100% 0;
+        background-size: 5px 5px, 5px 5px, 2.5em 2.5em;
+        background-repeat: no-repeat;
+        border-color: grey;
+        outline: 0;
+      }
+    }
   }
 `;
 
@@ -41,24 +90,66 @@ const FormSubmitButton = styled.button`
   height: 30px;
   outline: none;
   cursor: pointer;
-  background-color: blue;
   color: white;
-  border-color: blue;
   text-shadow: 1px 1px rgba(0, 0, 0, 0.5);
   box-shadow: -1px 3px 10px 1px rgba(0, 0, 0, 0.3);
 
+  /* Add Smurf Button */
+  background-color: ${props =>
+    props.submitType === submitTypes.create && `blue`};
+  border-color: ${props => props.submitType === submitTypes.create && `blue`};
+
+  /* Remove Smurf Button */
+  background-color: ${props =>
+    props.submitType === submitTypes.delete && `red`};
+  border-color: ${props => props.submitType === submitTypes.delete && `red`};
+
   &:hover {
-    border-top-color: #add8e6;
-    border-left-color: #add8e6;
-    background: linear-gradient(#add8e6, #0000ff);
+    /* Add Smurf Button */
+    border-top-color: ${props =>
+      props.submitType === submitTypes.create && `#add8e6`};
+    border-left-color: ${props =>
+      props.submitType === submitTypes.create && `#add8e6`};
+    background: ${props =>
+      props.submitType === submitTypes.create &&
+      `linear-gradient(#add8e6, #0000ff)`};
+
+    /* Remove Smurf Button */
+    border-top-color: ${props =>
+      props.submitType === submitTypes.delete && `#ff0000`};
+    border-left-color: ${props =>
+      props.submitType === submitTypes.delete && `#ff0000`};
+    background: ${props =>
+      props.submitType === submitTypes.delete &&
+      `linear-gradient(#ff0000, #a00000)`};
   }
 
   &:active {
-    border-top-color: #0000ff;
-    border-left-color: #0000ff;
-    border-right-color: #add8e6;
-    border-bottom-color: #add8e6;
-    background: linear-gradient(#0000ff, #add8e6);
+    /* Add Smurf Button */
+    border-top-color: ${props =>
+      props.submitType === submitTypes.create && `#0000ff`};
+    border-left-color: ${props =>
+      props.submitType === submitTypes.create && `#0000ff`};
+    border-right-color: ${props =>
+      props.submitType === submitTypes.create && `#add8e6`};
+    border-bottom-color: ${props =>
+      props.submitType === submitTypes.create && `#add8e6`};
+    background: ${props =>
+      props.submitType === submitTypes.create &&
+      `linear-gradient(#0000ff, #add8e6)`};
+
+    /* Remove Smurf Button */
+    border-top-color: ${props =>
+      props.submitType === submitTypes.delete && `red`};
+    border-left-color: ${props =>
+      props.submitType === submitTypes.delete && `red`};
+    border-right-color: ${props =>
+      props.submitType === submitTypes.delete && `#ff0000`};
+    border-bottom-color: ${props =>
+      props.submitType === submitTypes.delete && `#ff0000`};
+    background: ${props =>
+      props.submitType === submitTypes.delete &&
+      `linear-gradient(#a00000, #ff0000)`};
   }
 `;
 
@@ -119,7 +210,7 @@ class SmurfForm extends Component {
 
   renderSortedSmurfList = () => {
     return this.state.sortedSmurfsByName.map(smurf => (
-      <option value={smurf.name} key={smurf.id}>
+      <option value={smurf.id} key={smurf.id}>
         {smurf.name}
       </option>
     ));
@@ -155,16 +246,31 @@ class SmurfForm extends Component {
     });
   };
 
-  submitHandler = e => {
-    this.addSmurf(e);
-    this.clearNewSmurfState();
+  submitHandler = (e, submitType, id) => {
+    e.preventDefault();
+    if (submitType === submitTypes.create) {
+      if (
+        this.state.newSmurf.name &&
+        this.state.newSmurf.age &&
+        this.state.newSmurf.height
+      ) {
+        this.addSmurf(e);
+        this.clearNewSmurfState();
+      }
+    } else if (submitType === submitTypes.delete) {
+      if (id !== submitTypes.noDeleteId) this.removeSmurf(e, id);
+    }
   };
 
   //========================== Render ==========================
   render() {
     return (
       <div className='SmurfForm'>
-        <FormAddSmurf onSubmit={e => this.submitHandler(e)}>
+        <FormAddSmurf
+          onSubmit={e =>
+            this.submitHandler(e, submitTypes.create, submitTypes.noDeleteId)
+          }
+        >
           <h1>Add Smurf to Village</h1>
           <div>
             <input
@@ -187,19 +293,33 @@ class SmurfForm extends Component {
             />
           </div>
           <div>
-            <FormSubmitButton type='submit'>
+            <FormSubmitButton type='submit' submitType={submitTypes.create}>
               Add to the village
             </FormSubmitButton>
           </div>
         </FormAddSmurf>
-        <FormRemoveSmurf>
+        <FormRemoveSmurf
+          onSubmit={e =>
+            this.submitHandler(e, submitTypes.delete, submitTypes.noDeleteId)
+          }
+        >
           <h1>Remove Smurf from Village</h1>
-          <select>
-            <option value='Select Smurf' key={0}>
-              Select Smurf
-            </option>
-            {this.renderSortedSmurfList()}
-          </select>
+          <div>
+            <FormSubmitButton type='submit' submitType={submitTypes.delete}>
+              Remove from the village
+            </FormSubmitButton>
+          </div>
+          <div>
+            <select>
+              <option
+                value={submitTypes.noDeleteId}
+                key={submitTypes.noDeleteId}
+              >
+                Select Smurf
+              </option>
+              {this.renderSortedSmurfList()}
+            </select>
+          </div>
         </FormRemoveSmurf>
       </div>
     );
