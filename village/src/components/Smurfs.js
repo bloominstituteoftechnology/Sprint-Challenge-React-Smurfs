fb4ae960975e49572fea60f94632a4f4dc0773bb
Jamie Hall <hall.jamie@gmail.com>
2019-01-25 13:38:23 -0500

    Tweaked some styling

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -4,42 +4,52 @@ import { Smurf } from './Smurf';
 import styled from "styled-components";
 
 const Container = styled.div`
+  width: 100%; 
+  color: #00274c;
+  background-color: #ffcb05;
+  `
+
+const H1 = styled.h1`
+margin-bottom :10px;
+text-align: center;
+`
+
+const Content = styled.div`
   width: 100%;
   color: #ffcb05;
   background-color: #00274c;
- `;
-
-const H1 = styled.h1`
-margin: 10px;
-
-`;
-
+  padding-top: 15px;
+  padding-bottom: 15px;
+ `
+ 
 class Smurfs extends Component {
   render() {
     return (
       <Container>
         <H1>Smurf Village</H1>
-        <ul>
-          {this.props.smurfs.map(smurf => {
-            return (
-              <Smurf
-                name={smurf.name}
-                id={smurf.id}
-                age={smurf.age}
-                height={smurf.height}
-                key={smurf.id}
-                deleteSmurf={this.props.deleteSmurf}
-              />
-            );
-          })}
-        </ul>
+        <Content>
+          <ul>
+            {this.props.smurfs.map(smurf => {
+              return (
+                <Smurf
+                  name={smurf.name}
+                  id={smurf.id}
+                  age={smurf.age}
+                  height={smurf.height}
+                  key={smurf.id}
+                  deleteSmurf={this.props.deleteSmurf}
+                />
+              );
+            })}
+          </ul>
+        </Content>
       </Container>
     );
   }
 }
 
 Smurf.defaultProps = {
- smurfs: [],
+  smurfs: [],
 };
 
 export default Smurfs;
