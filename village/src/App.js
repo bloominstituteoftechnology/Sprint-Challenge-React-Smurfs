2bf410e56964f8abcc886eb24d3b136c9cdcd8bb
Samsoucy <newf930@outlook.com>
2019-01-25 11:58:55 -0500

    added AXIOS to retrieve data and displayed smurfs on the screen

--- village/src/App.js
+++ village/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+
+const baseUrl = "http://localhost:3333";
 
 class App extends Component {
   constructor(props) {
@@ -10,6 +13,20 @@ class App extends Component {
     this.state = {
       smurfs: [],
     };
+  }
+
+  componentDidMount() {
+    axios
+      .get(`${baseUrl}/smurfs`)
+      .then(res => {
+        this.setState({
+          smurfs: res.data
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
