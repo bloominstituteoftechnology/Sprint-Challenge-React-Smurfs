79110a8b74844c8d4a3602de2cf84719e442e1e0
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-01-25 11:49:04 -0500

    MVP Finished

--- village/src/App.js
+++ village/src/App.js
@@ -1,24 +1,65 @@
-import React, { Component } from 'react';
-
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
+import { Route, NavLink } from "react-router-dom";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      smurfs: [],
-    };
-  }
+  state = {
+    smurfs: [],
+    name: "",
+    age: "",
+    height: ""
+  };
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+  componentDidMount = () => {
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(res => this.setState({ smurfs: res.data }))
+      .catch(error => console.log(error));
+  };
+
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  addSmurf = event => {
+    event.preventDefault();
+    // add code to create the smurf using the api
+    axios
+      .post(`http://localhost:3333/smurfs`, this.state)
+      .then(res => {
+        this.setState({ smurfs: res.data, name: "", age: "", height: "" });
+        this.props.history.push("/");
+      })
+      .catch(error => console.log(error));
+  };
+
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <NavLink to="/">HOME</NavLink>
+        <NavLink to="/smurf-form">ADD NEW</NavLink>
+        <Route
+          exact
+          path="/"
+          render={props => <Smurfs {...props} smurfs={this.state.smurfs} />}
+        />
+        <Route
+          path="/smurf-form"
+          render={props => (
+            <SmurfForm
+              {...props}
+              handleInputChange={this.handleInputChange}
+              addSmurf={this.addSmurf}
+              state={this.state}
+            />
+          )}
+        />
       </div>
     );
   }
--- village/src/index.js
+++ village/src/index.js
@@ -1,7 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, withRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
 
-import './index.css';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+const AppWithRouter = withRouter(App);
+ReactDOM.render(
+  <Router>
+    <AppWithRouter />
+  </Router>,
+  document.getElementById("root")
+);
