a1daf55bfbaad670457254369a0e252841474e4e
Kseniya Platonava <ksu2008@gmail.com>
2019-01-26 13:33:35 -0500

    Add Routes to / and to Smurf-Form

--- village/src/App.js
+++ village/src/App.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import { Route, NavLink } from 'react-router-dom';
+
 
 class App extends Component {
   constructor(props) {
@@ -39,8 +41,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Route 
+           path ="/smurf-form"
+           render={props=>
+            <SmurfForm {...props} smurfs={this.state.smurfs} updateSmurfListOnServer={this.updateSmurfListOnServer}/> } />
+        <Route 
+          exact path ="/"
+          render ={props =>
+          <Smurfs  {...props} smurfs={this.state.smurfs} /> } />
+    
+          
       </div>
     );
   }
