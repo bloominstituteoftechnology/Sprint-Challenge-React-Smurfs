239855aa4d3b843f49e26ddd687b3351f95881c0
Noah <noah.stoeckel@gmail.com>
2019-01-26 12:46:42 -0500

    Implements Get And Add Smurf Methods

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -14,6 +15,29 @@ class App extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+  componentDidMount(){ 
+    this.getAllSmurfs();
+  }
+
+  getAllSmurfs = () =>{
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(response=>{
+        console.log(response.data)
+        this.setState({smurfs:response.data})
+      })
+      .catch(error=>console.log(error))
+  }
+
+  addNewSmurf = smurf =>{
+    axios
+      .post('http://localhost:3333/smurfs', smurf)
+      .then(response=>{
+        this.setState({smurfs:response.data})
+      })
+      .catch(error=>console.log(error))
+  }
+
   render() {
     return (
       <div className="App">
