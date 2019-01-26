55741f7960dc6dae0f820ddbfd1fdf7c33732e65
david-chua <dp_chua@hotmail.com>
2019-01-26 12:53:33 -0500

    finished MVP

--- village/src/App.js
+++ village/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Route, withRouter } from 'react-router-dom';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import Navigation from'./components/Navigation';
 
 class App extends Component {
   constructor(props) {
@@ -29,6 +31,7 @@ class App extends Component {
       .post(`http://localhost:3333/smurfs`, smurf )
       .then( response => {
         console.log(response)
+        this.props.history.push('/');
         this.setState({ smurfs: this.state.smurfs })
         window.location.reload();
       })
@@ -42,11 +45,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm addSmurf = { this.addSmurf }/>
-        <Smurfs smurfs={ this.state.smurfs} />
+        <Navigation />
+        <Route
+          exact path="/"
+          render={(props) => <Smurfs
+            {... props }
+            smurfs ={ this.state.smurfs }/>}
+        />
+        <Route
+          exact path="/smurfs-form"
+          render={(props) => <SmurfForm
+            {... props }
+            addSmurf = {this.addSmurf }/>}
+        />
+
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
