30964c053652121d72516d175a43114d9d958a07
CatsAreMLG <joshuadgon@gmail.com>
2019-01-26 12:04:47 -0600

    Added Form Styling

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,20 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const Smurf = props => {
   return (
-    <div className="Smurf">
-      <h3>{props.name}</h3>
-      <strong>{props.height} tall</strong>
-      <p>{props.age} smurf years old</p>
+    <div className="Smurfs__Smurf">
+      <h3 className="Smurfs__name">{props.name}</h3>
+      <strong className="Smurfs__height">{props.height} tall</strong>
+      <p className="Smurfs__age">{props.age} smurf years old</p>
     </div>
   );
 };
 
 Smurf.defaultProps = {
-  name: '',
-  height: '',
-  age: ''
+  name: "",
+  height: "",
+  age: ""
 };
 
 export default Smurf;
-
