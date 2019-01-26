bd65b8ef1664a7cf3158b65c468a209566b87c90
Jonathan Dorety <kyullog@gmail.com>
2019-01-26 14:20:50 -0500

    Basic Smurf List Styling

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Smurf from './Smurf';
+import Smurf from "./Smurf";
+import "./Smurfs.css";
 
 class Smurfs extends Component {
   render() {
@@ -26,7 +27,7 @@ class Smurfs extends Component {
 }
 
 Smurf.defaultProps = {
- smurfs: [],
+  smurfs: []
 };
 
 export default Smurfs;
