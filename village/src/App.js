fe7a601720b605c18fee49282a9204367a75d1cc
dickwinters <coding1965@gmail.com>
2019-01-26 11:30:16 -0600

    Routes for different paths

--- village/src/App.js
+++ village/src/App.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import {Route} from 'react-router-dom';
 
 class App extends Component {
   constructor(props) {
@@ -17,8 +18,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Route path="/smurf-form" render={props => <SmurfForm {...props} />} />
+        <Route
+          path="/"
+          render={props => <Smurfs {...props} smurfs={this.state.smurfs} />}
+        />
       </div>
     );
   }
