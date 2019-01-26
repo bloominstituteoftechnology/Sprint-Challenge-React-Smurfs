f5cada5a4bebe59c0d34f15c9f71117baf51f581
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2019-01-26 11:43:57 -0600

    Setting up Axios request and setting state.

--- village/src/App.js
+++ village/src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
-
+import { Route } from 'react-router-dom';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
-
+import axios from 'axios'
+import { NavLink } from 'react-router-dom'
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       smurfs: [],
-    };
+    }
   }
+
+  componentDidMount(){
+    axios.get('http://localhost:3333/smurfs')
+    .then((response) =>{
+      this.setState(() =>({smurfs: response.data}))
+    })
+    .catch(err=>console.log(err))
+  }
+  
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
