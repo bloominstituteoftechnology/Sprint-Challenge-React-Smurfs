8945050aca5edfd95859a3c1ec2840fb6fd58a3f
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2019-01-26 12:25:01 -0600

    Styled NavBar

--- village/src/components/NavBar.js
+++ village/src/components/NavBar.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
+import styled from 'styled-components';
+
+const Header = styled.header`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 300px;
+  margin: 0 auto;
+`;
 
 const NavBar = () => {
   return (
-    <div>
+    <Header>
       <NavLink to='/'>Smurf Village</NavLink>
       <NavLink to='/smurf-form'>Smurf Form</NavLink>
-    </div>
+    </Header>
   );
 };
 
