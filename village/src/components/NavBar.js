3e86416a3005d8b0a97a51e76e515c4dd8ae9819
Jonathan Dorety <kyullog@gmail.com>
2019-01-26 13:57:21 -0500

    Basic NavBar styling

--- village/src/components/NavBar.js
+++ village/src/components/NavBar.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./NavBar.css";
 
 const NavBar = () => {
   return (
     <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/addsmurf">Add Smurf</NavLink>
+      <NavLink className="navigation" exact activeClassName="selected" to="/">
+        Home
+      </NavLink>
+      <NavLink className="navigation" activeClassName="selected" to="/addsmurf">
+        Add Smurf
+      </NavLink>
     </nav>
   );
 };
