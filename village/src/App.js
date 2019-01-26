1f2de0a335e365eb3fdad2157b18dd37eee28573
dickwinters <coding1965@gmail.com>
2019-01-26 11:38:58 -0600

    CompDidMount to retieve data and set to state for render

--- village/src/App.js
+++ village/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 import {Route} from 'react-router-dom';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
@@ -12,13 +13,25 @@ class App extends Component {
       smurfs: [],
     };
   }
+
+  componentDidMount() {
+    axios.get('http://www.localhost:3333/smurfs').then(data => {
+      console.log(data.data);
+      this.setState({smurfs: data.data});
+    });
+  }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
       <div className="App">
-        <Route path="/smurf-form" render={props => <SmurfForm {...props} />} />
+        <Route
+          path="/smurf-form"
+          render={props => (
+            <SmurfForm {...props} newId={this.state.friends.length + 1} />
+          )}
+        />
         <Route
           path="/"
           render={props => <Smurfs {...props} smurfs={this.state.smurfs} />}
