e3baaf32e97f2dc0ff37fc239c0edb68ae02149b
iradsanchez <ira.d.sanchez@gmail.com>
2019-01-26 13:01:24 -0700

    debugging Smurfs list display

--- village/src/App.js
+++ village/src/App.js
@@ -15,32 +15,24 @@ class App extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
-  getSmurfs = () => {
+  componentDidMount() {
     axios
       .get("http://localhost:3333/smurfs")
       .then(res => this.setState({ smurfs: res.data }))
       .catch(err => console.log(err));
-  };
+  }
 
   render() {
     return (
       <div className="App">
         <nav>
-          <NavLink exact to="/">
-            Smurfs
-          </NavLink>
+          <NavLink to="/">Smurfs</NavLink>
           <NavLink to="/smurf-form">Add</NavLink>
         </nav>
 
         <Route
           path="/smurf-form"
-          render={props => (
-            <SmurfForm
-              {...props}
-              getSmurfs={this.getSmurfs}
-              smurfs={this.state.smurfs}
-            />
-          )}
+          render={props => <SmurfForm {...props} getSmurfs={this.getSmurfs} />}
         />
         <Route
           path="/"
