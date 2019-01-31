6cfb441f260f1060f6019238c732d1c8c02f93f0
Megan Fontenot <lambda_school_loaner_28@Lambda-School-Loaner-28s-MacBook-Pro.local>
2019-01-30 20:58:22 -0600

    answered missing question, removed commented code.

--- Answers.md
+++ Answers.md
@@ -9,5 +9,9 @@ HTTP stands for HyperText Transfer Protocol.
 3.  What does CRUD stand for?
 CRUD stands for Create, Read, Update and Delete.
 4.  Which HTTP methods can be mapped to the CRUD acronym that we use when interfacing with APIs/Servers.
+Create - POST
+Read - GET
+Update - PUT
+Delete - DELETE
 5.  Mention three tools we can use to make AJAX requests
 Fetch, Axios, and raw HTML
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,30 +1,6 @@
 import React, { Component } from 'react';
 
 class SmurfForm extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     name: '',
-  //     age: '',
-  //     height: ''
-  //   };
-  // }
-
-  // addSmurf = event => {
-  //   event.preventDefault();
-  //   // add code to create the smurf using the api
-
-  //   this.setState({
-  //     name: '',
-  //     age: '',
-  //     height: ''
-  //   });
-  // }
-
-  // handleInputChange = e => {
-  //   this.setState({ [e.target.name]: e.target.value });
-  // };
-
   render() {
     return (
       <div className="SmurfForm">
