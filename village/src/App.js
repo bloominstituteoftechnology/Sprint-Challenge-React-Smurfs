47e3f8aa1df341617d24d0fa2c2d617fdf5d8158
J A <jarata@gmail.com>
2019-02-01 17:07:20 -0800

    trying to clean up and refactor code

--- village/src/App.js
+++ village/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios'
-import { Route } from 'react-router-dom'
+import { Route, NavLink } from 'react-router-dom'
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -24,16 +24,44 @@ class App extends Component {
             console.log(error)
         })
   };
+    addSmurf = event => {
+        event.preventDefault();
+        // add code to create the smurf using the api
+        const newSmurf = this.state;
+        axios
+            .post('http://localhost:3333/smurfs', newSmurf)
+            .then(response => {
+                console.log(response.data);
+                this.setState({
+                    smurfs: response.data
+                })
+            })
+            .catch(error => {
+                console.log(error.response)
+            })
+    }
+
+    handleInputChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
       <div className="App">
-          <Route exact path="/" component={Smurfs} />
-          <Route path="/smurfs" smurfs={this.state.smurfs} />
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+          <div>
+              <NavLink exact to='/smurfs'>Home</NavLink>
+              <NavLink to='/smurf-form'>Add Smurf</NavLink>
+          </div>
+
+          <Route exact path='/smurfs' render={props => (
+              <Smurfs {...props} smurfs={this.state.smurfs} />
+          )} />
+
+          <Route exact path='/smurf-form' render={props => (
+              <SmurfForm {...props} smurfs={this.state.smurfs} addSmurf={this.addSmurf} handleChanges={this.handleInputChange} />
+          )} />
       </div>
     );
   }
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,64 +1,31 @@
-import React, { Component } from 'react';
-import axios from 'axios'
+import React from 'react';
 
-class SmurfForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      age: '',
-      height: ''
-    };
-  }
-
-  addSmurf = () => {
-    // event.preventDefault();
-    // add code to create the smurf using the api
-    const newSmurf = this.state;
-      axios
-        .post('http://localhost:3333/smurfs', newSmurf)
-          .then(response => {
-              console.log(response.data);
-              this.setState({
-                  smurfs: response.data
-              })
-          })
-          .catch(error => {
-              console.log(error.response)
-          })
-  }
-
-  handleInputChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
-  render() {
+function SmurfForm(props) {
     return (
       <div className="SmurfForm">
-        <form onSubmit={this.addSmurf}>
+        <form onSubmit={props.addSmurf}>
           <input
-            onChange={this.handleInputChange}
+            onChange={props.handleInputChange}
             placeholder="name"
-            value={this.state.name}
+            value={props.name}
             name="name"
           />
           <input
-            onChange={this.handleInputChange}
+            onChange={props.handleInputChange}
             placeholder="age"
-            value={this.state.age}
+            value={props.age}
             name="age"
           />
           <input
-            onChange={this.handleInputChange}
+            onChange={props.handleInputChange}
             placeholder="height"
-            value={this.state.height}
+            value={props.height}
             name="height"
           />
           <button type="submit">Add to the village</button>
         </form>
       </div>
     );
-  }
 }
 
 export default SmurfForm;
