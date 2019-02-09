6e006c44cbfc4123c09bb829c35bbbb7d48d101b
mustafa inaya <moose.inaya@icloud.com>
2019-02-09 15:52:45 -0200

    IMPORT, Imported Third Party Librarries,ROutr Dom, And My Files Into My App.js.

--- village/src/App.js
+++ village/src/App.js
@@ -1,27 +1,36 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
+// My files
+import SmurfForm from './components/SmurfForm';
+import Smurfs from './components/Smurfs';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      smurfs: [],
-    };
-  }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
-  render() {
-    return (
-      <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			smurfs: []
+		};
+	}
+	// add any needed code to ensure that the smurfs collection exists on
+	// state and it has data coming from the server
+	// Notice what your map function is looping over and returning inside of Smurfs.
+	// You'll need to make sure you have the right properties on state and pass them down to props.
+	render() {
+		return (
+			<div className="App">
+				<SmurfForm />
+				<Smurfs smurfs={this.state.smurfs} />
+			</div>
+		);
+	}
 }
 
 export default App;
