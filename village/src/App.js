cd64b3495ae235530330600eb07fecad5fd1f52b
AWSafran <andrewsafran@gmail.com>
2019-04-05 11:36:15 -0400

    successfully pulling data from server and setting to state, displaying smurf list

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -11,6 +12,17 @@ class App extends Component {
       smurfs: [],
     };
   }
+
+  componentDidMount = () =>{
+    console.log("attempting ajax get request");
+
+    axios.get('http://localhost:3333/smurfs')
+      .then(res => this.setState({smurfs: res.data}))
+      .catch(err => console.log(err));
+  }
+
+
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
