89a6847b7f1153ae0713c54b34a0ff4a8fe0cf9f
omar okla <omaro@me.com>
2019-04-05 11:48:24 -0400

    Implemented GET API request inside ComponentDidMount

--- village/src/App.js
+++ village/src/App.js
@@ -1,27 +1,39 @@
-import React, { Component } from 'react';
-
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      smurfs: [],
-    };
-  }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
-  render() {
-    return (
-      <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			smurfs: []
+		};
+	}
+	// add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
+	// Notice what your map function is looping over and returning inside of Smurfs.
+	// You'll need to make sure you have the right properties on state and pass them down to props.
+
+	componentDidMount() {
+		axios
+			.get("http://localhost:3333/smurfs")
+			.then(res => {
+				this.setState({ smurfs: res.data });
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	}
+
+	render() {
+		return (
+			<div className='App'>
+				<SmurfForm />
+				<Smurfs smurfs={this.state.smurfs} />
+			</div>
+		);
+	}
 }
 
 export default App;
