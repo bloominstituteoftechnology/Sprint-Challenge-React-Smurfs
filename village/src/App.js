6869fd08a1baa69d75ea16a4c6c51759ce479dc9
Lambda_School_Loaner_93 <lambda_school_loaner_93@Lambda-School-Loaner-93s-MacBook-Pro.local>
2019-04-05 11:28:57 -0400

    added axios and get function

--- village/src/App.js
+++ village/src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import axios from 'axios'
 
 class App extends Component {
   constructor(props) {
@@ -11,6 +12,21 @@ class App extends Component {
       smurfs: [],
     };
   }
+
+  componentDidMount(){
+    // pulls in my data to set my intial state
+     axios.get('http://localhost:3333/smurfs')
+     .then((res) =>{
+         // .then uses the resquest for data and sets my state. 
+         this.setState({
+             smurfs: res.data
+         })
+     })
+     .catch((err =>{
+        console.log('Oops I did it again', err)
+     }))
+    }
+     
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
