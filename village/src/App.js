9a0663432ff4c1d0b166d5a7c27df950a8553e94
Sean Doyle <sdoylelambda@gmail.com>
2019-04-19 11:32:29 -0400

    Axios request to retrieve smurfs data and add smurf.

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -12,6 +13,32 @@ class App extends Component {
     };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
+
+  componentDidMount() {
+    axios
+      .get('https://localhost:3333/smurfs')
+      .then(res => {
+        this.setState({ smurfs: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({ error: err });
+      })
+  }
+
+  addSmurf = (e, smurf) => {
+    e.preventDefault();
+    axios
+      .post("https://localhost:3333/smurfs", smurf)
+      .then(res => {
+        this.setState({ smurfs: res.data })
+      .catch(err => {
+        console.log(err);
+        this.setState({ error: err });
+        })
+      })
+  }
+
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
