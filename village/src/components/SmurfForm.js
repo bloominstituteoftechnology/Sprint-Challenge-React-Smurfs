3b8cb0866462b4c6e47f66ceb059ac9e86a3b62f
Bryce-Soghigian <49734722+Bryce-Soghigian@users.noreply.github.com>
2019-06-14 11:48:21 -0500

    Update SmurfForm.js
    Acually works now added some styles

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,27 +1,74 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
+import styled from "styled-components";
+const Container = styled.div`
+  display: flex;
+`;
+const CardHeader = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.5rem;
+  height: 75px;
+  width: 100%;
+  background-color: black;
+  color: white;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+`;
+const FriendCard = styled.div`
+  display: flex;
+  flex-flow: column;
+  margin: 25px;
+  width: 500px;
+  height: 200px;
+  border-radius: 10px;
+  box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
+  @media (max-width: 1300px) {
+    margin: 10px 0px;
+  }
+`;
 
+const HeaderContent = styled.h1`
+  font-size: 1.5rem;
+  margin: 10px;
+`;
+const ParagraphContent = styled.p`
+  font-size: 1rem;
+  margin: 10px;
+`;
+const Remove = styled.p`
+  font-size: 1.25rem;
+  margin: 10px;
+  align-self: flex-end;
+  cursor: pointer;
+  user-select: none;
+`;
 class SmurfForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      age: '',
-      height: ''
+      name: "",
+      age: "",
+      height: ""
     };
   }
 
-  handleAdd = e => {
-    e.preventDefault();
+  addSmurf = event => {
+    event.preventDefault();
     // add code to create the smurf using the api
-    //console.log(this.props.addSmurf)
-    this.props.addSmurf(this.state);
-
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
+    axios.post("smurfs", {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height
     });
-  }
+    this.setState({
+      name: "",
+      age: "",
+      height: ""
+    });
+  };
 
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
@@ -29,8 +76,8 @@ class SmurfForm extends Component {
 
   render() {
     return (
-      <div className="SmurfForm">
-        <form onSubmit={this.handleAdd}>
+      <FriendCard>
+        <form onSubmit={this.addSmurf}>
           <input
             onChange={this.handleInputChange}
             placeholder="name"
@@ -49,9 +96,11 @@ class SmurfForm extends Component {
             value={this.state.height}
             name="height"
           />
-          <button type="submit">Add to the village</button>
+          <button onClick={this.addSmurf} type="submit">
+            Add to the village
+          </button>
         </form>
-      </div>
+      </FriendCard>
     );
   }
 }
