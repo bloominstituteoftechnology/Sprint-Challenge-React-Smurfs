22ce8c0dc57148ada2042379423add95d0f22d71
Bryce-Soghigian <49734722+Bryce-Soghigian@users.noreply.github.com>
2019-06-14 11:50:17 -0500

    Update Smurfs.js

--- village/src/components/Smurfs.js
+++ village/src/components/Smurfs.js
@@ -1,32 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Smurf from './Smurf';
-
+import Smurf from "./Smurf";
+import styled from "styled-components";
+const Container = styled.div`
+  display: flex;
+  flex-flow: column;
+  margin: auto;
+  justify-content: center;
+`;
+const SmurfContainer = styled.div`
+  margin: auto;
+  display: flex;
+  flex-flow: wrap;
+  width: 1250px;
+  justify-content: center;
+`;
 class Smurfs extends Component {
+  createSmurfs = () => {
+    return this.props.smurfs.map(smurf => {
+      return (
+        <Smurf
+          name={smurf.name}
+          id={smurf.id}
+          age={smurf.age}
+          height={smurf.height}
+          key={smurf.id}
+        />
+      );
+    });
+  };
   render() {
     return (
-      <div className="Smurfs">
+      <Container>
         <h1>Smurf Village</h1>
-        <ul>
-          {this.props.smurfs.map(smurf => {
-            return (
-              <Smurf
-                name={smurf.name}
-                id={smurf.id}
-                age={smurf.age}
-                height={smurf.height}
-                key={smurf.id}
-              />
-            );
-          })}
-        </ul>
-      </div>
+        <SmurfContainer>{this.createSmurfs()}</SmurfContainer>
+      </Container>
     );
   }
 }
 
 Smurf.defaultProps = {
- smurfs: [],
+  smurfs: []
 };
 
 export default Smurfs;
