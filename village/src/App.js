9896394bcf55d181e1f56d869b3331db469aa469
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-06-14 11:42:17 +0400

    complete addSmurf method

--- village/src/App.js
+++ village/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom';
+import uuid from 'uuid';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+
+const SMURFS_API = 'http://localhost:3333/smurfs';
 
 class App extends Component {
 	constructor(props) {
@@ -12,7 +15,12 @@ class App extends Component {
 		this.state = {
 			smurfs: [],
 			isLoading: false,
-			errorMessage: ''
+			errorMessage: '',
+			form: {
+				nameValue: '',
+				ageValue: '',
+				heightValue: ''
+			}
 		};
 	}
 	// add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
@@ -22,7 +30,7 @@ class App extends Component {
 	componentDidMount() {
 		this.setState({ isLoading: true });
 		axios
-			.get('http://localhost:3333/smurfs')
+			.get(SMURFS_API)
 			.then(res => {
 				this.setState({ smurfs: res.data });
 			})
@@ -31,11 +39,29 @@ class App extends Component {
 			})
 			.finally(this.setState({ isLoading: false }));
 	}
+	addSmurf = () => {
+		this.setState({ isLoading: true });
+		const newSmurf = {
+			id: uuid(),
+			name: this.state.form.nameValue,
+			age: this.state.form.ageValue,
+			height: this.state.form.heightValue
+		};
+		axios
+			.post(SMURFS_API, { ...newSmurf })
+			.then(res => {
+				this.setState({ smurfs: res.data });
+			})
+			.catch(err => {
+				this.setState({ errorMessage: err.response.statusText });
+			})
+			.finally(this.setState({ isLoading: false }));
+	};
 
 	render() {
 		return (
 			<div className="App">
-				<SmurfForm />
+				<SmurfForm addSmurf={this.addSmurf} />
 				<Smurfs smurfs={this.state.smurfs} />
 			</div>
 		);
