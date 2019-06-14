225e9fbe1ffe735d022249e7e23b481a68fe7e84
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-06-14 11:30:47 +0400

    add componentDidMount

--- village/src/App.js
+++ village/src/App.js
@@ -1,27 +1,45 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      smurfs: [],
-    };
-  }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
-  render() {
-    return (
-      <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			smurfs: [],
+			isLoading: false,
+			errorMessage: ''
+		};
+	}
+	// add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
+	// Notice what your map function is looping over and returning inside of Smurfs.
+	// You'll need to make sure you have the right properties on state and pass them down to props.
+
+	componentDidMount() {
+		this.setState({ isLoading: true });
+		axios
+			.get('http://localhost:3333/smurfs')
+			.then(res => {
+				this.setState({ smurfs: res.data });
+			})
+			.catch(err => {
+				this.setState({ errorMessage: err.response.statusText });
+			})
+			.finally(this.setState({ isLoading: false }));
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<SmurfForm />
+				<Smurfs smurfs={this.state.smurfs} />
+			</div>
+		);
+	}
 }
 
 export default App;
