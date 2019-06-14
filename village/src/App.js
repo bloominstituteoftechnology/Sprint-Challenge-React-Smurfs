9ea5829c4eb888c1e65e2de101703d9ad18e3221
Jengopockets <jengopockets@gmail.com>
2019-06-14 12:43:01 -0400

    Created Nav Bar

--- village/src/App.js
+++ village/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import { runInContext } from 'vm';
 
 class App extends Component {
   constructor(props) {
@@ -29,8 +31,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Route
+            exact path='/'
+            render={(props) => <Smurfs {...props}
+              smurfs={this.state.smurfs}
+              sendFormData={(newData) => this.setState({smurfs: newData})} />} />
+        <Route
+            exact path='/smurf-from'
+            render={(props) => <SmurfForm {...props}
+              sendFormData={(newData) => this.setState({smurfs: newData})} />} />
       </div>
     );
   }
