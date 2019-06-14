9d8dcf761e011a0d423a6e35a23f7959b164130a
Bryce-Soghigian <49734722+Bryce-Soghigian@users.noreply.github.com>
2019-06-14 11:44:28 -0500

    Update App.js
    Redoing all my routes added some new components

--- village/src/App.js
+++ village/src/App.js
@@ -1,71 +1,18 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Route, NavLink} from 'react-router-dom';
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import React, { Component } from "react";
+import "./App.css";
+import Home from "./components/Home/Home";
+import Form from "./components/SmurfForm";
+import Nav from "./components/Nav/Nav";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      smurfs: [],
-    };
-  }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
-
-  componentDidMount(){
-    axios
-      .get('http://localhost:3333/smurfs')
-      .then( response => {
-        this.setState(() => ({ smurfs: response.data }))
-      })
-
-      .catch( error => {
-        console.error('Server Error', error);
-      });
-  }
-
-  postSmurf = (event, smurf) => {
-    event.preventDefault();
-    axios 
-      .post('http://localhost:3333/smurfs', smurf)
-      .then( response => {
-        this.setState({
-          smurfs: response.data
-        })
-      })
-
-      .catch( error => console.log("uh oh", error))
-  }
-
-
-  render() {
-    return (
-      <div className="App">
-
-      <div className = "nav-bar">
-          <NavLink to = "/smurf-form" className = "nav-links" activeClassName = "activeLink" exact> Smurf Form </NavLink>
-          <NavLink to = "/" className = "nav-links" activeClassName = "activeLink" exact> See the Smurfs </NavLink>
-      </div>
-
-
-        {/* <SmurfForm postSmurf = {this.postSmurf}/>
-        <Smurfs smurfs={this.state.smurfs} /> */}
-
-        <Route exact path = "/smurf-form" render = {
-           () =>
-            <SmurfForm postSmurf = {this.postSmurf} /> 
-            } />
-        <Route exact path = "/" render = {
-           () =>
-            <Smurfs smurfs = {this.state.smurfs}/>
-             } />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <Router>
+      <Nav />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/form" component={Form} />
+    </Router>
+  );
 }
 
 export default App;
