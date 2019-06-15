c45003406c093b3d8f105dee95f9c9360c55f7aa
Bryce-Soghigian <49734722+Bryce-Soghigian@users.noreply.github.com>
2019-06-15 13:26:08 -0500

    Update index.js

--- village/src/index.js
+++ village/src/index.js
@@ -5,4 +5,4 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router><App/></Router>, document.getElementById('root'));
