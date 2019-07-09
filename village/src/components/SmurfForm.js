70fbfd64f86a1f17f7903f25d57d99bfd4b8fa2a
Juan Aleman <juan.j.aleman@gmail.com>
2019-07-08 21:18:21 -0500

    Axios Post request

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -13,6 +14,15 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
+    axios
+      .post(`http://localhost:3333/smurfs`, this.state)
+      .then(response => {
+        this.props.sendFormData(response.data)
+        this.setState({ redirect: true })
+      })
+      .catch(error => {
+        console.error('Server Error', error)
+      })
 
     this.setState({
       name: '',
