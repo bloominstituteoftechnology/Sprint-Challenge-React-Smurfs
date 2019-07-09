c2f286b280085119dbce7545c6a760fe08b83ab7
Jordan Lanier <jordan.6195@yahoo.com>
2019-07-08 21:36:42 -0400

    mvp

--- village/src/App.js
+++ village/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import {Route, NavLink} from 'react-router-dom'
 
 class App extends Component {
   constructor(props) {
@@ -14,11 +15,34 @@ class App extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+  componentDidMount() {
+    axios.get('http://localhost:3333/smurfs')
+    .then(res => {
+      this.setState({
+        smurfs: res.data
+      })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+  
+  updateSmurfs = (smurfs) => {
+		this.setState({ smurfs })
+	}
+  
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+  
+        <Route path="/smurf-form" exact render={(props) => <SmurfForm {...props} updateSmurfs={this.updateSmurfs} />} />
+        <Route path="/" exact render={(props) => <Smurfs {...props} smurfs={this.state.smurfs} />} />
+
+<div className="nav-links">
+						<NavLink to="/" className='link'>Smurfs</NavLink>
+						<NavLink to="/smurf-form" className='link'>Add Smurf</NavLink>
+					</div>
+
       </div>
     );
   }
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,57 +1,56 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      age: '',
-      height: ''
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: '',
+			age: '',
+			height: ''
+		};
+	}
 
-  addSmurf = event => {
-    event.preventDefault();
-    // add code to create the smurf using the api
+	addSmurf = (event) => {
+		event.preventDefault();
+		// add code to create the smurf using the api
 
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
-  }
+		const { name, age, height } = this.state;
+		const payload = { name, age, height };
 
-  handleInputChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+		axios
+			.post('http//localhost:3333/smurfs', payload)
+			.then((res) => {
+				console.log(res.data);
+				this.setState({
+					name: res.data.name,
+					age: res.data.age,
+					height: res.data.height
+				});
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
+	handleInputChange = (e) => {
+		this.setState({ [e.target.name]: e.target.value });
+	};
 
-  render() {
-    return (
-      <div className="SmurfForm">
-        <form onSubmit={this.addSmurf}>
-          <input
-            onChange={this.handleInputChange}
-            placeholder="name"
-            value={this.state.name}
-            name="name"
-          />
-          <input
-            onChange={this.handleInputChange}
-            placeholder="age"
-            value={this.state.age}
-            name="age"
-          />
-          <input
-            onChange={this.handleInputChange}
-            placeholder="height"
-            value={this.state.height}
-            name="height"
-          />
-          <button type="submit">Add to the village</button>
-        </form>
+	render() {
+		return (
+    <div className="smurfformbox">
+			<div className="SmurfForm">
+        
+				<form onSubmit={this.addSmurf}>
+					<input onChange={this.handleInputChange} placeholder="name" value={this.state.name} name="name" />
+					<input onChange={this.handleInputChange} placeholder="age" value={this.state.age} name="age" />
+					<input onChange={this.handleInputChange} placeholder="height" value={this.state.height} name="height" />
+					<button type="submit">Submit</button>
+				</form>
+			</div>
       </div>
-    );
-  }
+		);
+	}
 }
 
 export default SmurfForm;
